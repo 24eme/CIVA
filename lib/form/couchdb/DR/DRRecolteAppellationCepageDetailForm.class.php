882c1c1d35146accd98f86ff79200264e99b2dfa
Vincent LAURENT <vlaurent@actualys.com>
2010-07-23 16:14:03 +0000

    commit precedent mise en place des forms

--- lib/form/couchdb/DR/DRRecolteAppellationCepageDetailForm.class.php
+++ lib/form/couchdb/DR/DRRecolteAppellationCepageDetailForm.class.php
@@ -2,7 +2,6 @@
 
 class DRRecolteAppellationCepageDetailForm extends BaseDRRecolteAppellationCepageDetailForm {
     public function configure() {
-        //print_r($this->getObject()->getData());
         $this->embedForm('0', new DRRecolteAppellationCepageDetailAcheteurForm($this->doc, $this->getObject()->get('acheteurs/0')));
     }
 }
