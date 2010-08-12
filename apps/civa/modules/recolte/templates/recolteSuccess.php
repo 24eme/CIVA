4073c846bec1a92159ae25aa77634cd0b615e474
Vincent LAURENT <vlaurent@actualys.com>
2010-08-12 14:10:28 +0000

    boutons actions dans la recolte

--- apps/civa/modules/recolte/templates/recolteSuccess.php
+++ apps/civa/modules/recolte/templates/recolteSuccess.php
@@ -72,8 +72,13 @@
 				</div>
 				<!-- fin #application_dr -->
 				
+				<ul id="btn_etape" class="btn_prev_suiv clearfix">
+				<!-- InstanceBeginEditable name="btn_etape" -->
+					<li class="prec"><a href="<?php echo url_for('@exploitation_autres') ?>"><img src="/images/boutons/btn_retourner_etape_prec.png" alt="Retourner à l'étape précédente" /></a></li>
+					<li class="suiv"><a href="<?php echo url_for('@validation') ?>"><img src="/images/boutons/btn_passer_etape_suiv.png" alt="Passer à l'étape suivante" /></a></li>
+				<!-- InstanceEndEditable -->
+				</ul>
 				
-				<?php include_partial('global/boutons', array('display' => array('precedent','suivant'))) ?>
 
 				
 			<!--</form>-->
