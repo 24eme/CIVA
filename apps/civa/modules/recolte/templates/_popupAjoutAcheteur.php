f8c26f2d97f77330c717c0187aeadb94e3cee6bb
Vincent LAURENT <vlaurent@actualys.com>
2010-08-31 17:30:43 +0000

    Gestion des acheteurs suites et integration des modifs dhamza sur la page recolte (suite)

---
+++ apps/civa/modules/recolte/templates/_popupAjoutAcheteur.php
@@ -0,0 +1,13 @@
+<div id="<?php echo $id ?>" class="popup_ajout" title="<?php echo $title ?>">
+    <form action="<?php echo $action ?>" method="post">
+
+        <input type="hidden" name="type_cssclass" value="<?php echo $cssclass ?>" />
+        <input type="hidden" name="type_name_field" value="<?php echo $name ?>" />
+
+        <label for="champ_acheteur_nom">Entrez le nom de l'acheteur, son CVI ou sa commune :</label>
+        <input id="champ_acheteur_nom" class="nom" type="text" name="" />
+        <input class="cvi" type="hidden" name="" />
+        <input class="commune" type="hidden" name="" />
+        <input type="image" name="" src="/images/boutons/btn_valider.png" alt="Valider" />
+    </form>
+</div>
