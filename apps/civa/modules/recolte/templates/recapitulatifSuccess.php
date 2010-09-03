0db85c57680499241184e8fbf5628f95560d1254
Vincent LAURENT <vlaurent@actualys.com>
2010-09-03 14:18:19 +0000

    bug nom du partial incorrect

--- apps/civa/modules/recolte/templates/recapitulatifSuccess.php
+++ apps/civa/modules/recolte/templates/recapitulatifSuccess.php
@@ -108,7 +108,7 @@ foreach($appellationlieu->acheteurs as $cvi => $info) {
 			</form>
 			<!-- fin #principal -->
 
-                        <?php include_partial('recolte/ajoutPopupOnglets', array('onglets' => $onglets,
+                        <?php include_partial('recolte/popupAjoutOnglets', array('onglets' => $onglets,
                                                          'form_appellation' => $form_ajout_appellation,
                                                          'form_lieu' => $form_ajout_lieu,
                                                          'url_lieu' => $url_ajout_lieu)) ?>
