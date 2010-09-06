4fcb316917a281a2956b03ded48b91eed90287db
Charlotte Devichet <cdevichet@actualys.com>
2010-09-06 12:45:35 +0000

    tache generate base_url for JSP

---
+++ lib/task/generateBaseUrlForJspTask.class.php
@@ -0,0 +1,46 @@
+<?php
+
+class generateBaseUrlForJspTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'civa'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'couchdb'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'civa-config';
+    $this->name             = 'generateBaseUrlForJsp';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [generateBaseUrlForJsp|INFO] task does things.
+Call it with:
+
+  [php symfony generateBaseUrlForJsp|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    
+    // add your code here
+    $base_url = sfConfig::get('app_base_url');
+    $fichier_conf = sfConfig::get('app_tomcat_dir').'/base_url.conf';
+    $fp = fopen($fichier_conf,"w");
+    if($fp){
+        fputs($fp, $base_url);
+        fclose($fp);
+    }
+    
+
+   
+
+  }
+}
