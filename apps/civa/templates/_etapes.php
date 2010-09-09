7bdac61aa597ebb88f2b5ddc8066e42de42777c7
Hamza Iqbal <hiqbal@actualys.com>
2010-09-09 16:12:14 +0000

    rail cliquable

--- apps/civa/templates/_etapes.php
+++ apps/civa/templates/_etapes.php
@@ -1,5 +1,5 @@
 <ul id="etape_declaration" class="clearfix">
-    <li class="<?php if ($etape > 1): ?>passe<?php elseif($etape == 1): ?>actif<?php endif; ?>">Exploitation <em>Etape 1</em></li>
-    <li class="<?php if ($etape > 2): ?>passe<?php elseif($etape == 2): ?>actif<?php endif; ?>">Récolte <em>Etape 2</em></li>
-    <li class="<?php if ($etape > 3): ?>passe<?php elseif($etape == 3): ?>actif<?php endif; ?>">Validation <em>Etape 3</em></li>
+    <li class="<?php if ($etape > 1): ?>passe<?php elseif($etape == 1): ?>actif<?php endif; ?>"><a href="#">Exploitation <em>Etape 1</em></a></li>
+    <li class="<?php if ($etape > 2): ?>passe<?php elseif($etape == 2): ?>actif<?php endif; ?>"><a href="#">Récolte <em>Etape 2</em></a></li>
+    <li class="<?php if ($etape > 3): ?>passe<?php elseif($etape == 3): ?>actif<?php endif; ?>"><a href="#">Validation <em>Etape 3</em></a></li>
 </ul>
