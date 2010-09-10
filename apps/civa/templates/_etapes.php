a84ca998813b9180d82959d5506412c753ebae84
Charlotte Devichet <cdevichet@actualys.com>
2010-09-10 09:06:55 +0000

    rail cliquable

--- apps/civa/templates/_etapes.php
+++ apps/civa/templates/_etapes.php
@@ -1,5 +1,5 @@
 <ul id="etape_declaration" class="clearfix">
-    <li class="<?php if ($etape > 1): ?>passe<?php elseif($etape == 1): ?>actif<?php endif; ?>"><a href="#">Exploitation <em>Etape 1</em></a></li>
-    <li class="<?php if ($etape > 2): ?>passe<?php elseif($etape == 2): ?>actif<?php endif; ?>"><a href="#">Récolte <em>Etape 2</em></a></li>
-    <li class="<?php if ($etape > 3): ?>passe<?php elseif($etape == 3): ?>actif<?php endif; ?>"><a href="#">Validation <em>Etape 3</em></a></li>
+    <li class="<?php if ($etape > 1): ?>passe<?php elseif($etape == 1): ?>actif<?php endif; ?>"><a href="/exploitation_administratif">Exploitation <em>Etape 1</em></a></li>
+    <li class="<?php if ($etape > 2): ?>passe<?php elseif($etape == 2): ?>actif<?php endif; ?>"><a href="/recolte">Récolte <em>Etape 2</em></a></li>
+    <li class="<?php if ($etape > 3): ?>passe<?php elseif($etape == 3): ?>actif<?php endif; ?>"><a href="/validation">Validation <em>Etape 3</em></a></li>
 </ul>
