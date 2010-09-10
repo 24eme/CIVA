6115061aa14a2843aadcdbf6cefac0c8e7afbc5f
Charlotte Devichet <cdevichet@actualys.com>
2010-09-10 15:25:30 +0000

    message compte deja cree

--- lib/validator/ValidatorFirstConnection.class.php
+++ lib/validator/ValidatorFirstConnection.class.php
@@ -3,6 +3,7 @@
 class ValidatorFirstConnection extends sfValidatorBase {
     public function configure($options = array(), $messages = array()) {
        $this->addMessage('mdp_invalid', 'CVI ou code de création invalide.');
+       $this->addMessage('compte_cree', 'Votre code a déja été créé.');
     }
 
     protected function doClean($values) {
@@ -13,11 +14,13 @@ class ValidatorFirstConnection extends sfValidatorBase {
         
         if($mdp && $cvi) {
             $tiers = sfCouchdbManager::getClient('Tiers')->retrieveByCvi($cvi);
-            if ($tiers && $tiers->mot_de_passe == '{TEXT}'.$mdp) {
+            if(substr($tiers->mot_de_passe, 0, 6)=='{SSHA}'){
+                throw new sfValidatorErrorSchema($this, array($this->getOption('mdp') => new sfValidatorError($this, 'compte_cree')));
+            }elseif ($tiers && $tiers->mot_de_passe == '{TEXT}'.$mdp) {
                 return array_merge($values, array('tiers' => $tiers));
+            }else{
+                throw new sfValidatorErrorSchema($this, array($this->getOption('mdp') => new sfValidatorError($this, 'mdp_invalid')));
             }
-            
-            throw new sfValidatorErrorSchema($this, array($this->getOption('mdp') => new sfValidatorError($this, 'mdp_invalid')));
         }
 
     }
