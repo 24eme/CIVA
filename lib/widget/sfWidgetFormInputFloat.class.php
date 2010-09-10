42e0764c396efbbe7c7979c4eacf6b4e1761fc19
Tangui Morlier <tangui@tangui.eu.org>
2010-09-10 08:44:44 +0000

    Accès au helper

--- lib/widget/sfWidgetFormInputFloat.class.php
+++ lib/widget/sfWidgetFormInputFloat.class.php
@@ -4,6 +4,7 @@ class sfWidgetFormInputFloat extends sfWidgetFormInputText
 {
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
+    sfApplicationConfiguration::getActive()->loadHelpers('civa');
     $value = sprintFloat($value);
     return parent::render($name, $value, $attributes, $errors);
   }
