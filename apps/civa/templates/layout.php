8da343be239046a8f2257d2ae5effca4d0465b64
Charlotte Devichet <cdevichet@actualys.com>
2010-09-15 16:04:05 +0000

    encodade HTML dans title

--- apps/civa/templates/layout.php
+++ apps/civa/templates/layout.php
@@ -5,7 +5,7 @@
     <?php include_metas() ?>
       <title>
         <?php $title = $sf_context->getInstance()->getResponse()->getTitle();
-        printf($title , date("Y")); ?>
+        printf(html_entity_decode($title) , date("Y")); ?>
       </title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
