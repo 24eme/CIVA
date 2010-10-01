7ca1f777ebbe89e8203a5d9d83ef2a5fb762cd51
Tangui Morlier <tangui@tangui.eu.org>
2010-10-01 15:37:58 +0000

    Deux décimales

--- apps/civa/modules/declaration/templates/_recapDeclaration.php
+++ apps/civa/modules/declaration/templates/_recapDeclaration.php
@@ -1,3 +1,4 @@
+<?php use_helper('civa') ?>
 <div id="recolte_totale" class="clearfix">
 
     <div id="appelations">
@@ -15,25 +16,25 @@
                 <tr>
                     <td>Superficie (ares)</td>
                     <?php foreach ($appellations as $a)  if (!isset($ignore[$a]) || !$ignore[$a]) : ?>
-                    <td><?php echo $superficie[$a]; ?></td>
+                    <td><?php echoFloat( $superficie[$a]); ?></td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td>Volume Total (Hl)</td>
                     <?php foreach ($appellations as $a) if (!isset($ignore[$a]) || !$ignore[$a]) : ?>
-                    <td><?php echo $volume[$a]; ?></td>
+                    <td><?php echoFloat( $volume[$a]); ?></td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td>Volume Revendiqué (Hl)</td>
                     <?php foreach ($appellations as $a) if (!isset($ignore[$a]) || !$ignore[$a]) : ?>
-                    <td><?php echo $revendique[$a]; ?></td>
+                    <td><?php echoFloat( $revendique[$a]); ?></td>
                     <?php endif; ?>
                 </tr>
                 <tr>
                     <td>DPLC (Hl)</td>
                     <?php foreach ($appellations as $a) if (!isset($ignore[$a]) || !$ignore[$a]) : ?>
-                    <td><?php echo $dplc[$a]; ?></td>
+                    <td><?php echoFloat( $dplc[$a]); ?></td>
                     <?php endif; ?>
                 </tr>
             </tbody>
@@ -44,10 +45,10 @@
     <div id="total_general">
         <h2 class="titre_section">Total général</h2>
         <ul class="contenu_section">
-            <li><input type="text" value="<?php echo $total_superficie;?> ares" readonly="readonly"></li>
-            <li><input type="text" value="<?php echo $total_volume;?> Hl" readonly="readonly"></li>
-            <li><input type="text" value="<?php echo $total_revendique;?> Hl" readonly="readonly"></li>
-            <li><input type="text" value="<?php echo $total_dplc;?> Hl" readonly="readonly"></li>
+            <li><input type="text" value="<?php echoFloat( $total_superficie);?> ares" readonly="readonly"></li>
+            <li><input type="text" value="<?php echoFloat( $total_volume);?> Hl" readonly="readonly"></li>
+            <li><input type="text" value="<?php echoFloat( $total_revendique);?> Hl" readonly="readonly"></li>
+            <li><input type="text" value="<?php echoFloat( $total_dplc);?> Hl" readonly="readonly"></li>
         </ul>
     </div>
 	<div id="recap_autres">
@@ -59,14 +60,14 @@
 			</thead>
 			<tbody>
 				<tr>
-					<td class="premiere_colonne">Jeunes Vignes : </td><td><?php echo $jeunes_vignes; ?>&nbsp;<small>ares</small></td>
+    <td class="premiere_colonne">Jeunes Vignes : </td><td><?php echoFloat( $jeunes_vignes); ?>&nbsp;<small>ares</small></td>
 				</tr>
 				<tr>
-					<td class="premiere_colonne">Lies :</td><td><?php echo $lies; ?>&nbsp;Hl</td>
+    <td class="premiere_colonne">Lies :</td><td><?php echoFloat( $lies); ?>&nbsp;Hl</td>
 				</tr>
 					<?php if (isset($vintable['superficie'])) : ?>
 				<tr>
-					<td class="premiere_colonne">Vin de table : </td><td><?php echo $vintable['superficie']; ?>&nbsp;<small>ares</small> / <?php echo $vintable['volume']; ?> Hl</td>
+				   <td class="premiere_colonne">Vin de table : </td><td><?php echoFloat( $vintable['superficie']); ?>&nbsp;<small>ares</small> / <?php echoFloat( $vintable['volume']); ?> Hl</td>
 				</tr>
 				<?php endif; ?>
 			</tbody>
