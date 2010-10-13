f5b53190c4177097336dd2ca054bde435e091b6f
Charlotte Devichet <cdevichet@actualys.com>
2010-10-13 12:43:54 +0000

    btn voir les dr prec

--- apps/civa/templates/_actions.php
+++ apps/civa/templates/_actions.php
@@ -6,7 +6,6 @@
 
 <ul id="actions_declaration">
     <?php if($etape == 2): ?>
-        <li class="btn"><a href="<?php echo url_for('@telecharger_la_notice') ?>" class=""><img src="/images/boutons/btn_telecharger_notice.png" alt="Télécharger la notice" /></a></li>
         <li class="btn"><a href="" class="btn_voir_dr_prec"><img src="/images/boutons/btn_voir_dr_prec.png" alt="Voir les DR précedentes" /></a></li>
     <?php endif; ?>
     <?php if(isset($help_popup_action)): ?><li><a href="" class="msg_aide" rel="<?php echo $help_popup_action ?>" title="Message aide">Teste message d'aide</a></li><?php endif; ?>
