624273996bcccd5de0894fc24a69259248a3d23c
Charlotte Devichet <cdevichet@actualys.com>
2010-10-22 17:01:58 +0000

    nouvelle page du footer

---
+++ apps/civa/modules/global/templates/contactSuccess.php
@@ -0,0 +1,15 @@
+<h2 class="titre_principal">Contact</h2>
+
+<!-- #application_dr -->
+<div id="application_dr" class="clearfix pages-statiques">
+    <h3>Conseil Interprofessionnel des Vins d’Alsace</h3>
+    <p>
+        12 avenue de la Foire-Aux-Vins<br />
+        BP 11217<br />
+        68012 Colmar Cedex<br />
+        France<br />
+        Tél. : (+33) 03 89 20 16 20<br />
+        Fax : (+33) 03 89 20 16 30<br />
+    </p>
+</div>
+<!-- fin #application_dr -->
