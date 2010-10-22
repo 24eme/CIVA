90a50d1ed17b69926aa3c891d04ffaa57a9668b9
Charlotte Devichet <cdevichet@actualys.com>
2010-10-22 15:51:08 +0000

    cas.css

--- apps/civa/modules/global/actions/actions.class.php
+++ apps/civa/modules/global/actions/actions.class.php
@@ -8,31 +8,41 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class globalActions extends sfActions
-{
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeMessageAjax(sfWebRequest $request)
-  {
-      $this->forward404Unless($request->isXmlHttpRequest());
-      return $this->renderText(json_encode(array('titre' => $request->getParameter('title', null),
-                                                 'message' => sfCouchdbManager::getClient('Messages')->getMessage($request->getParameter('id', null)))));
+class globalActions extends sfActions {
+    /**
+     * Executes index action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeMessageAjax(sfWebRequest $request) {
+        $this->forward404Unless($request->isXmlHttpRequest());
+        return $this->renderText(json_encode(array('titre' => $request->getParameter('title', null),
+                'message' => sfCouchdbManager::getClient('Messages')->getMessage($request->getParameter('id', null)))));
 
-  }
-  public function executeError404(){
-
-  }
-
-  public function executeSecure(){
-    if ($this->getUser()->isDeclarant()) {
-        return $this->redirect("@mon_espace_civa");
-    } elseif ($this->getUser()->isNonDeclarant()) {
-        return $this->redirect("@mon_espace_civa_non_declarant");
-    } elseif ($this->getUser()->isAdmin()) {
-        return $this->redirect("@login_admin");
     }
-  }
+    public function executeError404() {
+
+    }
+
+    public function executeSecure() {
+        if ($this->getUser()->isDeclarant()) {
+            return $this->redirect("@mon_espace_civa");
+        } elseif ($this->getUser()->isNonDeclarant()) {
+            return $this->redirect("@mon_espace_civa_non_declarant");
+        } elseif ($this->getUser()->isAdmin()) {
+            return $this->redirect("@login_admin");
+        }
+    }
+
+    public function executeMentionsLegales(){
+
+    }
+
+    public function executeAProposDuCiva(){
+
+    }
+
+    public function executeContact(){
+
+    }
 }
--- apps/civa/templates/_footer.php
+++ apps/civa/templates/_footer.php
@@ -2,9 +2,9 @@
 <div id="footer">
         <p>&copy; CIVA 2010</p>
         <ul>
-                <li><a href="#">A propos du CIVA</a></li>
-                <li><a href="#">Mentions l&eacute;gales</a></li>
-                <li><a href="#">Contactez nous</a></li>
-        </ul>
+                <li><a href="<?php echo url_for('@aProposDuCiva'); ?>">A propos du CIVA</a></li>
+                <li><a href="<?php echo url_for('@mentions-legales'); ?>">Mentions l&eacute;gales</a></li>
+                <li><a href="<?php echo url_for('@contact'); ?>">Contactez nous</a></li>
+       </ul>
 </div>
 <!-- fin #footer -->
