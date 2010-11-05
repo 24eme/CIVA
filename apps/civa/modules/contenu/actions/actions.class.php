9004720f9c356068588d084a344135912795499f
Vincent LAURENT <vlaurent@actualys.com>
2010-11-05 14:05:30 +0000

    creation dun module de contenu pour les pages corporates

---
+++ apps/civa/modules/contenu/actions/actions.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * contenu actions.
+ *
+ * @package    civa
+ * @subpackage contenu
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class contenuActions extends sfActions
+{
+  public function executeMentionsLegales(){
+
+  }
+
+  public function executeAProposDuCiva(){
+
+  }
+
+  public function executeContact(){
+
+  }
+}
---
+++ apps/civa/modules/contenu/templates/contactSuccess.php
@@ -0,0 +1,16 @@
+<h2 class="titre_principal">Contact</h2>
+
+<!-- #application_dr -->
+<div id="application_dr" class="clearfix pages-statiques">
+    <h3>Conseil Interprofessionnel des Vins d’Alsace</h3>
+    <p>
+        12 avenue de la Foire-Aux-Vins<br />
+        BP 11217<br />
+        68012 Colmar Cedex<br />
+        France<br /><br />
+        <strong>Tél. :</strong> (+33) 03 89 20 16 20<br />
+        <strong>Fax :</strong> (+33) 03 89 20 16 30<br />
+        <strong>E-mail :</strong> <a href="mailto:civa@civa.fr" >civa@civa.fr</a>
+    </p>
+</div>
+<!-- fin #application_dr -->
