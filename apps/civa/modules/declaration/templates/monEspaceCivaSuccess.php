265a0a47817adc4b91c5b6cf0883d6f2a9924201
Vincent LAURENT <vlaurent@actualys.com>
2010-12-10 16:49:36 +0000

    mon espace gamma

--- apps/civa/modules/declaration/templates/monEspaceCivaSuccess.php
+++ apps/civa/modules/declaration/templates/monEspaceCivaSuccess.php
@@ -76,13 +76,24 @@
             <?php endif; ?>
             <br />
             <?php if($has_no_assices){ ?>
+            <?php if ($sf_user->getTiers()->cvi == '7523700100'): ?>
+            <h3 class="titre_section">Alsace Gamm@ <a href="" class="msg_aide" rel="help_popup_mon_espace_civa_gamma" title="Message aide"></a></h3>
+            <div class="contenu_section">
+                <!--<p class="intro"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('intro_gamma'); ?></p>-->
+                <div class="ligne_form">
+                    <input type="radio" id="gamma_type_acces_test" name="gamma[type_acces]" value="test" checked="checked" />
+                    <label for="type_declaration_brouillon">Plateforme de test</label>
+                </div>
+                <div class="ligne_form ligne_btn">
+                    <input type="image" name="gamma" class="btn" src="../images/boutons/btn_valider.png" alt="Valider" />
+                </div>
+            </div>
+            <?php else: ?>
             <h3 class="titre_section">Gamma <a href="" class="msg_aide" rel="help_popup_mon_espace_civa_gamma" title="Message aide"></a></h3>
             <div class="contenu_section">
                 <p class="intro"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('intro_gamma'); ?></p>
-                <?php if ($sf_user->getTiers()->cvi == '7523700100'): ?>
-                <a href="http://qualif.gamma.vinsalsace.pro/" target="_blank">Accéder à Gamm@ test</a>
-                <?php endif; ?>
             </div>
+            <?php endif; ?>
             <?php } ?>
         </div>
         <!-- fin #nouvelle_declaration -->
