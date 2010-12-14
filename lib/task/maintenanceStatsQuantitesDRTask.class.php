99703db82c5d7c824fb662bfd69c997092b0343c
Vincent LAURENT <vlaurent@actualys.com>
2010-12-14 17:08:39 +0000

    tache totaux volumes et superficie par appellation memory limit

--- lib/task/maintenanceStatsQuantitesDRTask.class.php
+++ lib/task/maintenanceStatsQuantitesDRTask.class.php
@@ -30,6 +30,8 @@ EOF;
 
   protected function execute($arguments = array(), $options = array())
   {
+    ini_set("memory_limit", "256M");
+
     // initialize the database connection
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
