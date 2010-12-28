7b2b773ffff43e1c053dcf4f33e2b55c3f77d62b
Vincent LAURENT <vlaurent@actualys.com>
2010-12-28 12:50:34 +0000

    memory limit export xml

--- lib/task/exportDRXmlTask.class.php
+++ lib/task/exportDRXmlTask.class.php
@@ -30,7 +30,7 @@ EOF;
 
   protected function execute($arguments = array(), $options = array())
   {
-    ini_set('memory_limit', '1024M');
+    ini_set('memory_limit', '2048M');
     
     // initialize the database connection
     $databaseManager = new sfDatabaseManager($this->configuration);
