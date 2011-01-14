993e31b7bdbb05f01b88f294487bf154b95cd7df
Vincent LAURENT <vlaurent@actualys.com>
2011-01-14 17:32:28 +0000

    export xml

--- lib/task/exportDRXmlTask.class.php
+++ lib/task/exportDRXmlTask.class.php
@@ -49,9 +49,9 @@ EOF;
     foreach ($dr_ids as $id) {
         if ($id !== 'DR-7523700100-'.$options['campagne']) {
             $dr = sfCouchdbManager::getClient("DR")->retrieveDocumentById($id);
-            if ($options['campagne'] == "2010" && $dr->exist('import_db2') && $dr->import_db2 == 1) {
+            /*if ($options['campagne'] == "2010" && $dr->exist('import_db2') && $dr->import_db2 == 1) {
                 continue;
-            }
+            }*/
             try {
                 if (!$dr->updated)
                     throw new Exception();
