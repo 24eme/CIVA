b1a06594aeacf4e3727933c6c8003c086bf4a68d
Vincent LAURENT <vlaurent@actualys.com>
2011-07-26 09:14:53 +0000

    [Compte] task message ajout du message de la nouvelle page séléction de profils

--- lib/task/import/importMessagesTask.class.php
+++ lib/task/import/importMessagesTask.class.php
@@ -51,6 +51,8 @@ EOF;
 	$json->type = 'Messages';
 
 	$json->msg_compte_index_intro = "Pour créer votre compte, merci d'indiquer votre numéro CVI et votre code de création de compte (ceux que le CIVA vous a communiqués par courrier de compte)";
+        $json->msg_tiers_index_intro = "Merci de choisir un profil parmi la liste ci-dessous";
+        
         $json->telecharger_pdf_mon_espace = "Cette notice est au format PDF. Pour la visualiser, veuillez télécharger le logiciel <a href=\"http://get.adobe.com/fr/reader/\" >Adobe Reader</a>.";
         $json->telecharger_pdf = "Le fichier généré est au format PDF. Pour la visualiser, veuillez télécharger le logiciel <a href=\"http://get.adobe.com/fr/reader/\" >Adobe Reader</a>.";
 
