8e46c423127244656d328d1fc7944eac136f6a9d
Tangui Morlier <tangui@tangui.eu.org>
2011-08-10 13:36:15 +0000

    de la mise en forme un peu gorret...

--- apps/civa/modules/import/templates/csvViewSuccess.php
+++ apps/civa/modules/import/templates/csvViewSuccess.php
@@ -5,17 +5,20 @@ if (!$csv)
 <div>
 <style>
    .error{color: red;}
+.titre{background-color: grey;}
+.csv td{font-family: "Arial,sans-serif,sans";border: 1px solid grey;padding:2px;}
 </style>
-<table>
+<table class="csv">
+<tr class="titre"><td>&nbsp;</td><td>CVI acheteur</td><td>Nom acheteur</td><td>CVI récoltant</td><td>Nom récoltant</td><td>Appellation</td><td>Lieu</td><td>Couleur</td><td>Cepage</td><td>VT/SGN</td><td>Denomination</td><td>Volume acheté</td><td>Superficie acheté</td></tr>
 <?php   $cpt = 0;
 foreach ($csv->getRawValue()->getCsv() as $line) 
 {
   echo '<tr';
   if (count($errors[$cpt]))
     echo ' class="error"';
-  echo '><td>'.($cpt+1).'</td><td>'.$line[0].'</td><td>'.$line[1].'</td><td>'.$line[2].'</td><td>'.$line[3].'</td><td>'.$line[4].'</td><td>'.$line[5].'</td><td>'.$line[6].'</td><td>'.$line[7].'</td><td>'.$line[8].'</td><td>'.$line[9].'</td><td>'.$line[10].'</td><td>'.$line[11].'</td><tr>';
+  echo '><td class="titre">'.($cpt+1).'</td><td>'.$line[0].'</td><td>'.$line[1].'</td><td>'.$line[2].'</td><td>'.$line[3].'</td><td>'.$line[4].'</td><td>'.$line[5].'</td><td>'.$line[6].'</td><td>'.$line[7].'</td><td>'.$line[8].'</td><td>'.$line[9].'</td><td>'.$line[10].'</td><td>'.$line[11].'</td><tr>';
   if (count($errors[$cpt])) {
-    echo '<tr class="error"><td>&nbsp;</td><td colspan="12">';
+    echo '<tr class="error"><td class="titre">&nbsp;</td><td colspan="12">';
     foreach($errors[$cpt] as $error) {
       echo $error.' | ';
     }
