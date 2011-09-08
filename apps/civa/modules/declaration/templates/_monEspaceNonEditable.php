8f2372df0c30ede605df2f672a471a6e6d90b0da
Vincent LAURENT <vlaurent@actualys.com>
2011-09-08 16:19:13 +0000

    Bug metghode has credention mal écrite dans le bloc d'accès à la déclaration courante lorsqu'elle n'est pas éditable

--- apps/civa/modules/declaration/templates/_monEspaceNonEditable.php
+++ apps/civa/modules/declaration/templates/_monEspaceNonEditable.php
@@ -3,7 +3,7 @@
     <p class="intro"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('intro_mon_espace_civa_dr_non_editable'); ?></p>
     <div class="ligne_form ligne_btn">
         <?php echo link_to('<img src="../images/boutons/btn_visualiser.png" alt="" class="btn" />', '@visualisation?annee=' . $sf_user->getCampagne()); ?>
-        <?php if ($sf_user->has_crendential(myUser::CREDENTIAL_ADMIN)): ?>
+        <?php if ($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
             <a href="<?php echo url_for('@declaration_invalider_civa') ?>" onclick="return confirm('Si vous éditez cette DR, pensez à la revalider.')"><img src="../images/boutons/btn_editer_dr.png" alt="" class="btn" id="rendreEditable"  /></a>
             <a href="<?php echo url_for('@declaration_invalider_recoltant') ?>" onclick="return confirm('Etes-vous sûr de vouloir dévalider cette DR ?')"><img src="../images/boutons/btn_devalider_dr.png" alt="" class="btn" id=""  /></a>
         <?php endif; ?>
