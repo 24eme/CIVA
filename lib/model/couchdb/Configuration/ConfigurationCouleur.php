504bf7a934b2694350d8f78dc19d4acc39aaa48e
Vincent LAURENT <vlaurent@actualys.com>
2011-10-05 16:37:08 +0000

    Bug il manquait le champs rendement dans la config de la couleur

--- lib/model/couchdb/Configuration/ConfigurationCouleur.php
+++ lib/model/couchdb/Configuration/ConfigurationCouleur.php
@@ -4,10 +4,6 @@ class ConfigurationCouleur extends BaseConfigurationCouleur {
     public function getCepages() {
       return $this->filter('^cepage')->toArray();
     }
-    
-    public function getRendement() {
-      return null;
-    }
 
     public function hasRendement() {
       return ($this->hasRendementCouleur());
