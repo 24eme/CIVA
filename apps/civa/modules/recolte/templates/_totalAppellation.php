87398d904e17346023568908e7d505744cb651ce
JB <jblemetayer@actualys.com>
2011-10-27 14:10:31 +0000

    Correction de la colonne total de l'aslace grand cru qui affichait les champs du rendement appellation alors qu'il ne fallait pas

--- apps/civa/modules/recolte/templates/_totalAppellation.php
+++ apps/civa/modules/recolte/templates/_totalAppellation.php
@@ -58,7 +58,7 @@
     <?php if ($lieu->getConfig()->hasRendement()): ?>
     <li class="rendement <?php if ($lieu->getDplcTotal()) echo 'alerte'; ?>">Rdt : <strong><span id="appellation_current_rendement"><?php echo round($lieu->getRendementRecoltant(),0); ?></span>&nbsp;hl/ha</strong><span class="picto_rdt_aide_col_total"><a href="" class="msg_aide" rel="help_popup_DR_total_appellation" title="Message aide"></a></span></li>
     <?php endif; ?>
-       <?php if ($lieu->getConfig()->hasRendement()): ?>
+       <?php if ($lieu->getConfig()->hasRendementAppellation()): ?>
 		    <input type="hidden" id="appellation_max_volume" value="<?php echoFloat( $lieu->getVolumeMaxAppellation()); ?>"/>
 		       <input type="hidden" id="appellation_rendement" value="<?php echoFloat( $lieu->getConfig()->getRendementAppellation()); ?>"/>
 
