460410195ed8264a4997a22d8b97fa2fedbf4f28
Vincent LAURENT <vlaurent@actualys.com>
2011-10-27 10:11:56 +0000

    Derniers bugs de migration de comptes

--- lib/task/maintenance/maintenanceMigrationTiersTask.class.php
+++ lib/task/maintenance/maintenanceMigrationTiersTask.class.php
@@ -30,27 +30,36 @@ EOF;
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-        $ids_tiers = array_merge(sfCouchdbManager::getClient("Recoltant")->getAll(sfCouchdbClient::HYDRATE_ON_DEMAND)->getIds(), sfCouchdbManager::getClient("MetteurEnMarche")->getAll(sfCouchdbClient::HYDRATE_ON_DEMAND)->getIds(), sfCouchdbManager::getClient("Acheteur")->getAll(sfCouchdbClient::HYDRATE_ON_DEMAND)->getIds());
+        $ids_tiers = array_merge(sfCouchdbManager::getClient("Recoltant")->getAll(sfCouchdbClient::HYDRATE_ON_DEMAND)->getIds(), 
+                                 sfCouchdbManager::getClient("MetteurEnMarche")->getAll(sfCouchdbClient::HYDRATE_ON_DEMAND)->getIds(), 
+                                 sfCouchdbManager::getClient("Acheteur")->getAll(sfCouchdbClient::HYDRATE_ON_DEMAND)->getIds());
         foreach ($ids_tiers as $id_tiers) {
             $tiers = sfCouchdbManager::getClient()->retrieveDocumentById($id_tiers, sfCouchdbClient::HYDRATE_JSON);
-            $t = sfCouchdbManager::getClient()->retrieveDocumentById($id_tiers, sfCouchdbClient::HYDRATE_DOCUMENT);
+            if (is_string($tiers->compte) || (isset($tiers->gamma) && is_string($tiers->gamma))) {
+                $t = sfCouchdbManager::getClient()->retrieveDocumentById($id_tiers, sfCouchdbClient::HYDRATE_DOCUMENT);
+            }
+            
             if (is_string($tiers->compte)) {
                 $t->remove('compte');
                 $t->add('compte');
                 $t->compte->add(null, $tiers->compte);
                 if($t->isModified()) {
-                        $this->logSection($t->get('_id'), "updated compte");
+                        $this->logSection($t->get('_id'), "updated tiers");
                 }
             }
+            
             if(isset($tiers->gamma) && is_string($tiers->gamma)) {
                     $t->remove('gamma');
                     $t->add('gamma');
-                    $t->gamma->statut = $met->gamma;
+                    $t->gamma->statut = $tiers->gamma;
                     if($t->isModified()) {
                         $this->logSection($t->get('_id'), "updated gamma");
                     }
             }
-            $t->save();
+            
+            if(isset($t)) {
+                $t->save();
+            }
         }
     }
 
