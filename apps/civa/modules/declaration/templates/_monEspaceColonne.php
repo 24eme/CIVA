f14206e7ce3be5d0d2b24c5ec9b27e837013f667
JB <jblemetayer@actualys.com>
2011-10-28 10:18:28 +0000

    Ajout du bloc d'aide dans le cadre alsace récolte de mon espace civa

--- apps/civa/modules/declaration/templates/_monEspaceColonne.php
+++ apps/civa/modules/declaration/templates/_monEspaceColonne.php
@@ -15,11 +15,13 @@
         </ul>
     </div>
 </div>
-<!--<h3 class="titre_section">Documents d'aide</h3>
-<div class="contenu_section">
-    <p class="intro"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('intro_doc_aide'); ?></p>
-    <ul>
-        <li><a href="<?php echo url_for('@telecharger_la_notice') ?>" class="pdf"> Télécharger la notice</a></li>
-    </ul>
-    <p class="intro pdf_link"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('telecharger_pdf_mon_espace'); ?></p>
-</div>-->
+<div id="documents_aide">
+	<h3 class="titre_section">Documents d'aide</h3>
+	<div class="contenu_section">
+	    <p class="intro"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('intro_doc_aide'); ?></p>
+	    <ul>
+	        <li><a href="<?php echo url_for('@telecharger_la_notice') ?>" class="pdf"> Télécharger la notice</a></li>
+	    </ul>
+	    <p class="intro pdf_link"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('telecharger_pdf_mon_espace'); ?></p>
+	</div>
+</div>
