40b2dc43927e0c911b91f8f1481d1b99f6c95183
Vincent LAURENT <vlaurent@actualys.com>
2011-10-31 18:04:09 +0000

    On rajoute les négocaves dans la liste

--- lib/model/couchdb/Acheteur/client/AcheteurClient.class.php
+++ lib/model/couchdb/Acheteur/client/AcheteurClient.class.php
@@ -1,41 +1,56 @@
 <?php
 
 class AcheteurClient extends sfCouchdbClient {
+
     protected $_acheteurs = null;
 
     public function getAll($hydrate = sfCouchdbClient::HYDRATE_DOCUMENT) {
         return $this->startkey('ACHAT-0000000000')->endkey('ACHAT-9999999999')->execute($hydrate);
     }
 
-    public function loadAcheteurs() {	
-       $cooperatives = $this->startkey(array('Cooperative'))
-       		->endkey(array('Cooperative', array()))
-            ->executeView('ACHAT', 'qualite');
-       $negociants = $this->startkey(array('Negociant'))
-       		->endkey(array('Negociant', array()))
-            ->executeView('ACHAT', 'qualite');
-       
-       $acheteurs_negociant = array();
-       $acheteurs_cave = array();
-       $acheteurs_mout = array();
-       foreach ($cooperatives as $key => $value) {
-       		$acheteurs_cave[$value->cvi]['cvi'] = $value->cvi;
+    public function loadAcheteurs() {
+        $cooperatives = $this->startkey(array('Cooperative'))
+                ->endkey(array('Cooperative', array()))
+                ->executeView('ACHAT', 'qualite');
+        $negociants = $this->startkey(array('Negociant'))
+                ->endkey(array('Negociant', array()))
+                ->executeView('ACHAT', 'qualite');
+        $negocaves = $this->startkey(array('NegoCave'))
+                ->endkey(array('NegoCave', array()))
+                ->executeView('ACHAT', 'qualite');
+
+        $acheteurs_negociant = array();
+        $acheteurs_cave = array();
+        $acheteurs_mout = array();
+        foreach ($cooperatives as $key => $value) {
+            $acheteurs_cave[$value->cvi]['cvi'] = $value->cvi;
             $acheteurs_cave[$value->cvi]['commune'] = $value->commune;
             $acheteurs_cave[$value->cvi]['nom'] = $value->nom;
-            
-       		$acheteurs_mout[$value->cvi]['cvi'] = $value->cvi;
+
+            $acheteurs_mout[$value->cvi]['cvi'] = $value->cvi;
             $acheteurs_mout[$value->cvi]['commune'] = $value->commune;
             $acheteurs_mout[$value->cvi]['nom'] = $value->nom;
-       }
-       foreach ($negociants as $key => $value) {
-       		$acheteurs_negociant[$value->cvi]['cvi'] = $value->cvi;
+        }
+        foreach ($negociants as $key => $value) {
+            $acheteurs_negociant[$value->cvi]['cvi'] = $value->cvi;
             $acheteurs_negociant[$value->cvi]['commune'] = $value->commune;
             $acheteurs_negociant[$value->cvi]['nom'] = $value->nom;
-            
-       		$acheteurs_mout[$value->cvi]['cvi'] = $value->cvi;
+
+            $acheteurs_mout[$value->cvi]['cvi'] = $value->cvi;
             $acheteurs_mout[$value->cvi]['commune'] = $value->commune;
             $acheteurs_mout[$value->cvi]['nom'] = $value->nom;
-       }
+        }
+
+        foreach ($negocaves as $key => $value) {
+            $acheteurs_negociant[$value->cvi]['cvi'] = $value->cvi;
+            $acheteurs_negociant[$value->cvi]['commune'] = $value->commune;
+            $acheteurs_negociant[$value->cvi]['nom'] = $value->nom;
+
+            $acheteurs_mout[$value->cvi]['cvi'] = $value->cvi;
+            $acheteurs_mout[$value->cvi]['commune'] = $value->commune;
+            $acheteurs_mout[$value->cvi]['nom'] = $value->nom;
+        }
+
         uasort($acheteurs_mout, 'AcheteurClient::sortByNom');
 
         $acheteurs = array();
@@ -44,32 +59,33 @@ class AcheteurClient extends sfCouchdbClient {
         $acheteurs['mouts'] = $acheteurs_mout;
         return $acheteurs;
     }
-    
+
     public function retrieveByCvi($cvi, $hydrate = sfCouchdbClient::HYDRATE_DOCUMENT) {
-        return parent::retrieveDocumentById('ACHAT-'.$cvi, $hydrate);
+        return parent::retrieveDocumentById('ACHAT-' . $cvi, $hydrate);
     }
 
     public static function sortByNom($a, $b) {
         return strcmp($a['nom'], $b['nom']);
     }
-    
-    protected function loadAcheteursList($type) {
-      if (is_null($this->_acheteurs)) {
-        $this->_acheteurs = CacheFunction::cache('model', array($this, 'loadAcheteurs'), array(), 31556926);
-      }
 
-      return $this->_acheteurs[$type];
+    protected function loadAcheteursList($type) {
+        if (is_null($this->_acheteurs)) {
+            $this->_acheteurs = CacheFunction::cache('model', array($this, 'loadAcheteurs'), array(), 31556926);
+        }
+
+        return $this->_acheteurs[$type];
     }
 
     public function getNegoces() {
-       return $this->loadAcheteursList('negoces');
+        return $this->loadAcheteursList('negoces');
     }
 
     public function getCooperatives() {
-       return $this->loadAcheteursList('cooperatives');
+        return $this->loadAcheteursList('cooperatives');
     }
 
     public function getMouts() {
-       return $this->loadAcheteursList('mouts');
+        return $this->loadAcheteursList('mouts');
     }
+
 }
