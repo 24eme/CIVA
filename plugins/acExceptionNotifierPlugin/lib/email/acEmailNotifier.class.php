e5d5f06ce47243e302c4596aac3c8ebab7fd4e55
JB <jblemetayer@actualys.com>
2011-11-02 14:37:37 +0000

    Multi email plugin acExceptionNotifier

--- plugins/acExceptionNotifierPlugin/lib/email/acEmailNotifier.class.php
+++ plugins/acExceptionNotifierPlugin/lib/email/acEmailNotifier.class.php
@@ -12,7 +12,7 @@ class acEmailNotifier {
     public static function exceptionEmailNotifier($message) {
     	$emailInformations = sfConfig::get('app_ac_exception_notifier_email');
         $from = array($emailInformations['from'] => $emailInformations['from_name']);
-        $to = array($emailInformations['to']);
+        $to = $emailInformations['to'];
         $subject = $emailInformations['subject'];
         $email = self::getMailer()->compose($from, $to, $subject, $message)->setContentType('text/html');
         return self::getMailer()->send($email);
@@ -21,7 +21,7 @@ class acEmailNotifier {
     public static function exceptionAttachedEmailNotifier($message) {
         $emailInformations = sfConfig::get('app_ac_exception_notifier_email');
         $from = array($emailInformations['from'] => $emailInformations['from_name']);
-        $to = array($emailInformations['to']);
+        $to = $emailInformations['to'];
         $subject = $emailInformations['subject'];
         $email = self::getMailer()->compose($from, $to, $subject, '<p>Exception traces attached</p>')->attach(new Swift_Attachment($message, 'exception-traces.html', 'text/html'))->setContentType('text/html');
 
