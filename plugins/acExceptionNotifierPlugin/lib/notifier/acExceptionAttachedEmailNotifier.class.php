94c7f63be4e950d26390dfea63d276ce61f72ce7
JB <jblemetayer@actualys.com>
2011-11-02 14:15:54 +0000

    Suite acExceptionNotifierPlugin

--- plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionAttachedEmailNotifier.class.php
+++ plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionAttachedEmailNotifier.class.php
@@ -4,9 +4,8 @@ class acExceptionAttachedEmailNotifier extends acExceptionNotifier
 {
 	public static function exceptionHandler(sfEvent $event)
 	{	
-		//!sfConfig::get('sf_debug') && 
-		if (is_object($exception = $event->getSubject())) {
-			self::notify(self::renderTraces(new acException($exception, sfConfig::get('app_ac_exception_notifier_class'))));
+		if (!sfConfig::get('sf_debug') && is_object($exception = $event->getSubject())) {
+			self::notify(self::renderTraces(new acException($exception, sfConfig::get('app_ac_exception_notifier_format'))));
 		}
 	}
 	protected static function notify($message)
