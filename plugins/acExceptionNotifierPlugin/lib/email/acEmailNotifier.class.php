e836483b0e78f444d3910ee6ded9691a5592097e
JB <jblemetayer@actualys.com>
2011-11-02 13:57:21 +0000

    Debug envoi de mail

--- plugins/acExceptionNotifierPlugin/lib/email/acEmailNotifier.class.php
+++ plugins/acExceptionNotifierPlugin/lib/email/acEmailNotifier.class.php
@@ -10,31 +10,26 @@
 class acEmailNotifier {
     
     public static function exceptionEmailNotifier($message) {
-        $from = array(sfConfig::get('app_ac_exception_notifier_email_from') => sfConfig::get('app_ac_exception_notifier_email_from_name'));
-        $to = array(sfConfig::get('app_ac_exception_notifier_email_to'));
-        $subject = sfConfig::get('app_ac_exception_notifier_email_subject');
-        $body = self::getBodyFromPartial('exception_email_notifier', array('message' => $message));
-        $email = self::getMailer()->compose($from, $to, $subject, $body)->setContentType('text/html');
-
+    	$emailInformations = sfConfig::get('app_ac_exception_notifier_email');
+        $from = array($emailInformations['from'] => $emailInformations['from_name']);
+        $to = array($emailInformations['to']);
+        $subject = $emailInformations['subject'];
+        $email = self::getMailer()->compose($from, $to, $subject, $message)->setContentType('text/html');
         return self::getMailer()->send($email);
     }
     
     public static function exceptionAttachedEmailNotifier($message) {
-        $from = array(sfConfig::get('app_ac_exception_notifier_email_from') => sfConfig::get('app_ac_exception_notifier_email_from_name'));
-        $to = array(sfConfig::get('app_ac_exception_notifier_email_to'));
-        $subject = sfConfig::get('app_ac_exception_notifier_email_subject');
-        $body = self::getBodyFromPartial('exception_attached_email_notifier', array());
-        $email = self::getMailer()->compose($from, $to, $subject, $body)->attach(new Swift_Attachment($message, 'exception-traces.html', 'text/html'))->setContentType('text/html');
+        $emailInformations = sfConfig::get('app_ac_exception_notifier_email');
+        $from = array($emailInformations['from'] => $emailInformations['from_name']);
+        $to = array($emailInformations['to']);
+        $subject = $emailInformations['subject'];
+        $email = self::getMailer()->compose($from, $to, $subject, '<p>Exception traces attached</p>')->attach(new Swift_Attachment($message, 'exception-traces.html', 'text/html'))->setContentType('text/html');
 
         return self::getMailer()->send($email);
     }
 
     protected static function getMailer() {
         return sfContext::getInstance()->getMailer();
-    }
-
-    protected static function getBodyFromPartial($partial, $vars = null) {
-        return sfContext::getInstance()->getController()->getAction('acEmailNotifier', 'main')->getPartial('acEmailNotifier/' . $partial, $vars);
     }
 
 }
