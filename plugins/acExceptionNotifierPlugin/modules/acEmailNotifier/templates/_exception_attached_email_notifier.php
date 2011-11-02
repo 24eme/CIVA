5d00e793b242b0b5a7796d88dea3cc3352665654
JB <jblemetayer@actualys.com>
2011-11-02 09:35:26 +0000

    Ajout des templates mails du plugin acExceptionNotifier

---
+++ plugins/acExceptionNotifierPlugin/modules/acEmailNotifier/templates/_exception_attached_email_notifier.php
@@ -0,0 +1,5 @@
+<?php echo include_partial('acEmailNotifier/headerMail') ?>
+
+<p>Exception traces is attached</p>
+
+<?php echo include_partial('acEmailNotifier/footerMail') ?>
---
+++ plugins/acExceptionNotifierPlugin/modules/acEmailNotifier/templates/_exception_email_notifier.php
@@ -0,0 +1,5 @@
+<?php echo include_partial('acEmailNotifier/headerMail') ?>
+
+<?php echo $message ?>
+
+<?php echo include_partial('acEmailNotifier/footerMail') ?>
