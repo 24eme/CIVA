e12b95949d20df469a446f5b6f92533f4c10259a
JB <jblemetayer@actualys.com>
2011-11-02 10:24:42 +0000

    Correction bug acExceptionNotifier

--- plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionNotifier.class.php
+++ plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionNotifier.class.php
@@ -11,9 +11,10 @@ abstract class acExceptionNotifier
 {
 	public static function exceptionHandler(sfEvent $event)
 	{	
-		//if (!sfConfig::get('sf_debug') && is_object($exception = $event->getSubject())) {
+		//!sfConfig::get('sf_debug') && 
+		if (is_object($exception = $event->getSubject())) {
 			self::notify(self::renderTraces(new acException($exception, sfConfig::get('app_ac_exception_notifier_class'))));
-		//}
+		}
 	}
 	protected static function renderTraces(acException $acException)
 	{
