ca40710efa1cb0ec1b37271172fd2cb564936030
JB <jblemetayer@actualys.com>
2011-11-02 11:13:02 +0000

    Debug acExceptionNotifierPlugin

--- plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionAttachedEmailNotifier.class.php
+++ plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionAttachedEmailNotifier.class.php
@@ -2,6 +2,13 @@
 
 class acExceptionAttachedEmailNotifier extends acExceptionNotifier
 {
+	public static function exceptionHandler(sfEvent $event)
+	{	
+		//!sfConfig::get('sf_debug') && 
+		if (is_object($exception = $event->getSubject())) {
+			self::notify(self::renderTraces(new acException($exception, sfConfig::get('app_ac_exception_notifier_class'))));
+		}
+	}
 	protected static function notify($message)
 	{
 		return acEmailNotifier::exceptionAttachedEmailNotifier($message);
--- plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionNotifier.class.php
+++ plugins/acExceptionNotifierPlugin/lib/notifier/acExceptionNotifier.class.php
@@ -9,13 +9,6 @@
  */
 abstract class acExceptionNotifier
 {
-	public static function exceptionHandler(sfEvent $event)
-	{	
-		//!sfConfig::get('sf_debug') && 
-		if (is_object($exception = $event->getSubject())) {
-			acExceptionEmailNotifier::notify(self::renderTraces(new acException($exception, sfConfig::get('app_ac_exception_notifier_class'))));
-		}
-	}
 	protected static function renderTraces(acException $acException)
 	{
 		$traces  = implode('<br />', $acException->getExceptionInformations());
