d48ad3beb2b266265a92b7e80bc95275afcc85cc
Vincent LAURENT <vlaurent@actualys.com>
2011-11-08 14:34:35 +0000

    Bug de récupération du model "Current"

--- lib/model/couchdb/Current/client/CurrentClient.class.php
+++ lib/model/couchdb/Current/client/CurrentClient.class.php
@@ -5,7 +5,7 @@ class CurrentClient extends sfCouchdbClient {
 
   public static function getCurrent() {
     if (self::$current == null) {
-        self::$current = CacheFunction::cache('model', array(sfCouchdbManager::getClient(), 'retrieveDocumentById'), array('CURRENT'));
+        self::$current = CacheFunction::cache('model', array(sfCouchdbManager::getClient("Current"), 'retrieveCurrent'), array());
     }
     return self::$current;
   }
