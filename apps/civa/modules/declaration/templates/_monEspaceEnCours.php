b49dfc5912c82496ac37a79adc35eb210717dbbe
Vincent LAURENT <vlaurent@actualys.com>
2011-11-29 17:28:27 +0000

    Démarrer une DR, changement des libellés et inversion de l'ordre

--- apps/civa/modules/declaration/templates/_monEspaceEnCours.php
+++ apps/civa/modules/declaration/templates/_monEspaceEnCours.php
@@ -1,7 +1,7 @@
 <form id="form_dr" action="<?php echo url_for('@declaration_init') ?>" method="post">
     <h3 class="titre_section">Déclaration de l'année<a href="" class="msg_aide" rel="help_popup_mon_espace_civa_ma_dr" title="Message aide"></a></h3>
     <div class="contenu_section">
-        <p class="intro">Vous souhaitez faire une nouvelle déclaration :</p>
+        <p class="intro">Vous souhaitez :</p>
         <?php if (!$declaration->isNew()): ?>
             <div class="ligne_form">
                 <input type="radio" id="type_declaration_brouillon" name="dr[type_declaration]" value="brouillon" checked="checked" />
@@ -12,19 +12,21 @@
                 <label for="type_declaration_suppr">Supprimer ma déclaration <?php echo $sf_user->getCampagne() ?> en cours</label>
             </div>
         <?php else: ?>
+            <?php if ($has_import) : ?>
+                <div class="ligne_form">
+                    <input type="radio" id="type_declaration_import" name="dr[type_declaration]" value="import" checked="checked" />
+                    <label for="type_declaration_import">Visualiser, compléter et valider les données fournies par vos acheteurs (négociants ou caves coopératives)</label>
+                </div>
+            <?php endif; ?>
             <div class="ligne_form">
-                <input type="radio" id="type_declaration_vierge" name="dr[type_declaration]" value="vierge" checked="checked" />
-                <label for="type_declaration_vierge">A partir d'une déclaration vierge</label>
+                <input type="radio" id="type_declaration_vierge" name="dr[type_declaration]" value="vierge" <?php if(!$has_import): ?>checked="checked"<?php endif; ?> />
+                <label for="type_declaration_vierge">Démarrer à partir d'une déclaration vierge</label>
             </div>
-   <?php if ($has_import) : ?>
-            <div class="ligne_form">
-                    <input type="radio" id="type_declaration_import" name="dr[type_declaration]" value="import" />
-                    <label for="type_declaration_import">A partir des informations des coopératives ou acheteurs</label>
-            </div>
-   <?php endif; if (count($campagnes) > 0): ?>
+
+            <?php if (count($campagnes) > 0): ?>
                 <div class="ligne_form">
                     <input type="radio" id="type_declaration_precedente" name="dr[type_declaration]" value="precedente" />
-                    <label for="type_declaration_precedente">A partir d'une précédente déclaration</label>
+                    <label for="type_declaration_precedente">Démarrer à partir d'une déclaration d'un année précédente</label>
                 </div>
                 <div class="ligne_form ligne_btn">
                     <select id="liste_precedentes_declarations" name="dr[liste_precedentes_declarations]">
