6e46cdfd511647442419e3ab6fd56be43064ad9d
Vincent LAURENT <vlaurent@actualys.com>
2011-12-12 18:39:08 +0000

    Mon espace bloc "declaration terminée" on affiche pas le bouton visualiser si pas de DR

--- apps/civa/modules/declaration/templates/_monEspaceNonEditable.php
+++ apps/civa/modules/declaration/templates/_monEspaceNonEditable.php
@@ -2,7 +2,9 @@
 <div class="contenu_section">
     <p class="intro"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('intro_mon_espace_civa_dr_non_editable'); ?></p>
     <div class="ligne_form ligne_btn">
-        <?php echo link_to('<img src="/images/boutons/btn_visualiser.png" alt="" class="btn" />', '@visualisation?annee=' . $sf_user->getCampagne()); ?>
+        <?php if($sf_user->getDeclaration()->isValideeCiva()): ?>
+            <?php echo link_to('<img src="/images/boutons/btn_visualiser.png" alt="" class="btn" />', '@visualisation?annee=' . $sf_user->getCampagne()); ?>
+        <?php endif; ?>
         <?php if ($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
             <a href="<?php echo url_for('@declaration_invalider_civa') ?>" onclick="return confirm('Si vous éditez cette DR, pensez à la revalider.')"><img src="/images/boutons/btn_editer_dr.png" alt="" class="btn" id="rendreEditable"  /></a>
             <a href="<?php echo url_for('@declaration_invalider_recoltant') ?>" onclick="return confirm('Etes-vous sûr de vouloir dévalider cette DR ?')"><img src="/images/boutons/btn_devalider_dr.png" alt="" class="btn" id=""  /></a>
