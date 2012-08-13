ca5fcd714ca281305fa423b3dee2d464da933d49
Cédric Mefferte <cmefferte@actualys.com>
2012-08-13 18:09:29 +0200

    Ajout du fichier unavailable dans la config generale du projet

---
+++ project/config/unavailable.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate, post-check=0, pre-check=0" />
+    <meta http-equiv="Expires" content="0" />
+    <meta name="title" content="Espace des professionnels du Vignoble d’Alsace" />
+    <meta name="pragma" content="no-cache" />
+    <title>
+        Espace des professionnels du Vignoble d’Alsace      </title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/global.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/declaration_recolte.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/jquery.ui.css" />
+</head>
+<body id="declaration_recolte">
+<!-- #global -->
+<div id="global">
+    <!--<ul id="liens_evitement" class="clearfix">
+    </ul>-->
+
+    <!-- #header -->
+    <div id="header" class="clearfix pngfix">
+        <h1 id="logo"><a href="#" title="CIVA - Conseil Interprofessionnel des Vins d'Alsace "><img src="/images/visuels/logo_civa.png" alt="CIVA - Conseil Interprofessionnel des Vins d'Alsace" /></a></h1>
+
+        <div id="titre_rubrique">
+
+            <h1>
+                Espace des professionnels du Vignoble d’Alsace                    </h1>
+
+        </div>
+    </div>
+    <!-- fin #header -->                     <div id="contenu">
+
+    <div id="application_dr" class="clearfix">
+        <div id="espace_alsace_recolte">
+            <h2>Ce site est temporairement inaccessible</h2>
+<p>En raison d'une migration serveur, les services vinsalsace.pro sont indisponibles, ils seront de retour dans quelques heures.</p><p>Veuillez nous excuser pour la gêne occasionnée.</p>
+            </div>
+
+    </div>
+    <!-- fin #application_dr -->
+
+</div>
+    <!-- #footer -->
+    <div id="footer">
+        <p>&copy; CIVA 2010</p>
+
+    </div>
+    <!-- fin #footer -->
+</div>
+<!-- fin #global -->
+
+
+<![endif]-->
+
+</body>
+</html>
