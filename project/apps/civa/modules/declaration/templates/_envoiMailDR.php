63a02018ac252747c12602bb79d672ef47cdee60
Cédric Mefferte <cmefferte@actualys.com>
2012-08-20 10:37:14 +0200

    ajout message confirmation pr l envoi de mail dans la visu + fix sur la requete ajax de la generation de pdf =

--- project/apps/civa/modules/declaration/templates/_envoiMailDR.php
+++ project/apps/civa/modules/declaration/templates/_envoiMailDR.php
@@ -1,8 +1,39 @@
-<script>
-    ajax_url_to_send_email_pdf = "<?php echo url_for('@send_email_pdf?annee='.$annee.'&message=custom'); ?>";
-</script>
+<script type="text/javascript">
+    ajax_url_to_confirmation = "<?php echo url_for('declaration/confirmationMailDR'); ?>";
+    ajax_url_to_process = "<?php echo url_for('@send_email_pdf?annee='.$annee.'&message=custom'); ?>;"
+
+    var sendMail = function(){
+            openPopup($('#popup_loader_send'));
+            $.ajax({
+                url: ajax_url_to_process ,
+                success: function(data) {
+                    $('.popup-loading').empty();
+                    $('.popup-loading').css('background', 'none');
+                    $('.popup-loading').css('padding-top', '10px');
+                    $('.popup-loading').append('<p>' + data + '</p>');
+                    $('#popup_confirme_mail').dialog('close');
+
+                }
+            });
+        };
+    </script>
+
+<div id="popup_confirme_mail" class="popup_ajout" title="Confirmation d'envoi de mail de votre DR">
+    <form method="post" action="">
+        <p>
+            Confirmez vous l'envoi par mail de votre déclaration de récolte ? <br />
+        </p>
+        <div id="btns">
+            <br /s>
+            <input onclick="sendMail()" type="image" src="/images/boutons/btn_valider.png" alt="Valider votre déclaration" name="boutons[next]" id="valideDR" class="valideDR_OK" />
+            <a class="close_popup" href=""  onclick="$('#popup_confirmation').dialog('close');" ><img alt="Annuler" src="/images/boutons/btn_annuler.png"></a>
+        </div>
+    </form>
+</div>
+
+
 <div style="display: none" id="popup_loader_send" title="Envoie du PDF">
     <div class="popup-loading">
-        <p>L'envoi par e-mail de votre PDF est en cours.<br/>Merci de patienter.<br/><small>La procédure peut prendre 30 secondes</small></p>
+        <p>L'envoi par e-mail de votre DR est en cours.<br/>Merci de patienter.<br/><small>La procédure peut prendre 30 secondes</small></p>
     </div>
 </div>
