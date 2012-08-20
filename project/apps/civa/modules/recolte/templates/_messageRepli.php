12d62c33d8bf15062989c0733d21a6753e07d683
Cédric Mefferte <cmefferte@actualys.com>
2012-08-20 10:32:49 +0200

    ajout du message concernant le volume pouvant etre replié

--- project/apps/civa/modules/recolte/templates/_messageRepli.php
+++ project/apps/civa/modules/recolte/templates/_messageRepli.php
@@ -1,7 +1,20 @@
-<?php if( $onglets->getCurrentCouleur()->dplc > 0 && $onglets->getCurrentCepage()->libelle != 'Rebêches' ) : ?>
+<?php
 
-<div id="message_repli">
-  <p>Vous pouvez replier jusqu'à <?php echo $onglets->getCurrentCouleur()->dplc ?> hl en assemblage pour <?php echo $onglets->getCurrentLieu()->getParent()->getConfig()->libelle.' '.$onglets->getCurrentCouleur()->getConfig()->libelle; ?></p>
-</div>
+$cepage_assemblage = false;
+foreach ( $onglets->getCurrentCepage()->getConfig()->getParent()->filter('^cepage') as $cepage ) {
+    if( $cepage->key == 'cepage_ED' )
+        $cepage_assemblage = true;
+}
 
-<?php endif ;?>
+if ( $onglets->getCurrentCepage()->dplc > 0
+  && $cepage_assemblage == true
+  && $onglets->getCurrentCepage()->getConfig()->key != 'cepage_ED' ) : ?>
+
+    <fieldset class='message' id="message_repli">
+      <legend class="message_title">
+         Information
+      </legend>
+      <p>Vous pouvez replier jusqu'à <?php echo $onglets->getCurrentCouleur()->dplc ?> hl en assemblage pour <?php echo $onglets->getCurrentLieu()->getParent()->getConfig()->libelle.' '.$onglets->getCurrentCepage()->getConfig()->libelle; ?></p>
+    </fieldset>
+
+<?php endif; ?>
