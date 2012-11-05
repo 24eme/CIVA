7dbbfed9f0338e960b7711a0b6f6d8cf5b4d920e
drikos <drikos@drikos-XPS-L501X>
2012-11-05 08:03:44 +0100

    maj template ntoice evolutions

--- project/apps/civa/modules/tiers/templates/noticeEvolutionsSuccess.php
+++ project/apps/civa/modules/tiers/templates/noticeEvolutionsSuccess.php
@@ -1,6 +1,6 @@
-<div class="pages-statiques" id="notice_evolutions">
+<h2 class="titre_principal">DECLARATION DE RECOLTE 2012 – LES NOUVEAUTES</h2>
+<div id="notice_evolutions"><?php echo sfCouchdbManager::getClient('Messages')->getMessage('notice_evolutions_2012'); ?>
 
-   <?php echo sfCouchdbManager::getClient('Messages')->getMessage('notice_evolutions_2012'); ?>
-
-    <a href="<?php echo url_for('@mon_espace_civa') ?>"><img src="/images/boutons/btn_retourner_mon_espace.png" alt="Retourner à mon espace CIVA" /></img></a>
-</div>
+<a href="<?php echo url_for('@mon_espace_civa') ?>"><img
+	src="/images/boutons/btn_retourner_mon_espace.png"
+	alt="Retourner à mon espace CIVA" /></img></a></div>
