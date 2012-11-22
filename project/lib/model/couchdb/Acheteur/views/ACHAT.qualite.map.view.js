ac18453c27fc99eb3a2954ecc79b3700a416292a
Vincent LAURENT <vlaurent@actualys.com>
2012-11-22 19:56:41 +0100

    - Vue des caves et négoces excluant en mode dégueu 3 caves

--- project/lib/model/couchdb/Acheteur/views/ACHAT.qualite.map.view.js
+++ project/lib/model/couchdb/Acheteur/views/ACHAT.qualite.map.view.js
@@ -1,1 +1,9 @@
-function(doc) { if (doc['type'] == 'Acheteur') { emit([doc.qualite, doc.nom], {cvi: doc.cvi, nom: doc.nom, commune: doc.commune});}}
+function(doc) { 
+    if (doc['type'] == 'Acheteur') { 
+        if (doc.cvi == '6702108000' || doc.cvi == '6734808000' || doc.cvi == '6831050010' || doc.cvi == '7523700888') {
+
+            return;
+        }
+        emit([doc.qualite, doc.nom], {cvi: doc.cvi, nom: doc.nom, commune: doc.commune});
+    }
+}
