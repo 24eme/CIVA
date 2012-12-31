f1ccb6ad36d7b00ed7e154c9e0faeb9b3faa592c
Vincent LAURENT <vince.laurent@gmail.com>
2012-12-31 17:39:51 +0100

    - mise à jour de la vue stats de recolte puis désactivation car trop   gourmande

--- project/lib/model/couchdb/Stats/views/STATS.RECOLTE.map.view.js
+++ project/lib/model/couchdb/Stats/views/STATS.RECOLTE.map.view.js
@@ -1,43 +1,55 @@
 function(doc) {
-  return; /* à adapter */
+    
+    return; // Car trop gourmande
+
+    if (!(doc.type && doc.type == "DR" && doc.cvi.match('^(67|68)') && doc.validee && doc.modifiee )) {
+        return;
+    } 
 
 
-  if (!(doc.type && doc.type == "DR" && doc.cvi.match('^(67|68)') && doc.validee && doc.modifiee )) {
-  return;
-  }
-
-  for(appellation_key in doc.recolte) {
- if(appellation_key.match('^appellation')) {
-   var appellation_dplc = 0;
-   var appellation_volume_revendique = 0;
-    for(lieu_key in doc.recolte[appellation_key]) {
-     if(lieu_key.match('^lieu')) {
-       appellation_dplc += doc.recolte[appellation_key][lieu_key].dplc;
-        appellation_volume_revendique += doc.recolte[appellation_key][lieu_key].volume_revendique;
-        for(couleur_key in doc.recolte[appellation_key][lieu_key]) {
-          if(couleur_key.match('^couleur')) {
-           for(cepage_key in doc.recolte[appellation_key][lieu_key][couleur_key]) {
-              if(cepage_key.match('^cepage')) {
-               emit([doc.campagne, appellation_key, cepage_key], 
-                     {
-                       "nb_declarations_cepage": 0,
-                      "total_volume": doc.recolte[appellation_key][lieu_key][couleur_key][cepage_key].total_volume,
-                       "total_superficie": doc.recolte[appellation_key][lieu_key][couleur_key][cepage_key].total_superficie,  
-                     });
-              }
-           }
-         }
-       }
-     }
-   }
-   emit([doc.campagne, appellation_key, null], 
-        {
-         "nb_declarations_appellation": 0,
-         "dplc": appellation_dplc,
-         "volume_revendique": appellation_volume_revendique 
-        });
- }
-  }
+    var genre = doc.recolte.certification.genre;
+    for(appellation_key in genre) {
+        if(appellation_key.match('^appellation')) {
+            var appellation = genre[appellation_key];
+            var appellation_dplc = 0;
+            var appellation_volume_revendique = 0;
+            var appellation_usages_industriels = 0;
+            var appellation_usages_industriels_saisi = 0;
+            for(lieu_key in appellation.mention) {
+                if(lieu_key.match('^lieu')) {
+                    var lieu = appellation.mention[lieu_key];
+                    appellation_dplc += lieu.dplc;
+                    appellation_volume_revendique += lieu.volume_revendique;
+                    appellation_usages_industriels += lieu.usages_industriels_calcule;
+                    appellation_usages_industriels_saisi += lieu.usages_industriels_saisi;
+                    for(couleur_key in lieu) {
+                        if(couleur_key.match('^couleur')) {
+                            var couleur = lieu[couleur_key];
+                            for(cepage_key in couleur) {
+                                if(cepage_key.match('^cepage')) {
+                                    var cepage = couleur[cepage_key];
+                                    emit([doc.campagne, appellation_key, cepage_key], 
+                                    {
+                                     "nb_declarations_cepage": 0,
+                                     "total_volume": cepage.total_volume,
+                                     "total_superficie": cepage.total_superficie,  
+                                   });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            emit([doc.campagne, appellation_key, null], 
+            {
+             "nb_declarations_appellation": 0,
+             "dplc": appellation_dplc,
+             "usages_industriels": appellation_volume_revendique,
+             "usages_industriels_saisi": appellation_usages_industriels_saisi,
+             "volume_revendique": appellation_volume_revendique 
+            });
+        }
+    }
 
 
    emit([doc.campagne, null, null], 
--- project/lib/model/couchdb/Stats/views/STATS.RECOLTE.reduce.view.js
+++ project/lib/model/couchdb/Stats/views/STATS.RECOLTE.reduce.view.js
@@ -1,32 +1,39 @@
 function (keys, values, rereduce) {
 
   var total_volume = 0;
- var total_superficie = 0;
- var dplc = 0;
- var volume_revendique = 0;
+  var total_superficie = 0;
+  var dplc = 0;
+  var usages_industriels = 0;
+  var usages_industriels_saisi = 0;
+  var volume_revendique = 0;
   var nb_declarations = 0;
   var nb_declarations_appellation = 0;
   var nb_declarations_cepage = 0;
 
  //var key_ask = keys[0][0];
 
- for(item in values) {
-   if (values[item]['total_volume']) {
-     total_volume += values[item]['total_volume'];
-   }
-   if (values[item]['total_superficie']) {
-     total_superficie += values[item]['total_superficie'];
-   }
-   if (values[item]['dplc']) {
-     dplc += values[item]['dplc'];
-   }
-   if (values[item]['volume_revendique']) {
+  for(item in values) {
+    if (values[item]['total_volume']) {
+      total_volume += values[item]['total_volume'];
+    }
+    if (values[item]['total_superficie']) {
+      total_superficie += values[item]['total_superficie'];
+    }
+    if (values[item]['dplc']) {
+      dplc += values[item]['dplc'];
+    }
+    if (values[item]['usages_industriels']) {
+      usages_industriels += values[item]['usages_industriels'];
+    }
+    if (values[item]['usages_industriels_saisi']) {
+      usages_industriels_saisi += values[item]['usages_industriels_saisi'];
+    }
+    if (values[item]['volume_revendique']) {
       volume_revendique += values[item]['volume_revendique'];
-   }
-
-   if (values[item]['nb_declarations']) {
+    }
+    if (values[item]['nb_declarations']) {
       nb_declarations += values[item]['nb_declarations'];
-   }
+    } 
 
    /*if (values[item]['nb_declarations']) {
       nb_declarations += values[item]['nb_declarations'];
@@ -41,9 +48,11 @@ function (keys, values, rereduce) {
    }*/ 
   }
 
- total_volume = Math.round(total_volume*100)/100;
+  total_volume = Math.round(total_volume*100)/100;
   total_superficie = Math.round(total_superficie*100)/100;
   dplc = Math.round(dplc*100)/100;
+  usages_industriels = Math.round(usages_industriels*100)/100;
+  usages_industriels_saisi = Math.round(usages_industriels_saisi*100)/100;
   volume_revendique = Math.round(volume_revendique*100)/100;
 
   /*if (key_ask[0] == null || key_ask[1] == null) {
@@ -56,5 +65,5 @@ function (keys, values, rereduce) {
     return "nop";
  }*/
 
- return {"nb_declarations": nb_declarations, "total_volume": total_volume, "total_superficie": total_superficie, "dplc": dplc, "volume_revendique": volume_revendique};
+ return {"nb_declarations": nb_declarations, "total_volume": total_volume, "total_superficie": total_superficie, "dplc": dplc, "usages_industriels": usages_industriels, "usages_industriels_saisi": usages_industriels_saisi, "volume_revendique": volume_revendique};
 }
