c11af5d6a7baa545b7e733eb1cefb8246157c923
mathurin <mpetit@actualys.com>
2013-04-23 19:13:47 +0200

    mis en place d'un fct recuperant les prod jusqu'au noeud cepage (affiner a posteriori pour atteindre les noeuds details)

--- project/lib/model/couchdb/DR/DR.class.php
+++ project/lib/model/couchdb/DR/DR.class.php
@@ -1,5 +1,5 @@
 <?php
-class DR extends BaseDR {
+class DR extends BaseDR implements InterfaceProduitsDocument {
     const ETAPE_EXPLOITATION = 'exploitation';
     const ETAPE_RECOLTE = 'recolte';
     const ETAPE_VALIDATION = 'validation';
@@ -444,6 +444,26 @@ class DR extends BaseDR {
             return min($arr_date);
         }else
             return null;
+    }
+    
+
+    public function getProduits(){
+        $produits = array();          
+        foreach ($this->recolte->getAppellations() as $appelation) {
+            foreach ($appelation->getMentions() as $mention) {
+                //$libelles[] = $appelation->getMentions()->appellation;
+                foreach ($mention->getLieux() as $lieu) {
+                    foreach ($lieu->getCouleurs() as $couleur) {
+                        foreach ($couleur->getCepages() as $cepage) {
+                                $produits[] = $cepage;                            
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+        return $produits;
     }
 
 }
