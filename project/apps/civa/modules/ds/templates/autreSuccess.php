d4c11fd34a5e82d43168cd1d8dc8dff5633d58f2
Hamza Iqbal <hiqbal@actualys.com>
2013-06-07 15:09:10 +0200

    Tickets DS intégration

--- project/apps/civa/modules/ds/templates/autreSuccess.php
+++ project/apps/civa/modules/ds/templates/autreSuccess.php
@@ -6,41 +6,43 @@ echo $form->renderGlobalErrors();
 include_partial('dsRailEtapes',array('tiers' => $tiers, 'ds' => $ds, 'etape' => 4));
 ?>
 
-<h2 class="titre_page">Autres produits : saisissez ici vos rebêche, dépassements de rendement, lies et moûts <br /><small>(tous lieux de stockage confondus)</small></h2>
+<ul id="onglets_majeurs" class="clearfix">
+	<li class="ui-tabs-selected"><a href="#exploitation_administratif">Autres Produits</a></li>
+</ul>
+
 <!-- #application_ds --></h2>
 <div id="ajax_error"></div>
 	<div id="application_ds" class="clearfix">
-		<div class="bloc_autres">
-		<h2 class="titre_page">Autres Produits</h2>
-		<!-- #gestion_stock -->
-		<div id="gestion_stock" class="clearfix gestion_stock_donnees">
-			<ul id="liste_cepages">
-				<li><?php echo $form['mouts']->renderLabel() ?></li>
-				<li><?php echo $form['rebeches']->renderLabel() ?></li>
-				<li><?php echo $form['dplc']->renderLabel() ?></li>
-				<li><?php echo $form['lies']->renderLabel() ?></li>
-			</ul>
-			
-			<div id="donnees_stock_cepage">
-				<div id="col_hl" class="colonne">
-					<h2>hl</h2>
+		<p class="intro_declaration">Saisissez ici vos rebêche, dépassements de rendement, lies et moûts <small>(tous lieux de stockage confondus)</small></p>
 	
-					<div class="col_cont">
-						<ul>
-							<li><?php echo $form['mouts']->render(array('class' => 'num')) ?></li>
-							<li><?php echo $form['rebeches']->render(array('class' => 'num')) ?></li>
-							<li><?php echo $form['dplc']->render(array('class' => 'num')) ?></li>
-							<li><?php echo $form['lies']->render(array('class' => 'num')) ?></li>
-						</ul>
+		<div class="bloc_autres">
+			<!-- #gestion_stock -->
+			<div id="gestion_stock" class="clearfix gestion_stock_donnees">
+				<ul id="liste_cepages">
+					<li><?php echo $form['mouts']->renderLabel() ?></li>
+					<li><?php echo $form['rebeches']->renderLabel() ?></li>
+					<li><?php echo $form['dplc']->renderLabel() ?></li>
+					<li><?php echo $form['lies']->renderLabel() ?></li>
+				</ul>
+				
+				<div id="donnees_stock_cepage">
+					<div id="col_hl" class="colonne">
+						<h2>hl</h2>
+		
+						<div class="col_cont">
+							<ul>
+								<li><?php echo $form['mouts']->render(array('class' => 'num')) ?></li>
+								<li><?php echo $form['rebeches']->render(array('class' => 'num')) ?></li>
+								<li><?php echo $form['dplc']->render(array('class' => 'num')) ?></li>
+								<li><?php echo $form['lies']->render(array('class' => 'num')) ?></li>
+							</ul>
+						</div>
 					</div>
 				</div>
+				
 			</div>
-			
+			<!-- fin #gestion_stock -->
 		</div>
-		<!-- fin #gestion_stock -->
-		
-		
-	</div>
 	</div>
 	<!-- fin #application_ds -->
 	
