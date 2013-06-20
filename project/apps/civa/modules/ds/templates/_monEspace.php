6f8a046592b6812168b84429b8223c5ca0bcff63
mathurin <mpetit@actualys.com>
2013-06-20 18:45:19 +0200

    mis en place des template de "pre-ds" et "post-ds" l'ordre des bloque est parametré par le champs "declaration_courante"

--- project/apps/civa/modules/ds/templates/_monEspace.php
+++ project/apps/civa/modules/ds/templates/_monEspace.php
@@ -1,14 +1,13 @@
 <div id="nouvelle_declaration">
-<?php if(!$ds->isValidee()):
+<?php 
+if(!$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && (CurrentClient::getCurrent()->exist('ds_non_ouverte') && CurrentClient::getCurrent()->ds_non_ouverte == 1)): 
+    include_partial('ds/monEspaceNonOuvert');
+    elseif(!$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && (CurrentClient::getCurrent()->exist('ds_non_editable') && CurrentClient::getCurrent()->ds_non_editable == 1)): 
+        include_partial('ds/monEspaceNonEditable',array('ds' => $ds));
+    elseif(!$ds->isValidee()):
         include_component('ds', 'monEspaceEnCours');
     else:
         include_component('ds', 'monEspaceValidee'); 
     endif; 
 ?>
-        <?php //include_partial('ds/monEspaceValidee') ?>
-<?php //if(CurrentClient::getCurrent()->exist('ds_non_ouverte') && CurrentClient::getCurrent()->dr_non_ouverte == 1): ?>
-    <?php //include_partial('ds/monEspaceNonOuverte') ?>
-<?php //else: ?>
-    <?php //include_partial('ds/monEspaceNonEditable') ?>
-<?php //endif; ?>
 </div>
---
+++ project/apps/civa/modules/ds/templates/_monEspaceNonEditable.php
@@ -0,0 +1,13 @@
+<h3 class="titre_section">Déclaration de l'année <a href="" class="msg_aide" rel="help_popup_mon_espace_civa_ma_ds" title="Message aide"></a></h3>
+<div class="contenu_section">
+    <p class="intro"><?php echo acCouchdbManager::getClient('Messages')->getMessage('intro_mon_espace_civa_ds_non_editable'); ?></p>
+    <div class="ligne_form ligne_btn">
+        <?php if($sf_user->getDeclaration()->isValideeCiva()): ?>
+            <?php echo link_to('<img src="/images/boutons/btn_visualiser.png" alt="" class="btn" />', 'ds_visualisation',$ds); ?>
+        <?php endif; ?>
+        <?php if ($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
+            <a href="<?php echo url_for('ds_invalider_civa',$ds) ?>" onclick="return confirm('Si vous éditez cette DS, pensez à la revalider.')"><img src="/images/boutons/btn_editer_ds.png" alt="" class="btn" id="rendreEditable"  /></a>
+            <a href="<?php echo url_for('ds_invalider_recoltant',$ds) ?>" onclick="return confirm('Etes-vous sûr de vouloir dévalider cette DS ?')"><img src="/images/boutons/btn_devalider_ds.png" alt="" class="btn" id=""  /></a>
+        <?php endif; ?>
+    </div>
+</div>
