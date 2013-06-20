7e841632c97026d16fbb4b645360b91201eda316
mathurin <mpetit@actualys.com>
2013-06-20 18:55:59 +0200

    on affiche pas "recevoir email" si la ds n'est pas validee

--- project/apps/civa/modules/ds/templates/visualisationSuccess.php
+++ project/apps/civa/modules/ds/templates/visualisationSuccess.php
@@ -9,11 +9,13 @@ include_partial('recapitulatifDs', array('ds_principale' => $ds_principale, 'ds_
 			<img src="/images/boutons/btn_retourner_mon_espace.png" alt="Retourner à l'étape précédente" />
 		</a>
 	</li>
+        <?php if($ds_principale->isValidee()): ?>
 	<li class="suiv">
             <a href="<?php echo url_for('ds_export_pdf', $ds_principale);?>">
 		<input type="image" src="/images/boutons/btn-recevoir-ma-ds-par-email.png" alt="Recevoir ma ds par email" name="boutons[recevoir]" id="recevoir">
             </a>
 	</li>
+        <?php endif; ?>
 	<li class="previsualiser">
             <a href="<?php echo url_for('ds_export_pdf', $ds_principale);?>">
 		<input type="image" src="/images/boutons/btn_previsualiser.png" alt="Prévisualiser" name="boutons[previsualiser]" id="previsualiser">
