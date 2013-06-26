2fa333de6788be6445cfa60765e70187d232525a
mathurin <mpetit@actualys.com>
2013-06-26 16:36:22 +0200

    alsace récolte avec un grand "R"

--- project/apps/civa/modules/tiers/templates/_monEspaceDr.php
+++ project/apps/civa/modules/tiers/templates/_monEspaceDr.php
@@ -1,6 +1,6 @@
 <?php if($sf_user->hasCredential(myUser::CREDENTIAL_DECLARATION)): ?>
         <div id="espace_alsace_recolte">
-            <h2>Alsace récolte</h2>
+            <h2>Alsace Récolte</h2>
             <div class="contenu clearfix">  
                  <?php include_component('declaration', 'monEspace') ?>
                  <?php include_component('declaration', 'monEspaceColonne') ?>
