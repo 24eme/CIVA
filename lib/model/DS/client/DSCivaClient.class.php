05c47e41a883258c9ffd9139eb68d73d17f08321
mathurin <mpetit@actualys.com>
2013-07-01 19:31:03 +0200

    enlevage d'un exit

--- lib/model/DS/client/DSCivaClient.class.php
+++ lib/model/DS/client/DSCivaClient.class.php
@@ -400,7 +400,6 @@ class DSCivaClient extends DSClient {
             if($current_ds->hasNoAppellation() && !$current_ds->isDsPrincipale()){
                 $num_etape = $ds->getNumEtapeAbsolu();
                 $this->delete($current_ds);
-                var_dump($num_etape); exit;
                 $ds->updateEtape($num_etape);
                 $ds->save();
                 $deleted = true;
