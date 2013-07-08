2dda778f7bc71713d328dc317fbe6046c6b48feb
Vincent LAURENT <vlaurent@actualys.com>
2013-07-08 10:49:32 +0200

    Mot de passe minimum 98 caractères

--- project/lib/form/compte/CompteForm.class.php
+++ project/lib/form/compte/CompteForm.class.php
@@ -44,7 +44,7 @@ abstract class CompteForm extends BaseForm {
 
         $this->setValidators(array(
                 'email' => new sfValidatorEmail(array('required' => true),array('required' => 'Champ obligatoire', 'invalid' => 'Adresse email invalide.')),
-                'mdp1'  => new sfValidatorString(array('required' => true, "min_length" => "4"), array('required' => 'Champ obligatoire', "min_length" => "Votre mot de passe doit comporter au minimum 4 caractères.")),
+                'mdp1'  => new sfValidatorString(array('required' => true, "min_length" => "8"), array('required' => 'Champ obligatoire', "min_length" => "Votre mot de passe doit comporter au minimum 8 caractères.")),
                 'mdp2'  => new sfValidatorString(array('required' => true), array('required' => 'Champ obligatoire')),
         ));
         //$this->validatorSchema->setPostValidator(new ValidatorCreateCompte());
