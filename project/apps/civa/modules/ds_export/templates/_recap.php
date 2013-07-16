ca80eae4dd6ecc480f2bb4caa98a54e42b3448b5
Vincent LAURENT <vlaurent@actualys.com>
2013-07-16 11:32:02 +0200

    PDF Correction de largeur de texte et truncate text suite à l'augmentation de la police de caractère

--- project/apps/civa/modules/ds_export/templates/_recap.php
+++ project/apps/civa/modules/ds_export/templates/_recap.php
@@ -5,20 +5,20 @@
 <br /><br />
 <table border="1" cellspacing=0 cellpadding=0 style="text-align: right; border: 1px solid black;">
 <tr>
-  <th style="width: 198px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: left;">Appellations</th>
-  <th style="width: 110px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">Total</th>
-  <th style="width: 110px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">hors VT et SGN</th>
-  <th style="width: 110px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">VT</th>
-  <th style="width: 110px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">SGN</th>
+  <th style="width: 214px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: left;">Appellations</th>
+  <th style="width: 106px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">Total</th>
+  <th style="width: 106px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">hors VT et SGN</th>
+  <th style="width: 106px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">VT</th>
+  <th style="width: 106px; font-weight: bold; border: 1px solid black; background-color: black; color: white; text-align: center;">SGN</th>
 </tr>
 <?php foreach($recap_total as $item): ?>
 <tr>
-  <td style="text-align: left; width: 198px; border: 1px solid black; font-weight: bold;">&nbsp;<?php echo str_replace("TOTAL ", "", $item->nom) ?></td>
+  <td style="text-align: left; width: 214px; border: 1px solid black; font-weight: bold;">&nbsp;<?php echo str_replace("TOTAL ", "", $item->nom) ?></td>
   <?php if(!is_null($item->volume_total)): ?>
-  <td style="width: 110px; border: 1px solid black;"><?php echoVolume($item->volume_total, true) ?></td>
-  <td style="width: 110px; border: 1px solid black; <?php if(is_null($item->volume_normal)): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($item->volume_normal, true) ?></td>
-  <td style="width: 110px; border: 1px solid black; <?php if(is_null($item->volume_vt)): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($item->volume_vt, true) ?></td>
-  <td style="width: 110px; border: 1px solid black; <?php if(is_null($item->volume_sgn)): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($item->volume_sgn, true) ?></td>
+  <td style="width: 106px; border: 1px solid black;"><?php echoVolume($item->volume_total, true) ?></td>
+  <td style="width: 106px; border: 1px solid black; <?php if(is_null($item->volume_normal)): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($item->volume_normal, true) ?></td>
+  <td style="width: 106px; border: 1px solid black; <?php if(is_null($item->volume_vt)): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($item->volume_vt, true) ?></td>
+  <td style="width: 106px; border: 1px solid black; <?php if(is_null($item->volume_sgn)): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($item->volume_sgn, true) ?></td>
   <?php else: ?>
   <td style="border: 1px solid black; text-align: center;" colspan="4"><i>Néant</i></td>
   <?php endif; ?>
@@ -29,12 +29,12 @@
 <small><br /></small>
 <table border="1" cellspacing=0 cellpadding=0 style="text-align: right; border: 1px solid black;">
 <tr>
-  <td style="text-align: left; width: 198px; border: 1px solid black; font-weight: bold; background-color: black; color: white;">&nbsp;Total Général AOC</td>
+  <td style="text-align: left; width: 214px; border: 1px solid black; font-weight: bold; background-color: black; color: white;">&nbsp;Total Général AOC</td>
   <?php $volume_total_aoc = DSCivaClient::getInstance()->getTotalAOC($ds); ?>
   <?php if($volume_total_aoc > 0): ?>
-    <td style="width: 110px; border: 1px solid black;"><?php echoVolume(DSCivaClient::getInstance()->getTotalAOC($ds), true) ?></td>
+    <td style="width: 106px; border: 1px solid black;"><?php echoVolume(DSCivaClient::getInstance()->getTotalAOC($ds), true) ?></td>
   <?php else: ?>
-    <td style="width: 110px; border: 1px solid black; text-align:center; font-weight: bold;"><i>Néant</i></td>
+    <td style="width: 106px; border: 1px solid black; text-align:center; font-weight: bold;"><i>Néant</i></td>
   <?php endif; ?>
 </tr>
 </table>
@@ -42,8 +42,8 @@
 <table border="1" cellspacing=0 cellpadding=0 style="text-align: right; border: 1px solid black;">
 <?php foreach($recap_autres as $libelle => $volume): ?>
 <tr>
-  <td style="text-align: left; width: 306px; border: 1px solid black; font-weight: bold;">&nbsp;<?php echo $libelle ?></td>
-  <td style="width: 110px; border: 1px solid black;"><?php echoVolume($volume, true) ?></td>
+  <td style="text-align: left; width: 214px; border: 1px solid black; font-weight: bold;">&nbsp;<?php echo $libelle ?></td>
+  <td style="width: 106px; border: 1px solid black;"><?php echoVolume($volume, true) ?></td>
 </tr>
 <?php endforeach; ?>
 </table>
@@ -52,8 +52,8 @@
 <table border="1" cellspacing=0 cellpadding=0 style="text-align: right; border: 1px solid black;">
 <?php foreach($recap_vins_sans_ig as $libelle => $volume): ?>
 <tr>
-  <td style="text-align: left; width: 306px; border: 1px solid black; font-weight: bold;">&nbsp;<?php echo $libelle ?></td>
-  <td style="width: 110px; border: 1px solid black;"><?php echoVolume($volume, true) ?></td>
+  <td style="text-align: left; width: 214px; border: 1px solid black; font-weight: bold;">&nbsp;<?php echo $libelle ?></td>
+  <td style="width: 106px; border: 1px solid black;"><?php echoVolume($volume, true) ?></td>
 </tr>
 <?php endforeach; ?>
 </table>
--- project/apps/civa/modules/ds_export/templates/_tableau.php
+++ project/apps/civa/modules/ds_export/templates/_tableau.php
@@ -1,29 +1,28 @@
 <?php use_helper('Float') ?>
 <?php use_helper('dsExport') ?>
-<?php use_helper('Text') ?>
 
 <?php $tableau = $tableau->getRawValue() ?>
 <table border="1" cellspacing=0 cellpadding=0 style="text-align: right; border: 1px solid black;">
 <?php if(!array_key_exists('no_header', $tableau) || !$tableau['no_header']): ?>
   <tr>
     <?php foreach($tableau['colonnes'] as $libelle): ?>
-    <th style="text-align: left; font-weight: bold; width: <?php echo round(306/count($tableau['colonnes'])) ?>px; border: 1px solid black;">&nbsp;<?php echo $libelle ?></th>
+    <th style="text-align: left; font-weight: bold; width: <?php echo round(318/count($tableau['colonnes'])) ?>px; border: 1px solid black;">&nbsp;<?php echo $libelle ?></th>
     <?php endforeach; ?>
-    <th style="font-weight: bold; width: 110px; text-align: center;  border: 1px solid black;">hors VT et SGN</th>
-    <th style="font-weight: bold; width: 110px; text-align: center;  border: 1px solid black;">VT</th>
-    <th style="font-weight: bold; width: 110px; text-align: center;  border: 1px solid black;">SGN</th>
+    <th style="font-weight: bold; width: 106px; text-align: center;  border: 1px solid black;">hors VT et SGN</th>
+    <th style="font-weight: bold; width: 106px; text-align: center;  border: 1px solid black;">VT</th>
+    <th style="font-weight: bold; width: 106px; text-align: center;  border: 1px solid black;">SGN</th>
   </tr>
 <?php endif; ?>
 <?php foreach($tableau['produits'] as $produit): ?>
   <tr>
     <?php foreach($produit['colonnes'] as $colonne): ?>
     <?php if($colonne["rowspan"] > 0): ?>
-      <td style="text-align: left; border: 1px solid black; width: <?php echo round(306/count($tableau['colonnes'])) ?>px; <?php if(is_null($colonne['libelle'])): ?>background-color: #bbb;<?php endif; ?>" rowspan="<?php echo $colonne["rowspan"] ?>">&nbsp;<?php echo truncate_text($colonne['libelle'], round(50 / count($tableau['colonnes'])), "...", false) ?></td>
+      <td style="text-align: left; border: 1px solid black; width: <?php echo round(318/count($tableau['colonnes'])) ?>px; <?php if(is_null($colonne['libelle'])): ?>background-color: #bbb;<?php endif; ?>" rowspan="<?php echo $colonne["rowspan"] ?>">&nbsp;<?php echo truncate_text($colonne['libelle'], round(50 / count($tableau['colonnes'])), "<small>…</small>", false) ?></td>
     <?php endif; ?>
     <?php endforeach; ?>
-    <td style="border: 1px solid black; width: 110px; <?php if(is_null($produit["normal"])): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($produit["normal"]) ?></td>
-    <td style="border: 1px solid black; width: 110px;<?php if(is_null($produit["vt"])): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($produit["vt"]) ?></td>
-    <td style="border: 1px solid black; width: 110px; <?php if(is_null($produit["sgn"])): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($produit["sgn"]) ?></td>
+    <td style="border: 1px solid black; width: 106px; <?php if(is_null($produit["normal"])): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($produit["normal"]) ?></td>
+    <td style="border: 1px solid black; width: 106px;<?php if(is_null($produit["vt"])): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($produit["vt"]) ?></td>
+    <td style="border: 1px solid black; width: 106px; <?php if(is_null($produit["sgn"])): ?>background-color: #bbb;<?php endif; ?>"><?php echoVolume($produit["sgn"]) ?></td>
   </tr>
 <?php endforeach; ?>
 <tr>
