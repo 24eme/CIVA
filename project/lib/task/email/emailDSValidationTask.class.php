dcd0635face885d4040e8833423b23ae743f9b86
mathurin <mpetit@actualys.com>
2013-07-23 16:32:06 +0200

    mis en place de la tache de mail de rappel pour les ds

---
+++ project/lib/task/email/emailDSValidationTask.class.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of class emailDSValidationtask
+ * @author mathurin
+ */
+class emailDSValidationTask extends sfBaseTask {
+    
+  protected function configure()
+  {
+    $this->addArguments(array(
+            new sfCommandArgument('campagne', sfCommandArgument::REQUIRED, 'Année de déclaration'),
+    ));
+
+    $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'civa'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+    ));
+
+    $this->namespace        = 'email';
+    $this->name             = 'ds-validation';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [EmailValidationDS|INFO] task does things.
+Call it with:
+
+  [php symfony EmailValidationDS|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    set_time_limit('240');
+    ini_set('memory_limit', '512M');
+    
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    if (isset($arguments['campagne']) && !empty($arguments['campagne'])) {
+	    $nb_item = 0;
+	    $nb_email_send = 0;  
+            $campagne = $arguments['campagne'];
+            $exportManager = new ExportDSCiva($campagne);
+	    $dss = $exportManager->getDSNonValideesListe();
+	    foreach ($dss as $ds) {
+		$cvi = $ds->declarant->get('email');
+
+                $nb_item++;
+                if(!$ds->declarant->exist('email')) {
+                    $this->logSection('no email', $cvi, null, 'ERROR');
+                continue;
+                }
+            
+                try {
+            	$message = $this->getMailer()->compose()
+                      ->setFrom(array('dominique@civa.fr' => "Webmaster Vinsalsace.pro"))
+                      ->setTo($ds->declarant->get('email'))
+                     // ->setTo('mpetit@actualys.com')
+                      ->setSubject('RAPPEL DS '.$arguments['campagne'])
+                      ->setBody($this->getMessageBody($ds->declarant->get('nom'), $arguments['campagne']));
+                $sended = $this->getMailer()->send($message);
+                
+                //echo $this->getMessageBody($compte, $arguments['campagne'])."\n\n\n";
+            } catch (Exception $exc) {
+                $sended = false;
+            }
+            
+            if ($sended) {
+                $nb_email_send++;
+                $this->logSection('sended', $cvi . ' : ' . $ds->declarant->get('email'));
+            } else {
+                $this->logSection('send error', $cvi . ' : ' . $ds->declarant->get('email'), null, 'ERROR');
+            }
+            
+            
+	    }
+	    $this->logSection('Emails have been sended', sprintf('%d / %d envoyés', $nb_email_send,  $nb_item));
+    }
+  }
+
+  protected function getMessageBody($nom, $campagne) {
+      return "Bonjour ".$nom.",
+
+Vous avez commencé à saisir en ligne votre Déclaration de Stock ".$campagne." sur le site VinsAlsace.pro et ne l’avez pas encore validée. 
+
+Nous vous rappelons que vous devez impérativement la valider avant ce soir minuit. 
+
+Cordialement,
+
+Le CIVA";
+  }
+}
