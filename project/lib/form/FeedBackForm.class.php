7f60d142478f3da771f3636ec23a03af49e4ee35
Vincent LAURENT <vlaurent@actualys.com>
2013-07-29 19:18:45 +0200

    FeedBack

---
+++ project/lib/form/FeedBackForm.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class FeedBackForm extends sfForm {
+
+    public function configure() {
+	    $this->setWidgets(array(
+            'message' => new sfWidgetFormTextarea(),
+        ));
+
+        $this->setValidators(array(
+                'message' => new sfValidatorString(array('required' => true)),
+        ));
+            
+        $this->widgetSchema->setNameFormat('message[%s]');
+        $this->validatorSchema['message']->setMessage('required', 'Le message est obligatoire');
+    }
+
+}
+
+?>
