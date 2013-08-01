eace32c0e8a60eaa943ee67e2fe18685fb07c1b8
Vincent LAURENT <vlaurent@actualys.com>
2013-08-01 20:48:50 +0200

    Suppression du block d'import pour les acheteurs

--- project/apps/civa/modules/acheteur/templates/_monEspace.php
+++ project/apps/civa/modules/acheteur/templates/_monEspace.php
@@ -1,21 +1,3 @@
-
-<div id="import">
-    <h3 class="titre_section">Import</h3>
-    <div class="contenu_section">
-        <?php if ($csv): ?>
-            <ul>
-                <li><a href="<?php echo url_for('@export_dr_acheteur_csv') ?>">Télécharger l'import en CSV</a></li>
-                <?php if ($export): ?>
-                    <li><a target="_blank" href="https://<?php echo $sf_request->getHost().'/mise_a_disposition/'.$export->cle.'/declarations_de_recolte'; ?>">Télécharger les PDFs des déclarations de récolte</a></li>
-                <?php endif ?>
-               <li><a href="<?php echo url_for('@export_dr_validee_csv') ?>">Télécharger la liste des utilisateurs ayant validé leur DR</a></li>
-            </ul>
-        <?php else: ?>
-            <p> Le téléchargement des données sera accessible à partir du moment où vous avez soumis vos propres données. </p>
-        <?php endif; ?>
-    </div>
-</div>
-
 <div id="export">
     <h3 class="titre_section">Export</h3>
     <div class="contenu_section">
