14e86d6c581a0c9ababb756af27d9d393489b133
mathurin <mpetit@actualys.com>
2013-08-29 19:21:41 +0200

    maj task -> env=prod

--- project/lib/task/email/emailDSValidationTask.class.php
+++ project/lib/task/email/emailDSValidationTask.class.php
@@ -19,7 +19,7 @@ class emailDSValidationTask extends sfBaseTask {
 
     $this->addOptions(array(
             new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'civa'),
-            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
             new sfCommandOption('dsid', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', ''),
 
