3738ce6807ab9696e79719cb0be37f44b6d71890
mathurin <mpetit@actualys.com>
2013-09-02 19:05:08 +0200

    ajout d'un controle pour ne pas poster de mauvaise date ni de date inférieure au 31 aout

--- lib/form/DSLieuxDeStockageForm.class.php
+++ lib/form/DSLieuxDeStockageForm.class.php
@@ -41,12 +41,12 @@ class DSLieuxDeStockageForm extends acCouchdbForm {
         if($this->ds->isDateDepotMairie()){
             $this->setWidget('date_depot_mairie', new sfWidgetFormInput());
             $this->widgetSchema->setLabel('date_depot_mairie', 'Date de dépot en mairie :');
-            $this->setValidator('date_depot_mairie', new sfValidatorString(array('required' => true)));
+            $this->setValidator('date_depot_mairie', new sfValidatorRegex(array('required' => true, 'pattern' => "/^([0-9]){2}\/([0-9]){2}\/([0-9]){4}$/"),array('invalid' => 'Le format de la date de dépot en mairie doit être jj/mm/aaaa')));
         }
         
         $this->setWidget('neant', new sfWidgetFormChoice(array('choices' => $this->getNeant(),'expanded' => true, 'multiple' => true)));
         $this->setValidator('neant', new sfValidatorChoice(array('required' => false, 'choices' => array_keys($this->getNeant()), 'multiple' => true)));
-        $this->getValidatorSchema()->setPostValidator(new ValidatorLieuxStockageDS($this->lieux_stockage));
+        $this->getValidatorSchema()->setPostValidator(new ValidatorLieuxStockageDS($this->lieux_stockage,$this->ds));
 
         
         
