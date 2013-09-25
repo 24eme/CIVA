8b1ce706ea0d8adf8276e24b323922db7b61c421
Mikael Guillin <mguillin@actualys.com>
2013-09-25 17:34:41 +0200

    Intégration de l'annuaire des contrats

--- project/apps/civa/modules/annuaire/templates/indexSuccess.php
+++ project/apps/civa/modules/annuaire/templates/indexSuccess.php
@@ -1,53 +1,111 @@
-<div class="clearfix">
-		<h1>Récoltants (<?php echo count($annuaire->recoltants) ?>)</h1>
-		<br />
-		<?php if (count($annuaire->recoltants) > 0): ?>
-		<ul>
-		<?php foreach ($annuaire->recoltants as $key => $item): ?>
-			<li><?php echo $item ?> <small style="font-size: 10px;">(<?php echo $key ?>)</small>&nbsp;<a href="<?php echo url_for('annuaire_supprimer', array('type' => 'recoltants', 'id' => $key)) ?>">X</a></li>
-		<?php endforeach; ?>
-		</ul>
-		<?php else: ?>
-		<i>Aucun récoltants</i>
-		<?php endif; ?>
-		<br /><br />
-		<h1>Négociants (<?php echo count($annuaire->negociants) ?>)</h1>
-		<br />
-		<?php if (count($annuaire->negociants) > 0): ?>
-		<ul>
-		<?php foreach ($annuaire->negociants as $key => $item): ?>
-			<li><?php echo $item ?> <small style="font-size: 10px;">(<?php echo $key ?>)</small>&nbsp;<a href="<?php echo url_for('annuaire_supprimer', array('type' => 'negociants', 'id' => $key)) ?>">X</a></li>
-		<?php endforeach; ?>
-		</ul>
-		<?php else: ?>
-		<i>Aucun négociants</i>
-		<?php endif; ?>
-		<br /><br />
-		<h1>Caves coopératives (<?php echo count($annuaire->caves_cooperatives) ?>)</h1>
-		<br />
-		<?php if (count($annuaire->caves_cooperatives) > 0): ?>
-		<ul>
-		<?php foreach ($annuaire->caves_cooperatives as $key => $item): ?>
-			<li><?php echo $item ?> <small style="font-size: 10px;">(<?php echo $key ?>)</small>&nbsp;<a href="<?php echo url_for('annuaire_supprimer', array('type' => 'caves_cooperatives', 'id' => $key)) ?>">X</a></li>
-		<?php endforeach; ?>
-		</ul>
-		<?php else: ?>
-		<i>Aucun négociants</i>
-		<?php endif; ?>
-		<br /><br />
-		<p><a href="<?php echo url_for('@annuaire_selectionner') ?>">+ Ajouter une entrée</a></p>
-		<br /><br />
-		<h1>Commerciaux (<?php echo count($annuaire->commerciaux) ?>)</h1><br />
-		<?php if (count($annuaire->commerciaux) > 0): ?>
-		<ul>
-		<?php foreach ($annuaire->commerciaux as $key => $item): ?>
-			<li><?php echo $item ?> <a href="<?php echo url_for('annuaire_supprimer', array('type' => 'commerciaux', 'id' => $key)) ?>">X</a></li>
-		<?php endforeach; ?>
-		</ul>
-		<?php else: ?>
-		<i>Aucun commercial</i>
-		<?php endif; ?>
-		<br /><br />
-		<p><a href="<?php echo url_for('@annuaire_commercial_ajouter') ?>">+ Ajouter une entrée</a></p>
+<div id="contrats_vrac" class="clearfix">
+	
+	<div class="annuaire">
+		
+		<div class="bloc_annuaire">
+			<h2>Récoltants (<?php echo count($annuaire->recoltants) ?>)</h2>
+			
+			<div class="bloc">			
+				<?php if (count($annuaire->recoltants) > 0): ?>
+					<?php $i = 0; ?>
+					<h3>Raison sociale</h3>
+					<ul>
+						<?php foreach ($annuaire->recoltants as $key => $item): ?>
+							<?php $i++; ?>
+
+							<?php if($i % 2 == 0): ?>
+								<li class="alt"><?php echo $item ?> <small>(<?php echo $key ?>)</small><a href="<?php echo url_for('annuaire_supprimer', array('type' => 'recoltants', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php else: ?>
+								<li><?php echo $item ?> <small>(<?php echo $key ?>)</small><a href="<?php echo url_for('annuaire_supprimer', array('type' => 'recoltants', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php endif; ?>
+						<?php endforeach; ?>
+					</ul>
+				<?php else: ?>
+					<p>Aucun récoltants</p>
+				<?php endif; ?>
+			</div>
+
+			<a href="#" class="btn"><img src="/images/boutons/btn_ajouter_recoltant.png" alt="Ajouter un récoltant" /></a>
+		</div>
+	
+		<div class="bloc_annuaire">
+			<h2>Négociants (<?php echo count($annuaire->negociants) ?>)</h2>
+			<div class="bloc">
+				<?php if (count($annuaire->negociants) > 0): ?>
+					<?php $i = 0; ?>
+					<h3>Raison sociale</h3>
+					<ul>
+						<?php foreach ($annuaire->negociants as $key => $item): ?>
+							<?php $i++; ?>
+
+							<?php if($i % 2 == 0): ?>
+								<li class="alt"><?php echo $item ?> <small>(<?php echo $key ?>)</small><a href="<?php echo url_for('annuaire_supprimer', array('type' => 'negociants', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php else: ?>
+								<li><?php echo $item ?> <small>(<?php echo $key ?>)</small><a href="<?php echo url_for('annuaire_supprimer', array('type' => 'negociants', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php endif; ?>
+						<?php endforeach; ?>
+					</ul>
+				<?php else: ?>
+					<p>Aucun négociants</p>
+				<?php endif; ?>
+			</div>
+
+			<a href="#" class="btn"><img src="/images/boutons/btn_ajouter_negociant.png" alt="Ajouter un négociant" /></a>
+		</div>
+
+		<div class="bloc_annuaire">
+			<h2>Caves coopératives (<?php echo count($annuaire->caves_cooperatives) ?>)</h2>
+			<div class="bloc">
+				<?php if (count($annuaire->caves_cooperatives) > 0): ?>
+					<?php $i = 0; ?>
+					<h3>Raison sociale</h3>
+					<ul>
+						<?php foreach ($annuaire->caves_cooperatives as $key => $item): ?>
+							<?php $i++; ?>
+
+							<?php if($i % 2 == 0): ?>
+								<li class="alt"><?php echo $item ?> <small>(<?php echo $key ?>)</small><a href="<?php echo url_for('annuaire_supprimer', array('type' => 'caves_cooperatives', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php else: ?>
+								<li><?php echo $item ?> <small>(<?php echo $key ?>)</small><a href="<?php echo url_for('annuaire_supprimer', array('type' => 'caves_cooperatives', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php endif; ?>
+						<?php endforeach; ?>
+					</ul>
+				<?php else: ?>
+					<p>Aucun négociants</p>
+				<?php endif; ?>
+			</div>
+
+			<a href="<?php echo url_for('@annuaire_selectionner') ?>" class="btn"><img src="/images/boutons/btn_ajouter_cave.png" alt="Ajouter une cave coopérative" /></a>
+		</div>
+
+		<div class="bloc_annuaire">
+			<h2>Interlocuteurs commerciaux (<?php echo count($annuaire->commerciaux) ?>)</h2>
+			<div class="bloc">
+				<?php if (count($annuaire->commerciaux) > 0): ?>
+					<?php $i = 0; ?>
+					<h3>Raison sociale</h3>
+					<ul>
+						<?php foreach ($annuaire->commerciaux as $key => $item): ?>
+							<?php $i++; ?>
+
+							<?php if($i % 2 == 0): ?>
+								<li class="alt"><?php echo $item ?> <a href="<?php echo url_for('annuaire_supprimer', array('type' => 'commerciaux', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php else: ?>
+								<li><?php echo $item ?> <a href="<?php echo url_for('annuaire_supprimer', array('type' => 'commerciaux', 'id' => $key)) ?>" class="btn_supprimer">X</a></li>
+							<?php endif; ?>
+						<?php endforeach; ?>
+					</ul>
+				<?php else: ?>
+					<p>Aucun commercial</p>
+				<?php endif; ?>
+			</div>
+
+			<a href="<?php echo url_for('@annuaire_commercial_ajouter') ?>" class="btn"><img src="/images/boutons/btn_ajout_commercial.png" alt="Ajouter un interlocuteur commercial" /></a>
+		</div>
+	</div>
 </div>
+
+<ul id="btn_etape" class="btn_prev_suiv">
+	<li><a href="#"><img src="/images/boutons/btn_retour_espace_contrats.png" alt="Retourner à l'espace contrats" /></a></li>
+</ul>
 
--- project/apps/civa/modules/vrac/templates/_form_validation.php
+++ project/apps/civa/modules/vrac/templates/_form_validation.php
@@ -1,45 +1,58 @@
 <?php include_partial('vrac/soussignes', array('vrac' => $vrac)) ?>
 
-<div class="clearfix">
-		<table class="table_donnees" cellspacing="0" cellpadding="0">
-			<thead>
-				<tr>
-					<th>Produit</th>
-					<th width="40px"><span>Volume proposé</span></th>
-					<th width="40px"><span>Prix unitaire</span></th>
-				</tr>
-			</thead>
-			<tbody>
-			<?php 
-				foreach ($vrac->declaration->getActifProduitsDetailsSorted() as $details):
-				foreach ($details as $detail):
-			?>
-			<tr>
-				<td>
-					<strong><?php echo $detail->getLibelle(); ?></strong><?php if ($detail->millesime) { echo ' '.$detail->millesime; } if ($detail->denomination) { echo ' '.$detail->denomination; } if ($detail->vtsgn) { echo ' '.$detail->vtsgn; } ?>
-				</td>
-				<td width="40px">
-					<?php echo $detail->volume_propose ?>&nbsp;Hl
-				</td>
-				<td width="40px">
-					<?php echo $detail->prix_unitaire ?>&nbsp;&euro;/Hl
-				</td>
-			</tr>
-			<?php 
-				endforeach;
-				endforeach;
-			?>
-			</tbody>
-		</table>
-		<div>
-			<?php echo $form['conditions_paiement']->renderLabel() ?>
-			<?php echo $form['conditions_paiement']->render() ?>
-			<span><?php echo $form['conditions_paiement']->renderError() ?></span>
-		</div>
-		<div>
-			<?php echo $form['conditions_particulieres']->renderLabel() ?>
-			<?php echo $form['conditions_particulieres']->render() ?>
-			<span><?php echo $form['conditions_particulieres']->renderError() ?></span>
-		</div>
+<table class="table_donnees">
+	<thead>
+		<tr>
+			<th>Produit</th>
+			<th>Volume proposé</th>
+			<th>Prix unitaire</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php 
+			foreach ($vrac->declaration->getActifProduitsDetailsSorted() as $details):
+			foreach ($details as $detail):
+		?>
+		<tr>
+			<td>
+				<strong><?php echo $detail->getLibelle(); ?></strong><?php if ($detail->millesime) { echo ' '.$detail->millesime; } if ($detail->denomination) { echo ' '.$detail->denomination; } if ($detail->vtsgn) { echo ' '.$detail->vtsgn; } ?>
+			</td>
+			<td>
+				<?php echo $detail->volume_propose ?>&nbsp;Hl
+			</td>
+			<td>
+				<?php echo $detail->prix_unitaire ?>&nbsp;&euro;/Hl
+			</td>
+		</tr>
+		<?php 
+			endforeach;
+			endforeach;
+		?>
+
+		<tr>
+			<td>Lorem ipsum dolor sit amet.</td>
+			<td>Ipsa nobis voluptas quos cupiditate.</td>
+			<td>Quisquam, quas dignissimos laudantium accusamus.</td>
+		</tr>
+		<tr>
+			<td>Lorem ipsum dolor sit amet.</td>
+			<td>Ipsa nobis voluptas quos cupiditate.</td>
+			<td>Quisquam, quas dignissimos laudantium accusamus.</td>
+		</tr>
+
+	</tbody>
+</table>
+
+<div class="conditions">
+	<div class="ligne_form">
+		<?php echo $form['conditions_paiement']->renderLabel() ?>
+		<?php echo $form['conditions_paiement']->render() ?>
+		<span><?php echo $form['conditions_paiement']->renderError() ?></span>
+	</div>
+	<div class="ligne_form">
+		<?php echo $form['conditions_particulieres']->renderLabel() ?>
+		<?php echo $form['conditions_particulieres']->render() ?>
+		<span><?php echo $form['conditions_particulieres']->renderError() ?></span>
+	</div>
 </div>
 
