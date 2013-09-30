780bbcdcaaff36a9f72c1ec95d4f720c66a1ed79
JB <jblemetayer@actualys.com>
2013-09-30 15:15:51 +0200

    Prise en compte du statut dans la vue des vracs

--- lib/model/views/VracTousView.class.php
+++ lib/model/views/VracTousView.class.php
@@ -7,10 +7,19 @@ class VracTousView extends acCouchdbView
         return acCouchdbManager::getView('VRAC', 'tous', 'Vrac');
     }
 
-    public function findBy($identifiant, $campagne) {    
-
-        return $this->client->startkey(array($identifiant, $campagne))
-                            ->endkey(array($identifiant, $campagne, array()))
+    public function findBy($identifiant, $campagne = null, $statut = null) {    
+		$params = array($identifiant);
+		if ($campagne) {
+			$params[] = $campagne;
+			if ($statut) {
+				$params[] = $statut;
+			}
+		}
+		$startkey = $params;
+		$params[] = array();
+		$endkey = $params;
+        return $this->client->startkey($startkey)
+                            ->endkey($endkey)
                             ->getView($this->design, $this->view)->rows;
     }
 }  
