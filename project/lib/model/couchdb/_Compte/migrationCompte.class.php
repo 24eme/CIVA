7191d889179d2dc7e2282848854594c8345d04b4
mathurin <mpetit@actualys.com>
2013-10-01 16:47:34 +0200

    maj de la mise en place de l'update des TIERS

--- project/lib/model/couchdb/_Compte/migrationCompte.class.php
+++ project/lib/model/couchdb/_Compte/migrationCompte.class.php
@@ -10,20 +10,27 @@ class MigrationCompte {
     protected $_nouveau_cvi = null;
     protected $_ancien_compte = null;
     protected $_nouveau_compte = null;
+    protected $_newPassword = false;
 
-    public function __construct(acCouchdbJson $compte, $nouveau_cvi, $nom = null, $commune = null) {
+    public function __construct(acCouchdbJson $compte, $nouveau_cvi, $nom = null, $commune = null, $withCopyPasswords = false) {
         $this->_ancien_compte = $compte;
         $this->_ancien_cvi = str_replace(self::PREFIX_KEY_COMPTE, '', $compte->_id);
         $this->_nouveau_cvi = $nouveau_cvi;
         $this->nom = $nom;
         $this->commune = $commune;
+        if($withNewPasswords){
+            throw new sfException("Le nouveau mot de passe doit être un nombre chiffres");
+        }
+        $this->_newPassword = ($withCopyPasswords && $withCopyPasswords=="NON")? sprintf("%4d", rand(0, 9999)) : null;
     }
 
     public function process(){
         $this->createNewCompte();
         $this->createCompteTiers();
         $this->createLienSymbolique();
-
+        if($this->_newPassword){
+            $this->majNewPassword();
+        }
         return ((is_object(acCouchdbManager::getClient('_Compte')->find(self::PREFIX_KEY_COMPTE . $this->_nouveau_cvi))
             &&   is_object(acCouchdbManager::getClient('Recoltant')->retrieveByCvi($this->_nouveau_cvi)))) ? true : false;
     }
@@ -86,5 +93,13 @@ class MigrationCompte {
             $ls_dr->update();
             $ls_dr->save();
         }
+    }
+    
+    public function majNewPassword() {
+        $new_compte = acCouchdbManager::getClient('_Compte')->find(self::PREFIX_KEY_COMPTE . $this->_nouveau_cvi);
+        $new_compte->mot_de_passe = "{TEXT}".$this->_newPassword;
+        $new_compte->statut = "NOUVEAU";
+        $new_compte->save();
+        echo $this->_nouveau_cvi.",".$this->_newPassword."\n";
     }
 }
