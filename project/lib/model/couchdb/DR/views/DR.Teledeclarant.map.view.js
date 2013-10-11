cb90d480dde3e6cf4f95b14834d986616cbf03bc
mathurin <mpetit@actualys.com>
2013-10-11 15:07:14 +0200

    maj view des declarants

--- project/lib/model/couchdb/DR/views/DR.Teledeclarant.map.view.js
+++ project/lib/model/couchdb/DR/views/DR.Teledeclarant.map.view.js
@@ -3,19 +3,20 @@ function(doc) {
     if (!(doc.type == "DR" && doc.utilisateurs && doc.utilisateurs.validation)) {
         return;
     }
-
+    var founded = false;
     for (var u in doc.utilisateurs.validation) {
         if(u.match("^COMPTE-[0-9]{10}")){
             emit([doc.campagne, doc.cvi, doc.declarant.email], 1);
+	    founded = true;
             break;
         }
     }
-    
+    if(!founded){
     for (var u in doc.utilisateurs.edition) {
         if(u.match("^COMPTE-[0-9]{10}")){
             emit([doc.campagne, doc.cvi, doc.declarant.email], 1);
             break;
         }
     }
-
+    }
 }
