9d90ea9c4dd4b4866d5406d543bb9f306732320b
Mikael Guillin <mguillin@actualys.com>
2013-10-15 18:50:31 +0200

    Corrections inté contrats

--- project/apps/civa/modules/vrac/templates/_form_produits.php
+++ project/apps/civa/modules/vrac/templates/_form_produits.php
@@ -1,44 +1,44 @@
-<div class="clearfix">
-	<table class="produits table_donnees">
-		<thead>
-			<tr>
-				<th>Produit</th>
-				<th><span>Dénomination</span></th>
-				<th><span>Millésime</span></th>
-				<th><span>Volume proposé</span></th>
-				<th><span>Prix unitaire</span></th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php 
-			foreach ($form['produits'] as $key => $embedForm) :
-				$detail = $vrac->get($key);
-		?>
-			<tr>
-				<td><strong><?php echo $detail->getLibelle(); ?></strong><?php echo $detail->getComplementPartielLibelle(); ?></td>
-				<td>
-					<span><?php echo $embedForm['denomination']->renderError() ?></span>
-					<?php echo $embedForm['denomination']->render() ?>
-				</td>
-				<td>
-					<span><?php echo $embedForm['millesime']->renderError() ?></span>
-					<?php echo $embedForm['millesime']->render() ?>
-				</td>
-				<td class="volume">
-					<span><?php echo $embedForm['volume_propose']->renderError() ?></span>
-					<?php echo $embedForm['volume_propose']->render() ?>&nbsp;Hl
-				</td>
-				<td class="prix">
-					<span><?php echo $embedForm['prix_unitaire']->renderError() ?></span>
-					<?php echo $embedForm['prix_unitaire']->render() ?>&nbsp;&euro;/Hl
-					<a href="#" class="balayette" title="Effacer les champs">Effacer les champs</a>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-		</tbody>
+
+<table class="produits table_donnees">
+	<thead>
+		<tr>
+			<th class="produit">Produit</th>
+			<th class="denomination"><span>Dénomination</span></th>
+			<th class="millesime"><span>Millésime</span></th>
+			<th class="volume"><span>Volume proposé</span></th>
+			<th class="prix"><span>Prix unitaire</span></th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php 
+		foreach ($form['produits'] as $key => $embedForm) :
+			$detail = $vrac->get($key);
+	?>
+		<tr>
+			<td class="produit"><strong><?php echo $detail->getLibelle(); ?></strong><?php echo $detail->getComplementPartielLibelle(); ?></td>
+			<td class="denomination">
+				<span><?php echo $embedForm['denomination']->renderError() ?></span>
+				<?php echo $embedForm['denomination']->render() ?>
+			</td>
+			<td class="millesime">
+				<span><?php echo $embedForm['millesime']->renderError() ?></span>
+				<?php echo $embedForm['millesime']->render() ?>
+			</td>
+			<td class="volume">
+				<span><?php echo $embedForm['volume_propose']->renderError() ?></span>
+				<?php echo $embedForm['volume_propose']->render() ?>&nbsp;Hl
+			</td>
+			<td class="prix">
+				<span><?php echo $embedForm['prix_unitaire']->renderError() ?></span>
+				<?php echo $embedForm['prix_unitaire']->render() ?>&nbsp;&euro;/Hl
+				<a href="#" class="balayette" title="Effacer les champs">Effacer les champs</a>
+			</td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
 </table>
 <a href="<?php echo url_for('vrac_ajout_produit', array('sf_subject' => $vrac, 'etape' => $etape)) ?>" id="ajouter-produit">Ajouter un produit</a>
-</div>
+
 <script type="text/javascript">
 $(document).ready(function () {
 	$("#ajouter-produit").click(function() {
