d372e6cebdea11a2dd7b650af376f9e7dd0140f8
mathurin <mpetit@actualys.com>
2013-10-16 11:49:55 +0200

    mise en place de la date de dépot en mairie

--- project/lib/model/couchdb/DR/DR.class.php
+++ project/lib/model/couchdb/DR/DR.class.php
@@ -561,5 +561,17 @@ class DR extends BaseDR implements InterfaceProduitsDocument, IUtilisateursDocum
     public function hasDateDepotMairie() {
         return $this->exist('date_depot_mairie') && !is_null($this->date_depot_mairie);
     }
+    
+    public function getDateDepotMairieFr() {
+       return Date::francizeDate($this->date_depot_mairie);
+    }
+    
+    function setDepotmairie($date_iso) {
+        if($this->modifiee == $this->validee){
+            $this->modifiee = $date_iso;
+        }
+        $this->validee = $date_iso;
+        $this->add('date_depot_mairie',$date_iso);
+    }
 
 }
