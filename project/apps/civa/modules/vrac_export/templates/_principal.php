9b9086272db69444858712f680968a4a429cc19f
Mikael Guillin <mguillin@actualys.com>
2013-10-28 15:07:12 +0100

    Amélioration du pdf contrat

--- project/apps/civa/modules/vrac_export/templates/_principal.php
+++ project/apps/civa/modules/vrac_export/templates/_principal.php
@@ -3,103 +3,124 @@
 <?php  use_helper('vracExport'); ?>
 <?php  include_partial("vrac_export/soussignes", array('vrac' => $vrac));  ?>
 <small><br /></small>
-<span style="background-color: black; color: white; font-weight: bold;">&nbsp;<?php echo "TRANSACTIONS EN VRAC"; ?>&nbsp;</span><br />
+<span style="display: block; background-color: black; color: white; font-weight: bold; text-align: center; padding: 5px; width: 220px;">&nbsp;<?php echo "TRANSACTIONS EN VRAC"; ?>&nbsp;</span>
 
-<table border="1" cellspacing=0 cellpadding=0 style="text-align: right; border: none;">
-<tr>
-  <th style="width: 34px; font-weight: bold; text-align: center; border: 1px solid black; ">AOC<span style="font-size:8px;"><br/>&nbsp;<b>1</b> Alsace
-                                                                                                                                            <br/>&nbsp;<b>2</b> Crémant
-                                                                                                                                            <br/>&nbsp;<b>3</b> Grands Crus</span></th>
-  <th style="width: 116px; font-weight: bold; text-align: center; border: 1px solid black; ">Cépage</th>
-  <th style="width: 138px; font-weight: bold; text-align: center; border: 1px solid black; "><small>Dénominations spécifiques, communales, lieux-dits, VT/SGN, etc...</small></th>
-  <th style="width: 66px; font-weight: bold; text-align: center; border: 1px solid black; ">Millésime</th>  
-  <th style="width: 92px; font-weight: bold; text-align: center; border: 1px solid black; ">Prix* de l'hectolitre <small>(en &euro;/HL)</small></th>
-  <th style="width: 77px; font-weight: bold; text-align: center; border: 1px solid black; ">VOLUME estimé<br/><small>(en HL)</small></th>
-  <?php if ($vrac->isCloture()): ?>
-  <th style="width: 77px; font-weight: bold; text-align: center; border: 1px solid black; ">VOLUME réel<br/><small>(en HL)</small></th>
-  <?php endif; ?>
-</tr>
-<?php foreach ($vrac->declaration->getProduitsDetailsSorted() as $product): 
-    $productLine = $product->getRawValue();
-        foreach ($productLine as $detailKey => $detailLine): 
-            $backgroundColor = getColorRowDetail($detailLine);
-    ?>
-<tr>
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: center;"><?php echo $detailLine->getCepage()->getAppellation()->getCodeCiva(); ?></td>
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: left;">&nbsp;<?php echo $detailLine->getCepage()->getLibelle(); ?></td>
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echo $detailLine->getDenomination(); ?> <?php echo $detailLine->getLieuDit(); ?> <?php echo $detailLine->getVtsgn(); ?></td>
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: center;"><?php echo $detailLine->getMillesime(); ?>&nbsp;</td>    
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echoPrix($detailLine->getPrixUnitaire(), true); ?></td>
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echoVolume($detailLine->volume_propose, true); ?></td>
-    <?php if ($vrac->isCloture()): ?>
-    <td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echoVolume($detailLine->volume_enleve, true); ?></td>
-    <?php endif; ?>
-</tr>
-<?php 
-endforeach;
-endforeach; 
-?>
+<table border="0" cellspacing="0" cellpadding="0" width="100%" style="text-align: right; border-collapse: collapse;">
+	<tr>
+		<th style="width: 34px; font-weight: bold; text-align: center; border: 1px solid black; ">AOC
+			<span style="font-size:8px;">
+				<br/>&nbsp;<b>1</b> Alsace
+				<br/>&nbsp;<b>2</b> Crémant
+				<br/>&nbsp;<b>3</b> Grands Crus
+			</span>
+		</th>
+		<th style="width: 116px; font-weight: bold; text-align: center; border: 1px solid black; ">Cépage</th>
+		<th style="width: 138px; font-weight: bold; text-align: center; border: 1px solid black; "><small>Dénominations spécifiques, communales, lieux-dits, VT/SGN, etc...</small></th>
+		<th style="width: 66px; font-weight: bold; text-align: center; border: 1px solid black; ">Millésime</th>  
+		<th style="width: 92px; font-weight: bold; text-align: center; border: 1px solid black; ">Prix* de l'hectolitre <small>(en &euro;/HL)</small></th>
+		<th style="width: 77px; font-weight: bold; text-align: center; border: 1px solid black; ">VOLUME estimé<br/><small>(en HL)</small></th>
+		<?php if ($vrac->isCloture()): ?>
+			<th style="width: 77px; font-weight: bold; text-align: center; border: 1px solid black; ">VOLUME réel<br/><small>(en HL)</small></th>
+		<?php endif; ?>
+	</tr>
+	<?php foreach ($vrac->declaration->getProduitsDetailsSorted() as $product): 
+			$productLine = $product->getRawValue();
+					foreach ($productLine as $detailKey => $detailLine): 
+							$backgroundColor = getColorRowDetail($detailLine);
+			?>
+	<tr>
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: center;"><?php echo $detailLine->getCepage()->getAppellation()->getCodeCiva(); ?></td>
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: left;">&nbsp;<?php echo $detailLine->getCepage()->getLibelle(); ?></td>
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echo $detailLine->getDenomination(); ?> <?php echo $detailLine->getLieuDit(); ?> <?php echo $detailLine->getVtsgn(); ?></td>
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: center;"><?php echo $detailLine->getMillesime(); ?>&nbsp;</td>    
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echoPrix($detailLine->getPrixUnitaire(), true); ?></td>
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echoVolume($detailLine->volume_propose, true); ?></td>
+			<?php if ($vrac->isCloture()): ?>
+			<td style="border: 1px solid black; <?php echo $backgroundColor ?> text-align: right;"><?php echoVolume($detailLine->volume_enleve, true); ?></td>
+			<?php endif; ?>
+	</tr>
+	<?php 
+		endforeach;
+	endforeach; 
+	?>
 
-<tr>
-    <td style="border: none; text-align: left;" colspan="4" >&nbsp;*<span style="font-size:20px; padding-left: 20px"><?php echo getExplicationEtoile(); ?></span></td>
-    <td style="border: 2px solid black;"><?php echoVolume($vrac->getTotalVolumePropose(), true); ?></td>
-    <td style="border: 1px solid black; background-color: #bbb;">&nbsp;</td>    
-    <?php if ($vrac->isCloture()): ?>
-    <td style="border: 2px solid black;"><?php echoVolume($vrac->getTotalVolumeEnleve(), true); ?></td>
-    <?php endif; ?>
-</tr>
+	<tr>
+			<td style="border: none; text-align: left;" colspan="4" >&nbsp;*<span style="font-size:20px; padding-left: 20px"><?php echo getExplicationEtoile(); ?></span></td>
+			<td style="border: 2px solid black;"><?php echoVolume($vrac->getTotalVolumePropose(), true); ?></td>
+			<td style="border: 1px solid black; background-color: #bbb;">&nbsp;</td>    
+			<?php if ($vrac->isCloture()): ?>
+			<td style="border: 2px solid black;"><?php echoVolume($vrac->getTotalVolumeEnleve(), true); ?></td>
+			<?php endif; ?>
+	</tr>
 </table>
 
 <br />
 <br />
 <br />
-<table cellspacing="10" cellpadding="0" style="text-align: right; border: none;">
-<tr>
-  <td style="text-align: left; width: 165px; border: none; font-weight: bold;">Conditions de paiement :</td>
-  <td style="width: 520px; text-align: left;"><?php echo $vrac->conditions_paiement; ?></td>
-</tr>
-<tr>
-  <td style="text-align: left; width: 165px; border: none; font-weight: bold;">Conditions particulières :</td>
-  <td style="width: 520px; text-align: left;"><?php echo $vrac->conditions_particulieres; ?></td>
-</tr>
+<table cellspacing="10" cellpadding="0" border="0" width="100%" style="text-align: right;">
+	<tr>
+		<td style="text-align: left; width: 200px; border: none; font-weight: bold;">Conditions de paiement :</td>
+		<td style="text-align: left;"><?php echo $vrac->conditions_paiement; ?></td>
+	</tr>
+	<tr>
+		<td style="text-align: left; width: 200px; border: none; font-weight: bold;">Conditions particulières :</td>
+		<td style="text-align: left;"><?php echo $vrac->conditions_particulieres; ?></td>
+	</tr>
 </table>
 <br />
 <br />
 <?php for($i=0;$i<22-count($vrac->declaration->getProduitsDetailsSorted());$i++): ?>
-    <br />
+		<br />
 <?php endfor;?>
 
-<table cellspacing=10 cellpadding=0 style="text-align: left; border: none;">
+<table cellspacing="0" cellpadding="0" border="0" width="100%" style="text-align: left;">
 <?php $widthAcheteur = ($vrac->hasCourtier())? "width: 210px;" : "width: 420px;"; ?>
-<tr>
-  <td style="text-align: right; width: 210px; border: none; font-weight: bold;">LE VENDEUR</td>  
-  <?php if($vrac->hasCourtier()): ?>
-  <td style="text-align: right; width: 210px; border: none; font-weight: bold;">VU, le Courtier</td>
-  <?php  endif; ?>
-  <td style="text-align: right; <?php echo $widthAcheteur; ?> border: none; font-weight: bold;">L'ACHETEUR</td>
-</tr>
-<tr>
-  <td style="text-align: right; width: 210px; border: none; font-weight: bold; font-size:25px;"><?php if ($vrac->valide->date_validation_vendeur): ?>le <?php echo preg_replace('/^(\d+)\-(\d+)\-(\d+)$/', '\3/\2/\1', $vrac->valide->date_validation_vendeur); ?>,<?php endif; ?></td>  
-  <?php if($vrac->hasCourtier()): ?>
-  <td style="text-align: right; width: 210px; border: none; font-weight: bold; font-size:25px;"><?php if ($vrac->valide->date_validation_mandataire): ?>le <?php echo preg_replace('/^(\d+)\-(\d+)\-(\d+)$/', '\3/\2/\1', $vrac->valide->date_validation_mandataire); ?>,<?php endif; ?></td>
-  <?php  endif; ?>
-  <td style="text-align: right; <?php echo $widthAcheteur; ?> border: none; font-weight: bold; font-size:25px;"><?php if ($vrac->valide->date_validation_acheteur): ?>le <?php echo preg_replace('/^(\d+)\-(\d+)\-(\d+)$/', '\3/\2/\1', $vrac->valide->date_validation_acheteur); ?>,<?php endif; ?></td>
-</tr>
- <tr>
-  <td style="text-align: right; width: 210px; border: none; font-weight: bold; font-size:25px;"><?php if ($vrac->valide->date_validation_vendeur): ?>signé éléctroniquement<?php endif; ?></td>  
-  <?php if($vrac->hasCourtier()): ?>
-  <td style="text-align: right; width: 210px; border: none; font-weight: bold; font-size:25px;"><?php if ($vrac->valide->date_validation_mandataire): ?>signé éléctroniquement<?php endif; ?></td>
-  <?php  endif; ?>
-  <td style="text-align: right; <?php echo $widthAcheteur; ?> border: none; font-weight: bold; font-size:25px;"><?php if ($vrac->valide->date_validation_acheteur): ?>signé éléctroniquement<?php endif; ?></td>
-</tr>
+	<tr>
+		<td width="33%" valign="top" style="padding-right: 10px;">
+			<span style="display: block; padding: 5px; background: grey; text-align: center; color: #FFF; font-weight: bold; width: 150px;">LE VENDEUR</span>
+			<table cellspacing="0" cellpadding="5" border="0" width="100%" style="border: 1px solid #000;">
+				<tr>
+					<td><?php if ($vrac->valide->date_validation_vendeur): ?>le <?php echo preg_replace('/^(\d+)\-(\d+)\-(\d+)$/', '\3/\2/\1', $vrac->valide->date_validation_vendeur); ?>,<?php endif; ?></td>
+				</tr>
+				<tr>
+					<td><?php if ($vrac->valide->date_validation_vendeur): ?>signé éléctroniquement<?php endif; ?></td>
+				</tr>
+			</table>
+		</td>
+		<?php if($vrac->hasCourtier()): ?>
+		<td width="33%" valign="top">
+			<span style="display: block; padding: 5px; background: grey; text-align: center; color: #FFF; font-weight: bold; width: 150px;">VU, le Courtier</span>
+			<table cellspacing="0" cellpadding="5" border="0" width="100%" style="border: 1px solid #000;">
+				<tr>
+					<td><?php if ($vrac->valide->date_validation_mandataire): ?>le <?php echo preg_replace('/^(\d+)\-(\d+)\-(\d+)$/', '\3/\2/\1', $vrac->valide->date_validation_mandataire); ?>,<?php endif; ?></td>
+				</tr>
+				<tr>
+					<td><?php if ($vrac->valide->date_validation_mandataire): ?>signé éléctroniquement<?php endif; ?></td>
+				</tr>
+			</table>
+		</td>
+		<?php endif; ?>
+		<td width="33%" valign="top" style="padding-left: 10px;">
+			<span style="display: block; padding: 5px; background: grey; text-align: center; color: #FFF; font-weight: bold; width: 150px;">L'ACHETEUR</span>
+			<table cellspacing="0" cellpadding="5" border="0" width="100%" style="border: 1px solid #000;">
+				<tr>
+					<td><?php if ($vrac->valide->date_validation_acheteur): ?>le <?php echo preg_replace('/^(\d+)\-(\d+)\-(\d+)$/', '\3/\2/\1', $vrac->valide->date_validation_acheteur); ?>,<?php endif; ?></td>
+				</tr>
+				<tr>
+					<td><?php if ($vrac->valide->date_validation_acheteur): ?>signé éléctroniquement<?php endif; ?></td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+
 </table>
 <br />
 <br />
 <br />
 
-<table cellspacing=10 cellpadding=0 style="text-align: right; border: none;">
-<tr>
-  <td style="text-align: left; width: 600px; border: none; font-size:20px;"><?php echo getLastSentence(); ?></td>
-</tr>
+<table cellspacing="10" cellpadding="0" border="0" style="text-align: right;">
+	<tr>
+		<td style="text-align: left; width: 600px; border: none; font-size:20px;"><?php echo getLastSentence(); ?></td>
+	</tr>
 </table>
 
--- project/apps/civa/modules/vrac_export/templates/_soussignes.php
+++ project/apps/civa/modules/vrac_export/templates/_soussignes.php
@@ -1,107 +1,107 @@
 <?php use_helper('vracExport') ?>
 <br/>
 <br/>
-<table border="0">
-    <tr>
-        <td>
-            <span style="background-color: grey; color: white; font-weight: bold;">&nbsp;VENDEUR&nbsp;</span><br/>
-            <table style="border: 1px solid grey; width: 310px;">
-                            <tr>
-                                <td valign="top" width="90px">Raison Sociale :</td>
-                                <td><i><?php echo truncate_text($vrac->vendeur->raison_sociale); ?></i></td>
-                            </tr>
-                            <tr>
-                                <td valign="top" width="90px">N° CVI :</td>
-                                <td><i><?php echo $vrac->vendeur->cvi; ?></i></td>
-                            </tr>
-                            <tr>
-                                <td valign="top" width="90px">Adresse :</td>
-								<td><i><?php echo truncate_text($vrac->vendeur->adresse, 50, "...", false) ?></i></td>
-                            </tr>
-                            <tr>
-                            	<td valign="top" width="90px"></td>
-                                <td><i><?php echo $vrac->vendeur->code_postal; ?></i>&nbsp;<i><?php echo $vrac->vendeur->commune; ?></i></td>
-                            </tr>
-                			<tr>
-                            	<td valign="top" width="90px">Tel. :</td>
-                    			<td><i><?php echo $vrac->vendeur->telephone; ?></i></td>
-                			</tr>
-                			<tr>
-	                			<td valign="top" width="90px">SIRET :</td>
-	                			<td><i><?php echo $vrac->vendeur->siret; ?></i></td>
-	                		</tr>
-	                		<tr>
-	                			<td valign="top" width="90px">N° d'Accise :</td>
-	                			<td><i><?php echo $vrac->vendeur->num_accise; ?></i></td>  
-	                		</tr>
-            </table>
-        </td>
-        <td>
-            <span style="background-color: grey; color: white; font-weight: bold;">&nbsp;ACHETEUR&nbsp;</span><br/>
-            <table style="border: 1px solid grey; width: 310px;">
-                            <tr>
-                                <td valign="top" width="90px">Raison Sociale :</td>
-                                <td><i><?php echo $vrac->acheteur->raison_sociale; ?></i></td>
-                            </tr>
-                            <tr>
-                                <td valign="top" width="90px">N° CVI :</td>
-                                <td><i><?php echo $vrac->acheteur->cvi; ?></i></td>
-                            </tr>
-                            <tr>
-                                <td valign="top" width="90px">Adresse :</td>
-								<td><i><?php echo $vrac->acheteur->adresse ?></i></td>
-                            </tr>
-                            <tr>
-                            	<td valign="top" width="90px"></td>
-                                <td><i><?php echo $vrac->acheteur->code_postal; ?></i>&nbsp;<i><?php echo $vrac->acheteur->commune; ?></i></td>
-                            </tr>
-                			<tr>
-                            	<td valign="top" width="90px">Tel. :</td>
-                    			<td><i><?php echo $vrac->acheteur->telephone; ?></i></td>
-                			</tr>
-                			<tr>
-	                			<td valign="top" width="90px">SIRET :</td>
-	                			<td><i><?php echo $vrac->acheteur->siret; ?></i></td>
-	                		</tr>
-	                		<tr>
-	                			<td valign="top" width="90px">N° d'Accise :</td>
-	                			<td><i><?php echo $vrac->acheteur->num_accise; ?></i></td>  
-	                		</tr>
-            </table>
-        </td>
-    </tr>
+<table cellpadding="0" cellspacing="0" border="0" width="100%" style="border-collapse: collapse;">
+	<tr>
+		<td width="50%" valign="top" style="padding-right: 10px;">
+			<span style="display:block; background-color: grey; color: white; font-weight: bold; padding: 5px; text-align: center; width: 120px;">&nbsp;VENDEUR&nbsp;</span>
+			<table cellpadding="5" cellspacing="0" border="0" width="100%" style="border: 1px solid grey;">
+				<tr>
+					<td valign="top" width="100px">Raison Sociale :</td>
+					<td><i><?php echo truncate_text($vrac->vendeur->raison_sociale); ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">N° CVI :</td>
+					<td><i><?php echo $vrac->vendeur->cvi; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">Adresse :</td>
+					<td><i><?php echo truncate_text($vrac->vendeur->adresse, 50, "...", false) ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px"></td>
+					<td><i><?php echo $vrac->vendeur->code_postal; ?></i>&nbsp;<i><?php echo $vrac->vendeur->commune; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">Tel. :</td>
+					<td><i><?php echo $vrac->vendeur->telephone; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">SIRET :</td>
+					<td><i><?php echo $vrac->vendeur->siret; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">N° d'Accise :</td>
+					<td><i><?php echo $vrac->vendeur->num_accise; ?></i></td>  
+				</tr>
+			</table>
+		</td>
+		<td width="50%" valign="top">
+			<span style="display: block; background-color: grey; color: white; font-weight: bold; padding: 5px; text-align: center; width: 120px;">&nbsp;ACHETEUR&nbsp;</span>
+			<table cellpadding="5" cellspacing="0" width="100%" style="border: 1px solid grey;">
+				<tr>
+					<td valign="top" width="100px">Raison Sociale :</td>
+					<td><i><?php echo $vrac->acheteur->raison_sociale; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">N° CVI :</td>
+					<td><i><?php echo $vrac->acheteur->cvi; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">Adresse :</td>
+					<td><i><?php echo $vrac->acheteur->adresse ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px"></td>
+					<td><i><?php echo $vrac->acheteur->code_postal; ?></i>&nbsp;<i><?php echo $vrac->acheteur->commune; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">Tel. :</td>
+					<td><i><?php echo $vrac->acheteur->telephone; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">SIRET :</td>
+					<td><i><?php echo $vrac->acheteur->siret; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">N° d'Accise :</td>
+					<td><i><?php echo $vrac->acheteur->num_accise; ?></i></td>  
+				</tr>
+			</table>
+		</td>
+	</tr>
 </table>
 <br/>
-<table border="0">
-    <tr>
-        <td>
-            <span style="background-color: grey; color: white; font-weight: bold;">&nbsp;COURTIER&nbsp;</span><br/>
-            <table style="border: 1px solid grey; width: 630px;">
-            				<tr>
-	                			<td valign="top" width="90px">Raison Sociale :</td>
-	                			<td><i><?php echo $vrac->mandataire->raison_sociale; ?></i></td> 
-	                			<td valign="top" width="90px">N° Carte Pro :</td>
-	                			<td><i><?php echo $vrac->mandataire->carte_pro; ?></i></td> 
-            				</tr>
-                            <tr>
-                                <td valign="top" width="90px">Adresse :</td>
-								<td><i><?php echo $vrac->mandataire->adresse ?></i></td>
-                                <td valign="top" width="90px">SIRET :</td>
-								<td><i><?php echo $vrac->mandataire->siret ?></i></td>
-                            </tr>
-                            <tr>
-                            	<td valign="top" width="90px"></td>
-                                <td><i><?php echo $vrac->mandataire->code_postal; ?></i>&nbsp;<i><?php echo $vrac->mandataire->commune; ?></i></td>
-                            	<td valign="top" width="90px">Interlocuteur Comm. :</td>
-                    			<td><i><?php echo $vrac->interlocuteur_commercial; ?></i></td>
-                            </tr>
-                			<tr>
-                            	<td valign="top" width="90px">Tel. :</td>
-                    			<td><i><?php echo $vrac->mandataire->telephone; ?></i></td>
-                    			<td></td>
-                    			<td></td>
-                			</tr>
-            </table>
-        </td>
-    </tr>
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+	<tr>
+		<td width="100%">
+			<span style="display: block; background-color: grey; color: white; font-weight: bold; padding: 5px; text-align: center; width: 120px;">&nbsp;COURTIER&nbsp;</span>
+			<table cellpadding="5" cellspacing="0" width="100%" style="border: 1px solid grey;">
+				<tr>
+					<td valign="top" width="100px">Raison Sociale :</td>
+					<td><i><?php echo $vrac->mandataire->raison_sociale; ?></i></td> 
+					<td valign="top" width="150px">N° Carte Pro :</td>
+					<td><i><?php echo $vrac->mandataire->carte_pro; ?></i></td> 
+				</tr>
+				<tr>
+					<td valign="top" width="100px">Adresse :</td>
+					<td><i><?php echo $vrac->mandataire->adresse ?></i></td>
+					<td valign="top" width="150px">SIRET :</td>
+					<td><i><?php echo $vrac->mandataire->siret ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px"></td>
+					<td><i><?php echo $vrac->mandataire->code_postal; ?></i>&nbsp;<i><?php echo $vrac->mandataire->commune; ?></i></td>
+					<td valign="top" width="150px">Interlocuteur Comm. :</td>
+					<td><i><?php echo $vrac->interlocuteur_commercial; ?></i></td>
+				</tr>
+				<tr>
+					<td valign="top" width="100px">Tel. :</td>
+					<td><i><?php echo $vrac->mandataire->telephone; ?></i></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+		</td>
+	</tr>
 </table>
