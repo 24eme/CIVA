292bccfeb43178ef5ab8bd0d202b0ccc78adc70a
Vincent LAURENT <vince.laurent@gmail.com>
2013-10-30 12:42:29 +0100

    Suppression du test

--- project/apps/civa/modules/declaration/templates/sendPdfAcheteursSuccess.php
+++ project/apps/civa/modules/declaration/templates/sendPdfAcheteursSuccess.php
@@ -2,7 +2,7 @@
         Votre déclaration de récolte a bien été envoyée par email aux acheteurs suivant :
         <ul>
         <?php foreach ($sendMailAcheteursReport as $acheteurReport) : ?>
-            <li><?php echo $acheteurReport->email ?>, <?php echo $acheteurReport->type; ?>, <?php echo $acheteurReport->nom ?>, <?php echo $acheteurReport->cvi; ?></li>
+            <li><?php echo $acheteurReport->type; ?>, <?php echo $acheteurReport->nom ?>, <?php echo $acheteurReport->cvi; ?></li>
         <?php endforeach; ?>
         </ul>
        
