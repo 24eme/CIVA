d9c86d8e32ca10cd05f73ebdf67c7723717e1474
Vincent LAURENT <vlaurent@actualys.com>
2013-10-31 17:00:08 +0100

    Suppression du bloc de migration de CVI

--- project/apps/civa/modules/tiers/templates/_monEspaceDr.php
+++ project/apps/civa/modules/tiers/templates/_monEspaceDr.php
@@ -12,14 +12,4 @@
             <div class="contenu clearfix">
                 <?php include_component('tiers', 'delegationForm', array('form' => isset($formDelegation) ? $formDelegation : null)) ?>
             </div>
-        <?php endif;?>	
-
-        <?php if($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
-        <div id="espace_admin">
-            <h2>Espace Admin</h2>
-            <div class="contenu clearfix">
-                <a  href="<?php echo url_for('@migration_compte')?>">Migration compte</a>
-
-            </div>
-        </div>
-        <?php endif; ?>
+        <?php endif;?>
