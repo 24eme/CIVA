a2cfe54232235325850d55d51ee7ea94dad794ad
Vincent LAURENT <vlaurent@actualys.com>
2013-11-14 20:25:18 +0100

    cf commit précédent

--- project/apps/civa/modules/email/templates/_header.php
+++ project/apps/civa/modules/email/templates/_header.php
@@ -1,5 +1,6 @@
 Contrat vrac du <?php echo strftime('%d/%m/%Y', strtotime($vrac->valide->date_saisie)) ?>
 
+
 Vendeur : <?php echo ($vrac->vendeur->intitule)? $vrac->vendeur->intitule.' '.$vrac->vendeur->raison_sociale : $vrac->vendeur->raison_sociale ?>
 
 Acheteur : <?php echo ($vrac->acheteur->intitule)? $vrac->acheteur->intitule.' '.$vrac->acheteur->raison_sociale : $vrac->acheteur->raison_sociale ?><?php if (!$vrac->hasCourtier() && $vrac->interlocuteur_commercial->nom): ?> (votre interlocuteur : <?php echo $vrac->interlocuteur_commercial->nom ?>)<?php endif; ?>
