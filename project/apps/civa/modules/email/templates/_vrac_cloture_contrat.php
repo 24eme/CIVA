b06919842216d2f90d32e926efcb91ec9bb49e14
Vincent LAURENT <vlaurent@actualys.com>
2013-11-14 20:37:50 +0100

    base url depuis la conf dans les emails de validation et cloture

--- project/apps/civa/modules/email/templates/_vrac_cloture_contrat.php
+++ project/apps/civa/modules/email/templates/_vrac_cloture_contrat.php
@@ -5,6 +5,6 @@ Vous trouverez ci-joint la version pdf avec les volumes rééls.
 
 Nous vous invitons à bien conserver ce document, preuve de la transaction passée entre les différentes parties.
 
-Il sera également accessible sur la plateforme du CIVA à l'adresse suivante : <?php echo ProjectConfiguration::getAppRouting()->generate('vrac_fiche', array('sf_subject' => $vrac), true); ?>
+Il sera également accessible sur la plateforme du CIVA à l'adresse suivante : <?php echo sfConfig::get('app_base_url').ProjectConfiguration::getAppRouting()->generate('vrac_fiche', array('sf_subject' => $vrac)); ?>
 
 <?php include_partial('email/footer', array('vrac' => $vrac)); ?>
