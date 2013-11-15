0b77cff656faff46fe04dacf95efc39a0d1931ee
Vincent LAURENT <vlaurent@actualys.com>
2013-11-15 20:25:11 +0100

    Mise en forme fatale sur l'annexe du pdf

--- project/apps/civa/modules/vrac_export/templates/_annexe.php
+++ project/apps/civa/modules/vrac_export/templates/_annexe.php
@@ -1,6 +1,5 @@
 <div style="font-size: 7pt;">
-<h2 style="text-align: center;"><b>CONTRAT  DE VENTE EN VRAC DE VINS AOC PRODUITS  EN ALSACE</b></h2>
-<h3><b>Art. D644-6 du Code Rural modifié par le décret 2008-998 du 23/9/08</b></h3>
+<h4><b>Art. R644-6 du Code Rural modifié par le décret 2008-998 du 23/9/08</b></h4>
 <p>
 L’article susmentionné rend obligatoire l’information des organismes de contrôle agréés sur les intentions de transactions de vins non conditionnés.
 </p>
@@ -8,11 +7,12 @@ L’article susmentionné rend obligatoire l’information des organismes de con
 Cette obligation a été reprise par les cahiers des charges de l’ensemble des AOC Alsace.
 </p>
 
-<h3><b>Art.5 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</b><i>(arrêté ministériel d’extension du 13/12/2012)</i></h3>
+<h4>Art.5 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
+<i>(arrêté ministériel d’extension du 13/12/2012)</i>
  
 <p>
 Toute transaction de vin AOC produit en Alsace, entre deux opérateurs du vignoble AOC, donne lieu dès la conclusion de la vente et quelle que
-soit la nature de l’opérateur, à l’établissement immédiat d’un contrat de vente conforme au contrat type interprofessionnel
+soit la nature de l’opérateur, à l’établissement immédiat d’un contrat de vente conforme au contrat type interprofessionnel.
 </p>
 <p>
 Le contrat fait apparaître, l’appellation, le cépage (<i>ou s’il s’agit d’un assemblage, la désignation Edelzwicker</i>), les dénominations spécifiques susceptibles d’être revendiquées, le volume et le prix unitaire. Ce dernier s’entend net, c’est-à-dire hors taxes et tous escomptes déduits, la cotisation interprofessionnelle ainsi que les commissions de courtage étant à régler séparément.
@@ -21,7 +21,7 @@ Le contrat fait apparaître, l’appellation, le cépage (<i>ou s’il s’agit 
 Afin de garantir le suivi des transactions en vrac, notamment pour l’établissement par le CIVA des mercuriales bimensuelles, le nombre de contrats et le volume correspondant devront être reportés sur les DRM (déclarations récapitulatives mensuelles des produits en droits suspendus) <b>de l’acheteur et du vendeur.</b>
 </p>
 
-<h3>Transmission et enregistrement par le CIVA</h3>
+<h4>Transmission et enregistrement par le CIVA</h4>
 
 <p>
 Le contrat n’est recevable et ne peut être dûment enregistré par le CIVA que s’il comporte effectivement le VOLUME et le PRIX de la transaction pour chaque cépage.
@@ -33,23 +33,23 @@ C’est l’acheteur ou son courtier qui transmet au CIVA les 2 exemplaires du c
 Le contrat peut être établi et transmis de manière dématérialisée via la plateforme déclarative du portail interprofessionnel du CIVA. Dans ce cas  toutes les parties sont invitées à signer électroniquement le contrat dans un délai maximum de 5 jours. 
 </p>
 
-<h3>Visa par le CIVA</h3>
+<h4>Visa par le CIVA</h4>
 <p>
-Conformément aux art. L665-2, D665-30 et D665-1 du Code Rural, les transactions portant sur des produits issus de la vigne, conclues au stade de la  1ère commercialisation sur le territoire national, font l’objet d’un contrat visé par l’organisation Interprofessionnelle compétente si un accord interprofessionnel étendu le prévoit. Ce visa est délivré dans les plus brefs délais.
+Conformément aux art. L665-2, R665-30 et R665-1 du Code Rural, les transactions portant sur des produits issus de la vigne, conclues au stade de la  1ère commercialisation sur le territoire national, font l’objet d’un contrat visé par l’organisation interprofessionnelle compétente si un accord interprofessionnel étendu le prévoit. Ce visa est délivré dans les plus brefs délais.
 </p>
 <p>
 L’art. 5 du 13e accord interprofessionnel du CIVA précise que le CIVA adresse au déposant, immédiatement et au plus tard dans les 10 jours, un récépissé revêtu de son visa. 
 Dans le cas d’un contrat dématérialisé, le CIVA adressera à toutes les parties après signature, le pdf du contrat revêtu du numéro du Visa du CIVA. 
 </p>
 
-<h3>Art.12 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h3>
+<h4>Art.12 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
 <p>
 « Les producteurs-négociants, négociants et SICA paieront au CIVA la cotisation interprofessionnelle au taux en vigueur sur la totalité de leurs ventes en bouteilles dont la moitié, quelque soit le cépage, sera supportée par leurs vendeurs (qu’ils soient viticulteurs, coopératives ou négociants) et retenue à ceux-ci sur tous les achats de vins et de raisins. »
 </p>
 <p>
 La retenue sur les achats de vin en vrac correspond à la moitié de la cotisation interprofessionnelle en vigueur, plus la TVA.
 </p>
-<h3>Article L 632-7 du Code Rural modifié par loi 2010-874 du 27/7/2010</h3>
+<h4>Article L632-7 du Code Rural modifié par loi 2010-874 du 27/07/2010</h4>
 <p>
 « Tout contrat de fourniture de produits, passé entre personnes physiques ou morales ressortissant à un accord étendu et qui n’est pas conforme aux dispositions de cet accord, est nul de plein droit. L’organisation interprofessionnelle dans le cadre de laquelle a été conclu l’accord ainsi que chacune des organisations professionnelles qui la constituent, sont recevables à demander la reconnaissance de cette nullité au juge du contrat. »
 </p>
@@ -59,6 +59,10 @@ La retenue sur les achats de vin en vrac correspond à la moitié de la cotisati
 <p>
 « Dans tous les cas, la mise en œuvre des sanctions prévues à l’alinéa précédent ne fait pas obstacle à l’application éventuelle de celles prévues par les contrats de fourniture ainsi que par les règlements intérieurs des groupements coopératifs agricoles en cause, en cas de défaut d’exécution des clauses de ces règlements. »
 </p>
-
-<h3 style="text-align: center;">CETTE DECLARATION EST PROTÉGÉE PAR LE SECRET PROFESSIONNEL ET LES PRINCIPES DE CONFIDENTIALITE</h3>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h4 style="text-align: center; font-weight: normal;">CETTE DÉCLARATION EST PROTÉGÉE PAR LE SECRET <br /> PROFESSIONNEL ET LES PRINCIPES DE CONFIDENTIALITÉ</h4>
 </div>
