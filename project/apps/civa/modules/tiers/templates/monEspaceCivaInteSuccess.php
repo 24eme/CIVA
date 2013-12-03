d874586484ac60b1eab5be46112c0c9aab806c7d
Mikael Guillin <mguillin@actualys.com>
2013-12-03 14:38:47 +0100

    Intégration de l'espace civa

---
+++ project/apps/civa/modules/tiers/templates/monEspaceCivaInteSuccess.php
@@ -0,0 +1,50 @@
+<h2 class="titre_principal">Mon espace déclaratif</h2>
+
+<div id="application_dr" class="mon_espace_civa clearfix">
+
+	<ul id="onglets_majeurs" class="clearfix">
+		<li class="ui-tabs-selected"><a href="#">Accueil</a></li>
+		<li><a href="#">Alsace récolte</a></li>
+		<li><a href="#">Alsace Contrats</a></li>
+		<li><a href="#">Espace G@mma</a></li>
+		<li><a href="#">Alsace Stocks</a></li>
+	</ul>
+
+	<div class="contenu">
+		<h3>Vos échéances</h3>
+		
+		<table class="table_listing">
+			<thead>
+				<tr>
+					<th>Type</th>
+					<th>Echéance</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="recolte">
+					<td>Alsace Récolte</td>
+					<td class="alt">31/08/13</td>
+					<td><a href="#" class="vert">Valider</a></td>
+				</tr>
+				<tr class="contrats">
+					<td>Alsace Contrats</td>
+					<td class="alt">15/09/13</td>
+					<td><a href="#" class="orange">22 contrats à valider</a></td>
+				</tr>
+				<tr class="gamma">
+					<td>Espace G@mma</td>
+					<td class="alt">10/11/13</td>
+					<td><a href="#" class="rouge">Visualiser</a></td>
+				</tr>
+				<tr class="stocks">
+					<td>Alsace Stocks</td>
+					<td class="alt"></td>
+					<td><a href="#" class="vert">Accéder</a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+
