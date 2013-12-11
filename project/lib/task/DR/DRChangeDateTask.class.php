f7ce5cb72f7ed7ac705079c2610694de4ce1e929
Vincent LAURENT <vlaurent@actualys.com>
2013-12-11 17:48:22 +0100

    Bash pour remplacer les dates supérieur a une certaine date

--- project/lib/task/DR/DRChangeDateTask.class.php
+++ project/lib/task/DR/DRChangeDateTask.class.php
@@ -5,11 +5,15 @@ class DRChangeDateTask extends sfBaseTask
 
     protected function configure()
     {
+        $this->addArguments(array(
+          new sfCommandArgument('id', sfCommandArgument::REQUIRED, 'id'),
+          new sfCommandArgument('date', sfCommandArgument::REQUIRED, 'date'),
+        ));
+
         $this->addOptions(array(
             new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'civa'),
             new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
-            new sfCommandOption('date', null, sfCommandOption::PARAMETER_REQUIRED, 'Date to be changed', null)
         ));
 
         $this->namespace = 'dr';
@@ -30,30 +34,29 @@ EOF;
       $databaseManager = new sfDatabaseManager($this->configuration);
       $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
       
-      $strdate = $options['date'];
-      if(!preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $strdate)) {
-	throw new sfException("wrong date format");
+      $dr = DRClient::getInstance()->find($arguments['id']);
+
+      $date = $arguments['date'];
+
+      if(!$dr) {
+        echo "ERREUR;".$dr->_id.";La DR n'existe pas\n";
+        return;
       }
-      $date = strtotime($strdate);
-      
-      $drs = acCouchdbManager::getClient()->reduce(false)->getView("STATS", "DR")->rows;
-      
-      foreach ($drs as $obj) {
-	
-	if (!$obj->key[0])
-	  continue;
-	$dr_id = $obj->id;
-	$dr_date = $obj->key[2];
-	
-	if (strtotime($dr_date) <= $date)
-	  continue;
-	
-	$dr = acCouchdbManager::getClient()->find($dr_id);
-	
-	echo $dr->_id." changed (previous date $dr_date)\n";
-	$dr->validee = $strdate;
-	$dr->save();
+
+      if(!$dr->isValideeCiva()) {
+
+        return;
       }
+
+      if($dr->validee <= $date) {
+        return;
+      }
+
+      $dr->validee = $date;
+      
+      echo "INFO;".$dr->_id.";Date de validation passé au ".$date."\n";
+
+      $dr->save();
     }
     
 }
