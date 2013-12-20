db067ca3dbe505c299adb97d43e7b85ef469b3a7
Vincent LAURENT <vlaurent@actualys.com>
2013-12-20 14:42:53 +0100

    Export des jeunes vignes

--- project/bin/export_drs_ventes_raisins.sh
+++ project/bin/export_drs_ventes_raisins.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+ANNEE=$1
+
+if ! test "$ANNEE"; then
+	echo "L'année de déclaration de récolte est requise"
+	exit
+fi
+
 echo "cvi_vendeur;appellation;cvi_acheteur;type;volume;superficie;dontdplc"
-bash bin/export_drs_ventes.sh 2013 | grep -E ";(negoces|cooperatives);" | sort
+bash bin/export_drs_ventes.sh $ANNEE | grep -E ";(negoces|cooperatives);" | sort
