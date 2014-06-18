f4055966e6b6124e5c9755a1edd31a8cceb4f710
Vincent LAURENT <vlaurent@actualys.com>
2014-06-18 18:12:04 +0200

    Ajout de l'accès aux tâches dans l'admin

--- project/apps/civa/modules/admin/templates/_export.php
+++ project/apps/civa/modules/admin/templates/_export.php
@@ -1,6 +1,7 @@
-<h3 class="titre_section">Export CSV</h3>
+<h3 class="titre_section">Taches et exports</h3>
 <div class="contenu_section">
     <ul>
+        <li><a href="<?php echo url_for('@task_list') ?>">Tâches récurrentes</a></li>
         <li><a href="<?php echo url_for('@csv_comptes') ?>">Comptes</a></li>
         <li><a href="<?php echo url_for('@csv_tiers_dr_en_cours') ?>">DR en cours</a></li>
         <li><a href="<?php echo url_for('@ds_export_csv_en_cours') ?>">DS en cours</a></li>
