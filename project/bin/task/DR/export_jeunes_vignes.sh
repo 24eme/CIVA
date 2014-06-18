3160ec039f17cc4e39996cb3678b5217f66ec2b2
Vincent LAURENT <vlaurent@actualys.com>
2014-06-18 17:59:20 +0200

    Addaptation des tâche DR à l'inclusion

--- project/bin/task/DR/export_jeunes_vignes.sh
+++ project/bin/task/DR/export_jeunes_vignes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-. bin/config.inc
+. bin/task.inc
 
 DESCRIPTION="Export CSV des superficies de jeunes vignes des DR $DR_CAMPAGNE"
 
-. bin/task.inc
+. bin/task_start.inc
 
 mkdir $TASK_DIR/tmp > /dev/null
 
@@ -12,4 +12,4 @@ EXPORT_FILE=tmp/export_drs_jeunes_vignes.csv
 
 bash bin/export_drs_jeunes_vignes.sh $DR_CAMPAGNE > $TASK_DIR/$EXPORT_FILE
 
-echo "Télécharger le fichier : $TASK_URL/$EXPORT_FILE"
+echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE)"
