39a2a2829a8b3042b11b65d3bfe627e578cbed8f
Vincent LAURENT <vlaurent@actualys.com>
2014-06-18 17:49:10 +0200

    Fixe bug de js dans les taches récurrentes

--- project/apps/civa/modules/task/templates/infoSuccess.php
+++ project/apps/civa/modules/task/templates/infoSuccess.php
@@ -8,7 +8,6 @@
     <div class="btns">
         <a class="tache-popup btn_majeur btn_petit btn_jaune" href="<?php echo url_for('task_run', array('namespace' => $task->getNamespace(), 'slug' => $task->getSlug())) ?>" data-loader="#popup_loader" >Exécuter</a>
     </div>
-    
     <div style="display: none" id="popup_loader" class="popup_tache" title="<?php echo $task->getNamespace(); ?>">
         <h2><?php echo $task->getName(); ?></h2>
         <div class="popup-loading input">
