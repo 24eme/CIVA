e5d6700b6c2c1f0e177dd9532e67ff1c10c734f7
Vincent LAURENT <vlaurent@actualys.com>
2014-09-12 10:14:41 +0200

    Fixe template fermé

--- project/apps/civa/modules/ds/templates/_monEspaceNonEditable.php
+++ project/apps/civa/modules/ds/templates/_monEspaceNonEditable.php
@@ -1,13 +1,10 @@
 <?php use_helper('ds'); ?>
-<h3 class="titre_section">Déclaration de <?php echo getPeriodeFr($sf_user->getPeriodeDS()) ?><a href="" class="msg_aide_ds" rel="help_popup_mon_espace_civa_ma_ds<?php if($type_ds == DSCivaClient::TYPE_DS_NEGOCE): ?>_negoce<?php endif; ?>" title="Message aide"></a></h3> <span class="label_type_ds"><?php echo strtoupper($type_ds); ?>
+<h3 class="titre_section">Déclaration de <?php echo getPeriodeFr($sf_user->getPeriodeDS()) ?><a href="" class="msg_aide_ds" rel="help_popup_mon_espace_civa_ma_ds<?php if($type_ds == DSCivaClient::TYPE_DS_NEGOCE): ?>_negoce<?php endif; ?>" title="Message aide"></a></h3> <span class="label_type_ds"><?php echo strtoupper($type_ds); ?></span>
 <div class="contenu_section">
     <p class="intro"><?php echo acCouchdbManager::getClient('Messages')->getMessage('intro_mon_espace_civa_ds_non_editable'); ?></p>
     <div class="ligne_form ligne_btn">
         <?php if($ds && $ds->isValideeTiers()): ?>
             <?php echo link_to('<img src="/images/boutons/btn_visualiser.png" alt="" class="btn" />', 'ds_visualisation',$ds); ?>
-        <?php endif; ?>
-        <?php if($ds->isTypeDsPropriete()): ?>
-        <a class="btn_majeur btn_petit btn_jaune" href="<?php echo url_for('ds_export_pdf_empty', array('type' => $type_ds)); ?>" style="margin-top: 20px;">Télécharger mon brouillon</a>
         <?php endif; ?>
     </div>
 </div>
