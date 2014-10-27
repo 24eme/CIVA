4020bb7841545eb8486209199cd082a1fd22cedd
Vincent LAURENT <vlaurent@actualys.com>
2014-10-27 18:06:52 +0100

    Correction du bug quand négo pas de superficie

--- project/apps/civa/modules/upload/actions/actions.class.php
+++ project/apps/civa/modules/upload/actions/actions.class.php
@@ -92,8 +92,8 @@ class uploadActions extends EtapesActions {
             if ($this->shouldHaveVolume($line)) {
                 $this->errors[$cpt - 1][] = 'Il existe des volumes sans surfaces alors qu\'aucun assemblage n\'a été déclaré';
             }
-            if ($this->shouldHaveSuperficieTotal($line)) {
-                $this->errors[$cpt - 1][] = "La superficie totale de l'appellation AOC Alsace Blanc est nulle alors que de l'assemblage a été déclaré";
+            /*if ($this->shouldHaveSuperficieTotal($line)) {
+                $this->errors[$cpt - 1][] = "La superficie totale de l'appellation AOC Alsace Blanc est nulle alors que de l'assemblage a été déclaré";*/
             }
             if ($this->errorOnCVIAcheteur($line)) {
                 $this->errors[$cpt][] = 'Le CVI de la colonne acheteur ne correspond pas à celui de l\'utilisateur connecté';
@@ -144,9 +144,9 @@ class uploadActions extends EtapesActions {
         if ($this->shouldHaveVolume(false)) {
             $this->errors[$cpt][] = 'Il existe des volumes sans surfaces alors qu\'aucun assemblage n\'a été déclaré';
         }
-        if ($this->shouldHaveSuperficieTotal(false)) {
+        /*if ($this->shouldHaveSuperficieTotal(false)) {
             $this->errors[$cpt][] = "La superficie totale de l'appellation AOC Alsace Blanc est nulle alors que de l'assemblage a été déclaré";
-        }
+        }*/
 
         $this->recap = new stdClass();
         $this->recap->errors = array();
