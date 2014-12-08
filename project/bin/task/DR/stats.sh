85097d8a06c037798b8677916a8eaa4458d9f5ae
Vincent LAURENT <vlaurent@actualys.com>
2014-12-08 18:04:34 +0100

    Tache d'export des stats DR

---
+++ project/bin/task/DR/stats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. bin/task.inc
+
+DESCRIPTION="Statistiques des déclarations de Récolte $DR_CAMPAGNE"
+
+. bin/task_start.inc
+
+mkdir -m 777 $TASK_DIR/tmp > /dev/null
+
+EXPORT_FILE=tmp/dr_statistiques_"$DR_CAMPAGNE"_$(date +%Y%m%d%H%M%S).csv
+
+php symfony dr:stats-recolte $DR_CAMPAGNE | sed 's/\./,/g' > $TASK_DIR/$EXPORT_FILE
+
+echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE)"
+
---
+++ project/bin/task/DR/stats_mairies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. bin/task.inc
+
+DESCRIPTION="Statistiques des déclarations de Récolte par Mairie $DR_CAMPAGNE"
+
+. bin/task_start.inc
+
+mkdir -m 777 $TASK_DIR/tmp > /dev/null
+
+EXPORT_FILE=tmp/dr_statistiques_mairies_"$DR_CAMPAGNE"_$(date +%Y%m%d%H%M%S).csv
+
+php symfony dr:stats-recolte-mairie $DR_CAMPAGNE | sed 's/\./,/g' > $TASK_DIR/$EXPORT_FILE
+
+echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE)"
