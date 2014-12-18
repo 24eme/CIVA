4da60a7f58150e07fcac01f6ccfbbfe16d8caa66
Vincent LAURENT <vlaurent@actualys.com>
2014-12-18 18:31:27 +0100

    export des vtsgn oublié

---
+++ project/bin/export_drs_vtsgn_oubli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. bin/config.inc
+
+ANNEE=$1
+
+if ! test "$ANNEE"; then
+    echo "L'année de déclaration de récolte est requise"
+    exit;
+fi
+
+echo '"CVI acheteur";"nom acheteur";"CVI recoltant";"nom recoltant";"appellation";"lieu";"cepage";"vtsgn";"denomination";"superficie";"volume";"dont volume a detruire";"superficie totale";"volume total";"volume a detruire total";"date de creation";"date de validation";"validateur";"hash_produit"'
+
+bash bin/export_drs_csv.sh $ANNEE | grep -v ';"VT";' | grep -v ';"SGN"' | grep -Ei "(séléction|selection|grain| nobles|vendange|tardive|sgn|vt|v\.t|s\.g\.n)"
