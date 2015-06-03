2b86206173195480e20cb64efd6ce17bdf820fa1
Vincent LAURENT <vince.laurent@gmail.com>
2015-06-03 12:12:54 +0200

    Export recoltant sans dr verif fichier

--- project/bin/export_recoltant_sans_dr.sh
+++ project/bin/export_recoltant_sans_dr.sh
@@ -3,12 +3,20 @@
 . bin/config.inc
 
 ANNEE=$1
+FILE_TIERS=data/import/Tiers/Tiers-last
 
 if ! test "$ANNEE"; then
 echo "L'année de déclaration de récolte est requise"
 exit;
 fi
 
+
+
+if [ ! -e $FILE_TIERS ]; then
+echo "Le fichier $FILE_TIERS n'existe pas"
+exit;
+fi
+
 echo "CVI;NOM"
 
-cat data/import/Tiers/Tiers-last | awk -F ',' '{ print $24 ";" $58 ";" $7 }' | sed 's/"//g' | grep "O" | cut -d ";" -f 2,3 | grep -E "^6" | sed -r 's/[ ]+/ /' | while read ligne; do CVI=$(echo $ligne | cut -d ";" -f 1); curl -s $COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/DR-$CVI-$ANNEE | grep "not_found" | sed "s|^|$ligne|" | cut -d "{" -f 1; done
+cat $FILE_TIERS | awk -F ',' '{ print $24 ";" $58 ";" $7 }' | sed 's/"//g' | grep "O" | cut -d ";" -f 2,3 | grep -E "^6" | sed -r 's/[ ]+/ /' | while read ligne; do CVI=$(echo $ligne | cut -d ";" -f 1); curl -s $COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/DR-$CVI-$ANNEE | grep "not_found" | sed "s|^|$ligne|" | cut -d "{" -f 1; done
