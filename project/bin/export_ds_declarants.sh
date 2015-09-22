1fac0d1bbb5b5ddb70dbed4f20eb418eba0ebb9c
Vincent LAURENT <vince.laurent@gmail.com>
2015-09-22 23:51:34 +0200

    Entetes export declarant

--- project/bin/export_ds_declarants.sh
+++ project/bin/export_ds_declarants.sh
@@ -2,7 +2,7 @@
 
 . bin/config.inc
 
-echo "Type de la DS;DS déclaré;Login;Email;CVI;CIVABA;Catégorie;Qualité;Nom;Commune;Compte ID;Tiers ID;Mail Envoyé;Commentaires";
+echo "Type de la DS;DS déclaré;Login;Email;CVI;CIVABA;Catégorie;Qualité;Nom;Commune;Compte ID;Tiers ID";
 
 curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/COMPTE/_view/tous" | cut -d "," -f 1 | sed 's/{"id":"//' | sed 's/"//' | grep "COMPTE" | while read ligne  
 do
