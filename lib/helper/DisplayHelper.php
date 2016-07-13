124ddf66d1eb3eb5f2320b7d7622dacbd5e82e7d
Vincent LAURENT <vince.laurent@gmail.com>
2016-07-14 00:10:23 +0200

    Squashed 'project/plugins/acVinLibPlugin/' changes from e7027d5..92ae013
    92ae013 Merge branch 'mutualisation'
    81661c9 Import EDI bootstrapisation et adaptation
    1e24ee3 Prix HT dans le tableau des factures
    3deb912 separateur de millier
    27bb209 Le form des pays en bootstrap
    b058b15 Merge branch 'master' of github.com:24eme/giilda
    7074330 maj des pdfs de contrats
    280e886 Gestion de bug PDF
    7f05df5 Résolution du pb de filtre de l'autocomplete ajax des etablissements
    5ae0530 Correction pdf header drm
    3aace75 Gestion du label sous forme de tableau clé valeur et ajout par défaut dans la conf
    9baa004 hamza style mouvements
    8b2b39c Widget integer
    525c4cd Utilisation du bsWidgetFloat avec décimal par défaut
    80cac8f Gestion du nombre de décimal pour les floats dans la conf
    a349b4f Utilisation du plugin forma input jquery dans la DRM
    c682551 Formatage des nombres
    09d90cb Sprint 3 Contrat PDF IVSO
    67fd8eb Merge commit 'b2f5abd543f7a9d5de582a6f3512da9440ebf1e9'
    b2f5abd Merge commit 'd13b03232e61206796ca6d458f8315e0e0856b0f'
    d13b032 Merge commit 'e3d3c32221d0e7e66d52fcb8e96aad8f29e69b95'
    353f6fb Remove warnings in dev mode
    02e1a96 bsWidgetDatePicker
    758eb3c Activation du cache en mode dev
    070e472 Sprint 2
    d609901 Bloc dans la page soussigné
    fa941ae Icon dans la liste des types de transaction
    b27f099 Inline à true par défaut
    4e93de4 Fix bootsrap formatter et format renderRow et renderLabel
    22f63a9 form extends BaseForm
    c1f9815 Customisation des erreurs
    dfaab20 Bootstrap form select et form select radio
    5bb5da9 Bootstrap WidgetFormChoice expanded
    28a9db8 Squashed commit of the following:
    447d751 ajout des dateTime dans conversion date iso to fr
    8d52b85 Merge branch 'master' of gitorious.org:acvin/acvinlibplugin
    88681fb on enleve la fct d'affichage pour le Display => elle est placée dans VracHelper
    0cd9ecc ajout d'une fct pour l'affichage des contrats
    2e83dac validator pass maison
    6447fe2 suppression round
    7c6ce94 correction format
    b37776a Ajout de code pour format float
    0dae20a Merge branch 'master' of gitorious.org:acvin/acvinlibplugin
    de7b323 Ajout d'une exception pour erreur http 401
    
    git-subtree-dir: project/plugins/acVinLibPlugin
    git-subtree-split: 92ae013ac490cd55ff895a1b328d82688210c384

--- lib/helper/DisplayHelper.php
+++ lib/helper/DisplayHelper.php
@@ -1,11 +1,28 @@
 <?php
 
+function display_cvi_formatted($cvi){
+    if(is_null($cvi) || !$cvi){
+        echo '';
+    }
+    if(!preg_match('/[0-9]{10}/', $cvi)){
+        echo $cvi;
+    }
+    echo preg_replace('/([0-9]{2})([0-9]{3})([0-9]{5})/','\1 \2 \3' , $cvi);
+}
+
 function display_field($object, $fieldName) {
     if (is_null($object)) {
         echo '';
         return;
     }
     echo (!is_null($object->$fieldName)) ? $object->$fieldName : '';
+}
+
+function get_field($object, $fieldName) {
+    if (is_null($object)) {
+        return '';
+    }
+    return (!is_null($object->$fieldName)) ? $object->$fieldName : '';
 }
 
 function escape_string_for_latex($string) {
