1300665af2351d9a318127ebd62c9f66b38d4a57
Vincent LAURENT <vince.laurent@gmail.com>
2016-10-19 19:42:50 +0200

    Mise en forme de onglets pour une gestion multiligne et pour accueillir les multiples sous onglets VT et SGN

--- project/apps/civa/modules/dr_recolte/templates/_ongletsAppellations.php
+++ project/apps/civa/modules/dr_recolte/templates/_ongletsAppellations.php
@@ -1,16 +1,29 @@
 <ul id="onglets_majeurs" class="clearfix onglets_recolte">
     <?php foreach($appellations as $appellation): ?>
         <li <?php if (preg_match("|".$appellation['hash']."/|", $produit->getMention()->getHash()."/")): ?>class="ui-tabs-selected"<?php endif; ?>>
-            <a href="<?php echo url_for('dr_recolte_noeud', array('id' => $declaration->_id, 'hash' => $appellation['hash'])) ?>"><?php echo str_replace('AOC', '<span>AOC</span> <br />',$appellation['libelle']) ?></a>
+            <a href="<?php echo url_for('dr_recolte_noeud', array('id' => $declaration->_id, 'hash' => $appellation['hash'])) ?>"><?php echo str_replace("Mention", "<span>Mention</span> <br />", str_replace('AOC Alsace', '<span>AOC Alsace</span> <br />',$appellation['libelle'])) ?></a>
             <?php if (count($appellation['lieux']) > 0 && preg_match("|".$appellation['hash']."/|", $produit->getMention()->getHash()."/")): ?>
-            <?php include_partial('ongletsLieux', array('declaration' => $declaration,
+            <?php /*include_partial('ongletsLieux', array('declaration' => $declaration,
                                                         'produit' => $produit,
                                                         'items' =>  $appellation['lieux'],
-                                                        'appellation_key' => $produit->getAppellation()->getKey())); ?>
+                                                        'appellation_key' => $produit->getAppellation()->getKey()));*/ ?>
             <?php endif; ?>
         </li>
     <?php endforeach; ?>
         <?php if (!$declaration->recolte->getNoeudAppellations()->hasAllAppellation()): ?>
         <li class="ajouter ajouter_appelation"><a href="#">Ajouter une<br /> appellation</a></li>
         <?php endif; ?>
+</ul>
+<br />
+<ul id="onglets_majeurs" class="clearfix onglets_recolte">
+    <li class="ui-tabs-selected">
+    <?php foreach($appellations as $appellation): ?>
+            <?php if (count($appellation['lieux']) > 0 && preg_match("|".$appellation['hash']."/|", $produit->getMention()->getHash()."/")): ?>
+            <?php include_partial('ongletsLieux', array('declaration' => $declaration,
+                                                        'produit' => $produit,
+                                                        'items' =>  $appellation['lieux'],
+                                                        'appellation_key' => $produit->getAppellation()->getKey())); ?>
+            <?php endif; ?>
+    <?php endforeach; ?>
+    </li>
 </ul>
--- project/apps/civa/modules/dr_recolte/templates/_ongletsLieux.php
+++ project/apps/civa/modules/dr_recolte/templates/_ongletsLieux.php
@@ -1,10 +1,13 @@
 <ul class="sous_onglets">
-	<?php $first = true ?>
     <?php foreach($items as $key => $lieu): ?>
-        <li class="<?php if ($produit->getLieu()->getHash() == $lieu->getHash()): ?>ui-tabs-selected<?php endif; ?> <?php if ($first):?>premier<?php endif; ?>">
-            <a href="<?php echo url_for('dr_recolte_noeud', array('id' => $produit->getDocument()->_id, 'hash' => $lieu->getHash())) ?>"><?php if($lieu->getMention()->getKey() != "mention"): ?><?php echo $lieu->getAppellation()->getLibelle() ?> <?php endif; ?><?php echo $lieu->getLibelle() ?></a>
+        <li class="<?php if ($produit->getLieu()->getHash() == $lieu->getHash()): ?>ui-tabs-selected<?php endif; ?>">
+            <a href="<?php echo url_for('dr_recolte_noeud', array('id' => $produit->getDocument()->_id, 'hash' => $lieu->getHash())) ?>">
+				<?php if($lieu->getMention()->getKey() != "mention"): ?>
+					<?php echo ucfirst(preg_replace("/(AOC|Alsace) /", "", str_replace("Grands Crus", "Grd Cru ", $lieu->getAppellation()->getLibelle()))) ?>
+				<?php endif; ?>
+				<?php echo $lieu->getLibelle() ?>
+			</a>
         </li>
-		<?php $first = false ?>
     <?php endforeach; ?>
 
     <?php if (!$declaration->recolte->getNoeudAppellations()->get($appellation_key)->hasAllDistinctLieu()): ?>
