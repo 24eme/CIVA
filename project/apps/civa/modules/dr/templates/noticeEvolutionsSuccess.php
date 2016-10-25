9c19bf442b27b8d51b4093f4939e20fbd28cf7db
Vincent LAURENT <vince.laurent@gmail.com>
2016-10-25 18:34:13 +0200

    Correction notice évolutions

--- project/apps/civa/modules/dr/templates/noticeEvolutionsSuccess.php
+++ project/apps/civa/modules/dr/templates/noticeEvolutionsSuccess.php
@@ -5,8 +5,8 @@
 
 		<ol style="font-size: 14px; padding-left: 5px;">
 		    <li style="padding-top: 10px;">
-		        Les rendements maximum de l’AOC Alsace et du Crémant d’Alsace ont été porté à 83 hl/ha.<br /><br />
-				<span style="padding-left: 20px;">ATTENTION, les rendements maximum Riesling, Pinot Gris et Gewuztraminer restent néanmoins à 80 hl/ha</span>
+		        Les rendements maximum de l’AOC Alsace et du Crémant d’Alsace ont été porté à <strong>83 hl/ha</strong>.<br /><br />
+				<span style="padding-left: 20px;">ATTENTION, les rendements maximum Riesling, Pinot Gris et Gewuztraminer restent néanmoins à <strong>80 hl/ha</strong>.</span>
 		    </li>
 		    <li style="padding-top: 10px;">
 				Un rendement spécifique est mis en place à compter de cette année pour les mentions VT et SGN :<br />
@@ -18,8 +18,18 @@
 		    </li>
 		</ol>
 		<br />
+		<a href="<?php echo url_for("dr_telecharger_la_notice"); ?>" style="height: 20px; display:none" class="telecharger-btn"></a>
 		<br />
-		<a href="<?php echo url_for("dr_telecharger_la_notice"); ?>" style="height: 20px; display: none;" class="telecharger-btn"></a>
+		<br />
+		<br />
+		<h2>Rappel</h2>
+		<div style="font-size: 14px; padding-left: 5px;">
+			<p>Depuis la récolte 2012, le volume à inscrire en entrée est le <u>volume total récolté</u>, c’est-à-dire <u>lies et bourbes comprises</u> (même si des soutirages ont déjà été effectués).</p>
+
+			<p>La rubrique "Volume à détruire" comprend à la fois les lies et les volumes éventuels en dépassement de rendement (sans distinction).</p>
+
+			<p>La gestion des lies peut se faire globalement par appellation <u>ou</u> par cépage (cf écran "Répartition"). Si vous optez pour la gestion par cépage, vous inscrivez vos lies connues dans la ligne "Volume à détruire" et le système vous calculera automatiquement le volume revendiqué par cépage.</p>
+		</div>
 	</div>
 	<?php include_partial('dr/boutons', array('display' => array('precedent','suivant'), 'dr' => $dr)) ?>
 </form>
