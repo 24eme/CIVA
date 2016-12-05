cd6af5e930ae3915b81dea33b1d370f60f9922ae
Vincent LAURENT <vince.laurent@gmail.com>
2016-12-05 14:06:10 +0100

    Méthode mot de passe ssha pour les anciens comptes

--- project/lib/model/couchdb/_Compte/_Compte.class.php
+++ project/lib/model/couchdb/_Compte/_Compte.class.php
@@ -21,6 +21,17 @@ abstract class _Compte extends Base_Compte {
      *
      * @param string $mot_de_passe
      */
+    public function setMotDePasseSSHA($mot_de_passe) {
+        mt_srand((double) microtime() * 1000000);
+        $salt = pack("CCCC", mt_rand(), mt_rand(), mt_rand(), mt_rand());
+        $hash = "{SSHA}" . base64_encode(pack("H*", sha1($mot_de_passe . $salt)) . $salt);
+        $this->_set('mot_de_passe', $hash);
+    }
+
+    /**
+     *
+     * @param string $mot_de_passe
+     */
     public function setPasswordSSHA($mot_de_passe) {
         mt_srand((double)microtime()*1000000);
         $salt = pack("CCCC", mt_rand(), mt_rand(), mt_rand(), mt_rand());
