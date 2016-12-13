ba0459ed191b21a2225e3f7ab7e914682727d325
Vincent LAURENT <vince.laurent@gmail.com>
2016-12-13 09:40:31 +0100

    Correction dans le rail d'étape

--- project/apps/civa/modules/dr/templates/_etapes.php
+++ project/apps/civa/modules/dr/templates/_etapes.php
@@ -5,21 +5,21 @@
         <a href="<?php echo url_for('dr_exploitation', $dr); ?>">Exploitation <em>Etape 1</em></a>
     </li>
     <li class="<?php if ($etape > 2): ?>passe<?php elseif($etape == 2): ?>actif<?php endif; ?>">
-        <?php if ($etapesConfig->isAutorized($dr->etape, "repartition")): ?>
+        <?php if ($dr->exist('etape') && $etapesConfig->isAutorized($dr->etape, "repartition")): ?>
             <a href="<?php echo url_for('dr_repartition', $dr); ?>">Répartition <em>Etape 2</em></a>
         <?php else: ?>
             <a href="#" onclick="return false;">Répartition <em>Etape 2</em></a>
         <?php endif; ?>
     </li>
     <li class="<?php if ($etape > 3): ?>passe<?php elseif($etape == 3): ?>actif<?php endif; ?>">
-        <?php if ($etapesConfig->isAutorized($dr->etape, "recolte")): ?>
+        <?php if ($dr->exist('etape') && $etapesConfig->isAutorized($dr->etape, "recolte")): ?>
             <a href="<?php echo url_for('dr_recolte', $dr); ?>">Récolte <em>Etape 3</em></a>
         <?php else: ?>
              <a href="#" onclick="return false;">Récolte <em>Etape 3</em></a>
         <?php endif; ?>
     </li>
     <li class="<?php if ($etape > 4): ?>passe<?php elseif($etape == 4): ?>actif<?php endif; ?>">
-        <?php if ($etapesConfig->isAutorized($dr->etape, "validation")): ?>
+        <?php if ($dr->exist('etape') && $etapesConfig->isAutorized($dr->etape, "validation")): ?>
             <a href="<?php echo url_for('dr_validation', $dr); ?>">Validation <em>Etape 4</em></a>
         <?php else: ?>
             <a href="#" onclick="return false;">Validation <em>Etape 4</em></a>
