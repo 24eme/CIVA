78cfab36f29de1426d68f9bc15ae5068a09c0f44
Vincent LAURENT <vince.laurent@gmail.com>
2016-12-13 18:05:26 +0100

    Correction d'une coquille dans le nom du fichier

--- project/bin/build_tiers_file_from_db2.sh
+++ project/bin/build_tiers_file_from_db2.sh
@@ -3,7 +3,7 @@
 PATH_DB2_ACTIF=$1
 PATH_DB2_CLOTURE=$2
 
-PATH_TIERS=data/import/Tiers/$(date +%Y%m%d)
+PATH_TIERS=data/import/Tiers/Tiers-$(date +%Y%m%d)
 PATH_TIERS_LAST=data/import/Tiers/Tiers-last
 cat $PATH_DB2_ACTIF $PATH_DB2_CLOTURE | iconv -f ISO-8859-1 -t utf-8 > $PATH_TIERS
 echo "Copie et conversion en utf-8 de $1 et $2 dans $PATH_TIERS"
