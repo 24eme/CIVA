0e9164dc257c1f0f492525435e52e441f4adfdba
Vincent LAURENT <vince.laurent@gmail.com>
2016-12-13 18:19:09 +0100

    Exclusion des cloture pour l'export recoltant sans DR

--- project/bin/export_recoltant_sans_dr.sh
+++ project/bin/export_recoltant_sans_dr.sh
@@ -19,4 +19,4 @@ fi
 
 echo "CVI;NOM"
 
-cat $FILE_TIERS | awk -F ',' '{ print $24 ";" $58 ";" $7 }' | sed 's/"//g' | grep "^O;" | cut -d ";" -f 2,3 | grep -E "^6" | sed -r 's/[ ]+/ /' | while read ligne; do CVI=$(echo $ligne | cut -d ";" -f 1); curl -s $COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/DR-$CVI-$ANNEE | grep "not_found" | sed "s|^|$ligne|" | cut -d "{" -f 1; done
+cat $FILE_TIERS | awk -F ',' '{ if($36 == "\"O\"") { next; } print $24 ";" $58 ";" $7 }' | sed 's/"//g' | grep "^O;" | cut -d ";" -f 2,3 | grep -E "^6" | sed -r 's/[ ]+/ /' | while read ligne; do CVI=$(echo $ligne | cut -d ";" -f 1); curl -s $COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/DR-$CVI-$ANNEE | grep "not_found" | sed "s|^|$ligne|" | cut -d "{" -f 1; done
