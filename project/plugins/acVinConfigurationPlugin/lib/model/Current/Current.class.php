336259a51e593ed928b87a33a398e38927f14a6c
Vincent LAURENT <vince.laurent@gmail.com>
2017-07-12 19:16:18 +0200

    Ouverture de la DS 201707

--- project/plugins/acVinConfigurationPlugin/lib/model/Current/Current.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/model/Current/Current.class.php
@@ -64,7 +64,7 @@ class Current extends BaseCurrent {
             return CurrentClient::getInstance()->getCurrentFromTheFuture()."07";
         }
 
-        return "201612";
+        return "201712";
     }
 
     public function getAnneeDS($type_ds = null)
