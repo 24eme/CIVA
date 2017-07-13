2b7e5119fb95d78c065b0e0f5f9434d13078546d
Vincent LAURENT <vince.laurent@gmail.com>
2017-07-13 09:09:39 +0200

    Adaptation de la tâche d'envoi des ds à la nouvelle structure de comptes

--- project/lib/task/DS/DSSendMailOuvertureTask.class.php
+++ project/lib/task/DS/DSSendMailOuvertureTask.class.php
@@ -11,7 +11,7 @@ class DSSendBrouillonTask extends sfBaseTask
         // // add your own arguments here
         $this->addArguments(array(
             new sfCommandArgument('periode', sfCommandArgument::REQUIRED, 'periode'),
-            new sfCommandArgument('id_compte', sfCommandArgument::REQUIRED, 'id_compte'),
+            new sfCommandArgument('id_etablissement', sfCommandArgument::REQUIRED, 'id_etablissement'),
         ));
 
         $this->addOptions(array(
@@ -40,84 +40,58 @@ class DSSendBrouillonTask extends sfBaseTask
         $this->periode = $arguments['periode'];
         $this->is_decembre = preg_match("/12$/", $this->periode);
 
-        $compte = _CompteClient::getInstance()->find($arguments["id_compte"]);
+        $etablissement = CompteClient::getInstance()->find($arguments["id_etablissement"], acCouchdbClient::HYDRATE_JSON);
 
-        if(!$compte){
-
-            return;
-        }
-        if($compte->type != "CompteTiers") {
+        if(!$etablissement){
 
             return;
         }
 
-        if(!$compte->isActif()){
+        if($etablissement->statut != EtablissementClient::STATUT_ACTIF){
 
             return;
         }
 
-        if(!$compte->isInscrit()){
+        $etablissement = CompteClient::getInstance()->find($arguments["id_etablissement"]);
 
-            return;
+        if($etablissement->hasDroit(Roles::TELEDECLARATION_DS_PROPRIETE) && !$etablissement->hasDroit(Roles::TELEDECLARATION_DS_NEGOCE)) {
+
+            $this->executeSendMail($etablissement, DSCivaClient::TYPE_DS_PROPRIETE, $options['dryrun']);
         }
 
-        if(!$compte->email){
+        if($etablissement->hasDroit(Roles::TELEDECLARATION_DS_NEGOCE)) {
 
-            return;
+            $this->executeSendMail($etablissement, DSCivaClient::TYPE_DS_NEGOCE, $options['dryrun']);
         }
 
-        if(!$compte->hasDroit(_CompteClient::DROIT_DS_DECLARANT)) {
-            return;
-        }
-
-        $this->executeSendMail($compte, DSCivaClient::TYPE_DS_PROPRIETE, $options['dryrun']);
-        $this->executeSendMail($compte, DSCivaClient::TYPE_DS_NEGOCE, $options['dryrun']);
     }
 
-    public function executeSendMail($compte, $type_ds, $dryrun = false)
+    public function executeSendMail($etablissement, $type_ds, $dryrun = false)
     {
-        $tiers = $compte->getDeclarantDS($type_ds);
-
-        if(!$tiers) {
-
-            return;
-        }
+        $tiers = $etablissement;
 
         if($this->is_decembre && (!$tiers->exist('ds_decembre') || !$tiers->ds_decembre)) {
 
             return;
         }
 
-        if(!$tiers->hasLieuxStockage() && !$tiers->isAjoutLieuxDeStockage()) {
+        if(!$etablissement->hasLieuxStockage() && !$etablissement->isAjoutLieuxDeStockage()) {
 
             echo $type_ds.";ERROR;PAS DE LIEU DE STOCKAGE;".$compte->_id."\n";
             return;
         }
 
-        // Exclusion des ds négoce des comptes acheteurs inscrit sur le metteur en marché
-        if($tiers->type == "MetteurEnMarche" && $tiers->cvi && $tiers->cvi == $compte->login) {
-            $tiersAchat = AcheteurClient::getInstance()->findByCvi($tiers->cvi);
-            if($tiersAchat) {
-                $compteAchat = $tiersAchat->getCompteObject();
-                $compteMetteurEnMarche = $tiers->getCompteObject();
 
-                if($compte->_id == $compteAchat->_id && $compteMetteurEnMarche->isInscrit() && $compteMetteurEnMarche->email && $compteMetteurEnMarche->isActif() && $compteMetteurEnMarche->hasDroit(_CompteClient::DROIT_DS_DECLARANT)) {
-
-                    return;
-                }
-            }
-        }
-
-        $previousPeriode=(preg_replace("/[0-9]{2}$/", "", $this->periode)-1).preg_replace("/^[0-9]{4}/", "", $this->periode);
-        $ds = $tiers->getDs($previousPeriode);
+        $previousDateStock=(preg_replace("/[0-9]{2}$/", "", $this->periode)-1)."-".preg_replace("/^[0-9]{4}/", "", $this->periode)."-31";
+        $ds = DSCivaClient::getInstance()->getDSPrincipale($tiers, $previousDateStock);
         $teledeclarant = ($ds && (!$ds->exist("date_depot_mairie") || !$ds->get("date_depot_mairie")));
         $recuperationDoc = ($type_ds == DSCivaClient::TYPE_DS_PROPRIETE);
-        $email = $compte->email;
+        $email = $etablissement->getEmailTeledeclaration();
 
-        $log = sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $type_ds, $teledeclarant, $recuperationDoc, !is_null($ds), $compte->login, $compte->email, $tiers->cvi, $tiers->civaba, $tiers->categorie, $tiers->qualite_categorie, $tiers->nom, $tiers->siege->commune, $compte->_id, $tiers->_id);
+        $log = sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $type_ds, $teledeclarant, $recuperationDoc, !is_null($ds), $email, $tiers->cvi, $tiers->num_interne, $tiers->famille, $tiers->raison_sociale, $tiers->commune, $tiers->_id);
 
-        if($dryrun) {
-            echo $log.";0\n";
+        if(!$email || $dryrun) {
+            echo $log.";".boolval($email)."\n";
             return;
         }
 
@@ -152,20 +126,22 @@ class DSSendBrouillonTask extends sfBaseTask
         sleep(1);
     }
 
-    protected function configureMessage($message, $type_ds, $teledeclarant) {
+    protected function configureMessage($message, $type_ds, $teledeclarant, $campagne) {
+
+        $campagne = preg_replace("/[0-9]{2}$/", "", $this->periode);
 
         if($type_ds == DSCivaClient::TYPE_DS_PROPRIETE && $teledeclarant) {
 
-            $message->setSubject("Déclaration de Stocks \"Propriété\" au 31 juillet 2016")
+            $message->setSubject("Déclaration de Stocks \"Propriété\" au 31 juillet ".$campagne)
                     ->setBody("Bonjour,
 
-Vous avez télé-déclaré votre Stock 2015 sur le Portail du CIVA <https://declaration.vinsalsace.pro> et nous n'avons donc pas pré-identifié de formulaire pour votre entreprise en Mairie.
+Vous avez télé-déclaré votre Stock ".($campagne - 1)." sur le Portail du CIVA <https://declaration.vinsalsace.pro> et nous n'avons donc pas pré-identifié de formulaire pour votre entreprise en Mairie.
 
-Si vous optez à nouveau pour cette solution, la procédure pour la télé-déclaration des Stocks au 31 juillet 2016 sera accessible à compter du 18 juillet et vous n'avez donc aucun document à remettre en Mairie.
+Si vous optez à nouveau pour cette solution, la procédure pour la télé-déclaration des Stocks au 31 juillet ".$campagne." sera accessible à compter du 18 juillet et vous n'avez donc aucun document à remettre en Mairie.
 
 Attention la date limite de la télé-déclaration est fixée par les Douanes au 10 septembre minuit.
 
-Pour vous aider dans votre démarche vous trouverez ci-joint un brouillon personnalisé de votre DS 2016, qui reprend les produits théoriquement détenus en stocks.
+Pour vous aider dans votre démarche vous trouverez ci-joint un brouillon personnalisé de votre DS ".$campagne.", qui reprend les produits théoriquement détenus en stocks.
 
 Ce document constitue une aide à la télé-déclaration et n'est en aucun cas à retourner au CIVA.
 
@@ -182,16 +158,16 @@ Le CIVA");
             $attachment = new Swift_Attachment(file_get_contents(sfConfig::get('sf_data_dir')."/pdf/votre_declaration_de_stocks_pas_a_pas_propriete.pdf"), "votre_declaration_de_stocks_pas_a_pas_propriete.pdf", 'application/pdf');
             $message->attach($attachment);
 
-            $message->setSubject("Déclaration de Stocks \"Propriété\" au 31 juillet 2016")
+            $message->setSubject("Déclaration de Stocks \"Propriété\" au 31 juillet ".$campagne)
                     ->setBody("Bonjour,
 
-En 2015, vous avez déposé une Déclaration de Stocks \"papier\", nous avons donc envoyé en Mairie un formulaire pré-identifié pour votre entreprise.
+En ".($campagne - 1).", vous avez déposé une Déclaration de Stocks \"papier\", nous avons donc envoyé en Mairie un formulaire pré-identifié pour votre entreprise.
 
-Si néanmoins, vous souhaitez cette année télé-déclarer votre Stock au 31 juillet 2016 sur le Portail CIVA <https://declaration.vinsalsace.pro>, le télé-service \"Alsace Stocks\" sera accessible à compter du 18 juillet et vous n'aurez donc aucun document à remettre en Mairie.
+Si néanmoins, vous souhaitez cette année télé-déclarer votre Stock au 31 juillet ".$campagne." sur le Portail CIVA <https://declaration.vinsalsace.pro>, le télé-service \"Alsace Stocks\" sera accessible à compter du 18 juillet et vous n'aurez donc aucun document à remettre en Mairie.
 
 Attention la date limite de télé-déclaration est fixée par les Douanes au 10 septembre minuit.
 
-Pour vous aider dans votre démarche vous trouverez ci-joint, un document explicatif \"Pas à pas\", ainsi qu'un brouillon personnalisé de votre DS 2016, qui reprend les produits théoriquement détenus en stocks.
+Pour vous aider dans votre démarche vous trouverez ci-joint, un document explicatif \"Pas à pas\", ainsi qu'un brouillon personnalisé de votre DS ".$campagne.", qui reprend les produits théoriquement détenus en stocks.
 
 Ce document constitue une aide à la télé-déclaration et n'est en aucun cas à retourner au CIVA.
 
@@ -205,10 +181,10 @@ Le CIVA");
 
         if($type_ds == DSCivaClient::TYPE_DS_NEGOCE && $teledeclarant) {
 
-            $message->setSubject("Déclaration de Stocks \"Négoce\" au 31 juillet 2016")
+            $message->setSubject("Déclaration de Stocks \"Négoce\" au 31 juillet ".$campagne)
                     ->setBody("Bonjour,
 
-Vous avez télé-déclaré votre Stock 2015 sur le Portail du CIVA <https://declaration.vinsalsace.pro> et nous ne vous avons donc pas fait parvenir de formulaire pré-identifié.
+Vous avez télé-déclaré votre Stock ".($campagne - 1)." sur le Portail du CIVA <https://declaration.vinsalsace.pro> et nous ne vous avons donc pas fait parvenir de formulaire pré-identifié.
 
 Si vous optez à nouveau pour cette solution, le télé-service \"Alsace Stocks\" sera accessible du 18 juillet au 10 septembre inclus, et vous n'avez donc pas de formulaire papier à envoyer au CIVA.
 
@@ -227,10 +203,10 @@ Le CIVA");
             $attachment = new Swift_Attachment(file_get_contents(sfConfig::get('sf_data_dir')."/pdf/votre_declaration_de_stocks_pas_a_pas_negoce.pdf"), "votre_declaration_de_stocks_pas_a_pas_negoce.pdf", 'application/pdf');
             $message->attach($attachment);
 
-            $message->setSubject("Déclaration de Stocks \"Négoce\" au 31 juillet 2016")
+            $message->setSubject("Déclaration de Stocks \"Négoce\" au 31 juillet ".$campagne)
                     ->setBody("Bonjour,
 
-Vous recevrez dans les prochains jours votre Déclaration de Stocks au 31 juillet 2016 à retourner au CIVA au plus tard le 10 septembre.
+Vous recevrez dans les prochains jours votre Déclaration de Stocks au 31 juillet ".$campagne." à retourner au CIVA au plus tard le 10 septembre.
 
 Depuis 2014 vous avez la possibilité de télé-déclarer sur le Portail CIVA <https://declaration.vinsalsace.pro>, votre Stock au 31 Juillet voire celui au 31 Décembre si vous êtes concerné.
 
