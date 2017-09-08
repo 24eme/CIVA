769ec0715292d32d47af290cb8abf2a86df4fef5
Vincent LAURENT <vince.laurent@gmail.com>
2017-09-08 16:55:02 +0200

    Tache d'envoi du mail de rappel de validation

--- project/lib/task/DS/DSSendMailRappelTask.class.php
+++ project/lib/task/DS/DSSendMailRappelTask.class.php
@@ -5,13 +5,14 @@ class DSSendMailRappelTask extends sfBaseTask
 
     protected $debug = false;
     protected $periode = null;
-    
+
     protected function configure()
     {
         // // add your own arguments here
         $this->addArguments(array(
-            new sfCommandArgument('periode', sfCommandArgument::REQUIRED, 'periode'),
-            new sfCommandArgument('id_compte', sfCommandArgument::REQUIRED, 'id_compte'),
+            new sfCommandArgument('periode', sfCommandArgument::REQUIRED, "Période"),
+            new sfCommandArgument('identifiant', sfCommandArgument::REQUIRED, "Identifiant de l'établissement"),
+            new sfCommandArgument('type_ds', sfCommandArgument::REQUIRED, 'Type de la DS'),
         ));
 
         $this->addOptions(array(
@@ -38,167 +39,104 @@ EOF;
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
         sfContext::createInstance($this->configuration);
         set_time_limit(0);
-        ini_set('memory_limit', '512M');
-        if(!array_key_exists('periode',$arguments) || !preg_match('/^([0-9]{6})$/', $arguments['periode'])){
-            throw new sfException("La periode doit être passé en argument et doit être de la forme AAAAMM");
-        }
-        $this->periode = $arguments['periode'];
 
-        $compte = _CompteClient::getInstance()->find($arguments["id_compte"]);
-
-        if(!$compte){
-            
-            return;
+        $etablissement = EtablissementClient::getInstance()->findByIdentifiant($arguments['identifiant']);
+        if(!$etablissement) {
+            $etablissement = EtablissementClient::getInstance()->findByCvi($arguments['identifiant']);
         }
-        if($compte->type != "CompteTiers") {
-            
+
+        if(!$etablissement) {
+            echo "Établissement non trouvé : ".$arguments['id_etablissement']."\n";
             return;
         }
 
-        if(!$compte->isActif()){
-            
+        if(!$etablissement->hasDroit('teledeclaration_ds_'.$arguments['type_ds'])) {
+            echo "L'établissement ne fait pas de ds ".$arguments['type_ds']." : ".$arguments['id_etablissement']."\n";
+            return;
+        }
+        $ds = DSCivaClient::getInstance()->findPrincipaleByEtablissementAndPeriode($arguments['type_ds'], $etablissement, $arguments['periode']);
+
+        if(!$ds) {
+            echo "La DS n'existe pas : ".$arguments['identifiant']."\n";
             return;
         }
 
-        if(!$compte->isInscrit()){
-            
+        if($ds->exist("date_depot_mairie") && $ds->get("date_depot_mairie")) {
+            echo "Il s'agit d'une DS papier : ".$ds->_id."\n";
             return;
         }
 
-        if(!$compte->email){
-            
+        if($ds->isValideeTiers()) {
+            echo "La DS est déjà validée : ".$ds->_id."\n";
             return;
         }
 
-        if(!$compte->hasDroit(_CompteClient::DROIT_DS_DECLARANT)) {
-            return;
-        }
-
-        $this->executeSendMail($compte, DSCivaClient::TYPE_DS_PROPRIETE, $options['dryrun']);
-        $this->executeSendMail($compte, DSCivaClient::TYPE_DS_NEGOCE, $options['dryrun']);
+        $this->sendMailValidation($etablissement, $ds, $options['dryrun']);
     }
-    
-    public function executeSendMail($compte, $type_ds, $dryrun = false)
-    {   
-        $tiers = $compte->getDeclarantDS($type_ds);
 
-        if(!$tiers) {
-
-            return;
-        }
-
-        if(!$tiers->hasLieuxStockage() && !$tiers->isAjoutLieuxDeStockage()) {
-
-            //echo $type_ds.";ERROR;PAS DE LIEU DE STOCKAGE;".$compte->_id."\n";
-            return;
-        }
-
-        // Exclusion des ds négoce des comptes acheteurs inscrit sur le metteur en marché 
-        if($tiers->type == "MetteurEnMarche" && $tiers->cvi && $tiers->cvi == $compte->login) {
-            $tiersAchat = AcheteurClient::getInstance()->findByCvi($tiers->cvi);
-            if($tiersAchat) {
-                $compteAchat = $tiersAchat->getCompteObject();
-                $compteMetteurEnMarche = $tiers->getCompteObject();
-
-                if($compte->_id == $compteAchat->_id && $compteMetteurEnMarche->isInscrit() && $compteMetteurEnMarche->email && $compteMetteurEnMarche->isActif() && $compteMetteurEnMarche->hasDroit(_CompteClient::DROIT_DS_DECLARANT)) {
-
-                    return;
-                }
-            }
-        }
-
-        $dsCurrent = $tiers->getDs(CurrentClient::getCurrent()->ds_periode."");
-        $ds = $tiers->getDs($this->periode);
-        $teledeclarant = ($ds && (!$ds->exist("date_depot_mairie") || !$ds->get("date_depot_mairie")));
-        $enCours = ($dsCurrent && !$dsCurrent->isValideeTiers());
-
-        if($dsCurrent && $dsCurrent->isValideeTiers()) {
-
-            return;
-        }
-
-        if(!$teledeclarant && !$enCours) {
-
-            return;
-        }
-
-        $email = $compte->email;
-
-        $log = sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s", $type_ds, $teledeclarant, $enCours, !is_null($ds), $compte->login, $compte->email, $tiers->cvi, $tiers->civaba, $tiers->categorie, $tiers->qualite_categorie, $tiers->nom, $tiers->siege->commune, $compte->_id, $tiers->_id);
-
-        if($dryrun) {
-            echo $log.";0\n";
-            return;
-        }
-
+    public function sendMailValidation($etablissement, $ds, $dryrun) {
+        $email = $ds->declarant->get('email');
         $message = Swift_Message::newInstance()
-                ->setFrom(array('dominique@civa.fr' => "Dominique Wolff"))
-                ->setTo($email);
+            ->setFrom(array('dominique@civa.fr' => "Dominique Wolff"))
+            ->setTo($email)
+            ->setSubject("RAPPEL DS au 31 juillet ".date('Y'))
+            ->setBody("Bonjour,
 
-        $this->configureMessage($message, $teledeclarant, $enCours);
-
-        try {
-            $this->getMailer()->send($message);
-        } catch (Exception $e) {
-            echo $log.";0;".$e->getMessage()."\n";
-            return false;
-        }
-
-        echo $log.";1\n";
-
-        sleep(1);
-    }
-
-    protected function configureMessage($message, $teledeclarant, $enCours) {
-
-        if($enCours) {
-
-            $message->setSubject("RAPPEL DS au 31 juillet 2015")
-                    ->setBody("Bonjour,
-
-Vous avez commencé à saisir en ligne votre Déclaration de Stocks 2015 sur le site VinsAlsace.pro, mais ne l'avez pas encore validée.
+Vous avez commencé à saisir en ligne votre Déclaration de Stocks ".date('Y')." sur le site VinsAlsace.pro, mais ne l'avez pas encore validée.
 
 Nous vous rappelons que vous devez impérativement la valider AVANT le 10 septembre minuit.
 
 Pour terminer la saisie, cliquez sur le lien suivant : <https://declaration.vinsalsace.pro>
 
-Si vous avez souscrit une déclaration papier, merci de m'en informer par retour de mail.
 
-
-Cordialement, 
+Cordialement,
 
 Dominique WOLFF");
 
-            return $message;
+        try {
+            if($dryrun) {
+                throw new sfException("Dry run");
+            }
+            $this->getMailer()->send($message);
+        } catch (Exception $e) {
+            echo "L'envoi du mail validation a échoué (".$email."): ".$e->getMessage()." (".$ds->_id.")\n";
+            return false;
         }
 
-        if(!$enCours) {
+        echo "Mail validation envoyé (".$email.") : ".$ds->_id."\n";
+        sleep(1);
+    }
 
-            $message->setSubject("RAPPEL DS au 31 juillet 2015")
-                    ->setBody("Bonjour,
+    public function sendMailOubli($etablissement) {
+        $message = Swift_Message::newInstance()
+        ->setFrom(array('dominique@civa.fr' => "Dominique Wolff"))
+        ->setTo($etablissement->email)
+        ->setSubject("RAPPEL DS au 31 juillet ".date('Y'))
+        ->setBody("Bonjour,
 
-En 2014 vous avez télé-déclaré votre Stock sur le Portail du CIVA.
+La Déclaration de Stocks \"Papier\" a définitivement disparue en 2017 et vous devez donc désormais télé-déclarer votre Stock sur le Portail du CIVA.
 
-A ce jour nous n'avons pas enregistré de saisie pour celle au 31 Juillet 2015.
+A ce jour nous n'avons pas enregistré de saisie pour la déclaration au 31 Juillet ".date('Y').".
 
-Nous vous rappelons que vous devez impérativement déposer ou télé-déclarer votre Stock AVANT le 10 septembre minuit.
+Nous vous rappelons que vous devez impérativement télé-déclarer votre Stock AVANT le 10 septembre minuit.
 
-Vous pouvez effectuer la saisie en cliquant sur le lien suivant <https://declaration.vinsalsace.pro> 
-
-Si vous avez souscrit une déclaration papier, merci de m'en informer par retour de mail.
+Vous pouvez effectuer la saisie en cliquant sur le lien suivant <https://declaration.vinsalsace.pro>
 
 
-Cordialement, 
+Cordialement,
 
-Dominique WOLFF");
+Dominique WOLFF
+");
 
-            return $message;
+        try {
+            $this->getMailer()->send($message);
+        } catch (Exception $e) {
+            echo "L'envoi du mail oubli a échoué : ".$e->getMessage()." (".$etablissement->_id.")\n";
+            return false;
         }
+
+        echo "Mail oubli envoyé : ".$ds->_id."\n";
+        sleep(1);
     }
-    
-    public function getPartial($templateName, $vars = null) {
-        $this->configuration->loadHelpers('Partial');
-        $vars = null !== $vars ? $vars : $this->varHolder->getAll();
-        return get_partial($templateName, $vars);
-    }
+
 }
