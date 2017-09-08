de79ca82935dd88b736b9300f10d1e3b5e3da22e
Vincent LAURENT <vince.laurent@gmail.com>
2017-09-08 10:23:58 +0200

    Ajout du nom dans l'export ce fichier

--- project/bin/export_declarant_dr_sur_place_sans_ds.sh
+++ project/bin/export_declarant_dr_sur_place_sans_ds.sh
@@ -15,11 +15,11 @@ if ! test "$PERIODE_DS"; then
     exit;
 fi
 
-bash bin/export_drs_csv.sh $ANNEE_DR | grep "SUR PLACE" | grep -v "Jeunes Vignes" |grep -v "Vins sans IG" | cut -d ";" -f 2,3,4,11 | awk -F ';' '{ if($4 == 0 || !$4) { next;} print $2; }' | sed 's/"//g' | sort | uniq  > /tmp/declarants_dr_volume_sur_place.csv
+bash bin/export_drs_csv.sh $ANNEE_DR | grep "SUR PLACE" | grep -v "Jeunes Vignes" |grep -v "Vins sans IG" | cut -d ";" -f 2,3,4,11 | awk -F ';' '{ if($4 == 0 || !$4) { next;} print $2 ";" $3; }' | sed 's/"//g' | sort | uniq  > /tmp/declarants_dr_volume_sur_place.csv
 
-bash bin/export_ds_csv.sh "propriete" "$PERIODE_DS" | cut -d ";" -f 4 | sort | uniq > /tmp/declarants_ds_propriete.csv
+bash bin/export_ds_csv.sh "propriete" "$PERIODE_DS" | awk -F ';' '{ print $4 }' | sort | uniq > /tmp/declarants_ds_propriete.csv
 
 join -v 1 -t ";" -1 1 -2 1 /tmp/declarants_dr_volume_sur_place.csv /tmp/declarants_ds_propriete.csv
 
-rm /tmp/declarants_dr_volume_sur_place.csv
-rm /tmp/declarants_ds_propriete.csv
+#rm /tmp/declarants_dr_volume_sur_place.csv
+#rm /tmp/declarants_ds_propriete.csv
