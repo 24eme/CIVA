d13f134ccade4df3f13bb5900bd3fcf78ffc7416
Vincent LAURENT <vince.laurent@gmail.com>
2017-09-08 18:08:12 +0200

    Modification de texte dans l'annexe du contrat vente de vrac

--- project/apps/civa/modules/vrac_export/templates/_annexe.php
+++ project/apps/civa/modules/vrac_export/templates/_annexe.php
@@ -12,7 +12,7 @@ En outre, dès lors que ces produits sont transférés <b><u>hors</u></b> de la 
 
 <h4>Art.5 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
 <i>(arrêté ministériel d’extension du 13/11/2012)</i>
- 
+
 <p>
 Toute transaction de vin AOC produit en Alsace, entre deux opérateurs du vignoble AOC, donne lieu dès la conclusion de la vente et quelle que soit la nature de l’opérateur, à l’établissement immédiat d’un contrat de vente conforme au contrat type interprofessionnel.
 </p>
@@ -26,7 +26,7 @@ Pour les transactions en bouteilles, le contrat fait apparaître, l’appellatio
 C’est l’acheteur ou son courtier qui transmet au CIVA les 2 exemplaires du contrat prévus à cet effet.
 </p>
 <p>
-Le contrat peut être établi et transmis de manière dématérialisée via la plateforme déclarative du portail interprofessionnel du CIVA. Dans ce cas  toutes les parties sont invitées à signer électroniquement le contrat dans un délai maximum de 5 jours.
+Le contrat peut être établi et transmis de manière dématérialisée via la plateforme déclarative du portail interprofessionnel du CIVA. Dans ce cas toutes les parties sont invitées à signer électroniquement le contrat dans un délai maximum de 5 jours.
 </p>
 
 <h4>Visa par le CIVA</h4>
@@ -34,10 +34,10 @@ Le contrat peut être établi et transmis de manière dématérialisée via la p
 Conformément aux art. L665-2, D665-30 et D665-1 du Code Rural, les transactions portant sur des produits issus de la vigne, conclues au stade de la  1ère commercialisation sur le territoire national, font l’objet d’un contrat visé par l’organisation Interprofessionnelle compétente si un accord interprofessionnel étendu le prévoit. Dans le cas des transactions en bouteilles, ce visa est délivré dès la validation du contrat.
 </p>
 <p>
-Dans le cas d’un contrat dématérialisé, le CIVA adressera à toutes les parties après signature, le pdf du contrat revêtu du numéro du Visa du CIVA.  
+Dans le cas d’un contrat dématérialisé, le CIVA adressera à toutes les parties après signature, le pdf du contrat revêtu du numéro du Visa du CIVA.
 </p>
 
-<h4>Art.13 du décret n°2012-655 du 4 mai 2012</h4>
+<h4>Art.13 du décret n°20123-655 du 4 mai 2012</h4>
 <p>
 Les vins et les vins de liqueur préemballés en France peuvent être mis en circulation s'ils répondent aux conditions suivantes :
 </p>
@@ -53,7 +53,7 @@ Les vins et les vins de liqueur préemballés en France peuvent être mis en cir
 <h4>Article L 632-7 du Code Rural modifié par loi 2010-874 du 27/7/2010</h4>
 <p>
 « Tout contrat de fourniture de produits, passé entre personnes physiques ou morales ressortissant à un accord étendu et qui n’est pas conforme aux dispositions de cet accord, est nul de plein droit. L’organisation interprofessionnelle dans le cadre de laquelle a été conclu l’accord ainsi que chacune des organisations professionnelles qui la constituent, sont recevables à demander la reconnaissance de cette nullité au juge du contrat. »<br />
-« En cas de violation des règles résultant des accords étendus, il est alloué par le juge d’instance, à la demande de l’organisation interprofessionnelle et à son profit, une indemnité dont les limites sont comprises entre 76,22 € et la réparation intégrale du préjudice subi. »
+« En cas de violation des règles résultant des accords étendus, il est alloué par le juge d’instance, à la demande de l’organisation interprofessionnelle et à son profit, u3ne indemnité dont les limites sont comprises entre 76,22 € et la réparation intégrale du préjudice subi. »
 </p>
 <p>
 « Dans tous les cas, la mise en œuvre des sanctions prévues à l’alinéa précédent ne fait pas obstacle à l’application éventuelle de celles prévues par les contrats de fourniture ainsi que par les règlements intérieurs des groupements coopératifs agricoles en cause, en cas de défaut d’exécution des clauses de ces règlements. »
@@ -72,9 +72,9 @@ L’article susmentionné rend obligatoire l’information des organismes de con
 Cette obligation a été reprise par les cahiers des charges de l’ensemble des AOC Alsace.
 </p>
 
-<h4>Art.5 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
-<i>(arrêté ministériel d’extension du 13/11/2012)</i>
- 
+<h4>Art.5 du 15e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
+<i>(arrêté ministériel d’extension du 9/08/2016)</i>
+
 <p>
 Toute transaction de vin AOC produit en Alsace, entre deux opérateurs du vignoble AOC, donne lieu dès la conclusion de la vente et quelle que
 soit la nature de l’opérateur, à l’établissement immédiat d’un contrat de vente conforme au contrat type interprofessionnel.
@@ -83,7 +83,7 @@ soit la nature de l’opérateur, à l’établissement immédiat d’un contrat
 Le contrat fait apparaître, l’appellation, le cépage (<i>ou s’il s’agit d’un assemblage, la désignation Edelzwicker</i>), les dénominations spécifiques susceptibles d’être revendiquées, le volume et le prix unitaire. Ce dernier s’entend net, c’est-à-dire hors taxes et tous escomptes déduits, la cotisation interprofessionnelle ainsi que les commissions de courtage étant à régler séparément.
 </p>
 <p>
-Afin de garantir le suivi des transactions en vrac, notamment pour l’établissement par le CIVA des mercuriales bimensuelles, le nombre de contrats et le volume correspondant devront être reportés sur les DRM (déclarations récapitulatives mensuelles des produits en droits suspendus) <b>de l’acheteur et du vendeur.</b>
+Afin de garantir le suivi des transactions en vrac, notamment pour l’établissement par le CIVA des mercuriales bimensuelles, le nombre de contrats et le volume correspondant devront être reportés sur les DRM (déclarations récapitulatives mensuelles des produits en droits suspendus) <b>de l'acheteur et du vendeur.</b>
 </p>
 
 <h4>Transmission et enregistrement par le CIVA</h4>
@@ -92,10 +92,10 @@ Afin de garantir le suivi des transactions en vrac, notamment pour l’établiss
 Le contrat n’est recevable et ne peut être dûment enregistré par le CIVA que s’il comporte effectivement le VOLUME et le PRIX de la transaction pour chaque cépage.
 </p>
 <p>
-C’est l’acheteur ou son courtier qui transmet au CIVA les 2 exemplaires du contrat prévus à cet effet. 
+C’est l'acheteur ou son courtier qui transmet au CIVA l'exemplaire du contrat prévu à cet effet.
 </p>
 <p>
-Le contrat peut être établi et transmis de manière dématérialisée via la plateforme déclarative du portail interprofessionnel du CIVA. Dans ce cas  toutes les parties sont invitées à signer électroniquement le contrat dans un délai maximum de 5 jours. 
+Le contrat peut être établi et transmis de manière dématérialisée via la plateforme déclarative du portail interprofessionnel du CIVA. Dans ce cas  toutes les parties sont invitées à signer électroniquement le contrat dans un délai maximum de 5 jours.
 </p>
 
 <h4>Visa par le CIVA</h4>
@@ -103,11 +103,11 @@ Le contrat peut être établi et transmis de manière dématérialisée via la p
 Conformément aux art. L665-2, R665-30 et R665-1 du Code Rural, les transactions portant sur des produits issus de la vigne, conclues au stade de la  1ère commercialisation sur le territoire national, font l’objet d’un contrat visé par l’organisation interprofessionnelle compétente si un accord interprofessionnel étendu le prévoit. Ce visa est délivré dans les plus brefs délais.
 </p>
 <p>
-L’art. 5 du 13e accord interprofessionnel du CIVA précise que le CIVA adresse au déposant, immédiatement et au plus tard dans les 10 jours, un récépissé revêtu de son visa. 
-Dans le cas d’un contrat dématérialisé, le CIVA adressera à toutes les parties après signature, le pdf du contrat revêtu du numéro du Visa du CIVA. 
+L’art. 5 du 15e accord interprofessionnel du CIVA précise que le CIVA adresse au déposant, immédiatement et au plus tard dans les 10 jours, un récépissé revêtu de son visa.
+Dans le cas d’un contrat dématérialisé, le CIVA adressera à toutes les parties après signature, le pdf du contrat revêtu du numéro du Visa du CIVA.
 </p>
 
-<h4>Art.12 du 13e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
+<h4>Art.11 du 15e Accord Interprofessionnel du Conseil Interprofessionnel des Vins d’Alsace</h4>
 <p>
 « Les producteurs-négociants, négociants et SICA paieront au CIVA la cotisation interprofessionnelle au taux en vigueur sur la totalité de leurs ventes en bouteilles dont la moitié, quelque soit le cépage, sera supportée par leurs vendeurs (qu’ils soient viticulteurs, coopératives ou négociants) et retenue à ceux-ci sur tous les achats de vins et de raisins. »
 </p>
