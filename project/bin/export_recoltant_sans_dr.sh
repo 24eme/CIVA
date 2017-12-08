6a5a01798f6e8ceb3de36da3ee165d592c180e1d
Vincent LAURENT <vince.laurent@gmail.com>
2017-12-08 16:34:02 +0100

    Correction dans le précédent commit

--- project/bin/export_recoltant_sans_dr.sh
+++ project/bin/export_recoltant_sans_dr.sh
@@ -19,4 +19,4 @@ fi
 
 echo "CVI;TELEPHONE;NOM;COOP;TYPE;EMAIL"
 
-cat $FILE_TIERS | awk -F ',' '{ if($36 == "\"O\"") { next; } print $24 ";" $58 ";" $38 ";" $7 ";" $12 ";" $18 ":" $41 }' | sed 's/"//g' | grep "^O;" | cut -d ";" -f 2,3,4,5,6,7 | grep -E "^6" | sed -r 's/[ ]+/ /' | while read ligne; do CVI=$(echo $ligne | cut -d ";" -f 1); curl -s $COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/DR-$CVI-$ANNEE | grep "not_found" | sed "s|^|$ligne|" | cut -d "{" -f 1; done
+cat $FILE_TIERS | awk -F ',' '{ if($36 == "\"O\"") { next; } print $24 ";" $58 ";" $38 ";" $7 ";" $12 ";" $18 ";" $41 }' | sed 's/"//g' | grep "^O;" | cut -d ";" -f 2,3,4,5,6,7 | grep -E "^6" | sed -r 's/[ ]+/ /' | while read ligne; do CVI=$(echo $ligne | cut -d ";" -f 1); curl -s $COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/DR-$CVI-$ANNEE | grep "not_found" | sed "s|^|$ligne|" | cut -d "{" -f 1; done
