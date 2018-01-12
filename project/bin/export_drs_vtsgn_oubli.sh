3d9c1fe2b05d962c92b1aabf4c25cd228add06b7
Vincent LAURENT <vince.laurent@gmail.com>
2018-01-12 09:42:58 +0100

    Que les détails

--- project/bin/export_drs_vtsgn_oubli.sh
+++ project/bin/export_drs_vtsgn_oubli.sh
@@ -11,4 +11,4 @@ fi
 
 echo '"CVI acheteur";"nom acheteur";"CVI recoltant";"nom recoltant";"appellation";"lieu";"cepage";"vtsgn";"denomination";"superficie";"volume";"dont volume a detruire";"superficie totale";"volume total";"volume a detruire total";"date de creation";"date de validation";"validateur"'
 
-bash bin/export_drs_csv.sh $ANNEE | grep -v '"CVI acheteur"' | grep -v "mentionVT" | grep -v "mentionSGN" | cut -d ";" -f -18 | grep -Ei "(séléction|selection|grain| nobles|vendange|tardive|sgn|vt|v\.t|s\.g\.n)"
+bash bin/export_drs_csv.sh $ANNEE | grep -v '"CVI acheteur"' | grep -v "mentionVT" | grep -v "mentionSGN" | grep "detail" | cut -d ";" -f -18 | grep -Ei "(séléction|selection|grain| nobles|vendange|tardive|sgn|vt|v\.t|s\.g\.n)"
