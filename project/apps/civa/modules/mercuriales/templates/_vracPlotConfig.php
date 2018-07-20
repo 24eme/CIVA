75453440cc981f1acea54dd3f76cd64950b977c6
Jb Lm <lemetayer.jb@gmail.com>
2018-07-20 17:45:17 +0200

    calage des derniers elements du plot

--- project/apps/civa/modules/mercuriales/templates/_vracPlotConfig.php
+++ project/apps/civa/modules/mercuriales/templates/_vracPlotConfig.php
@@ -15,7 +15,9 @@ set style arrow 1 nohead lw 1 lc rgb "red" lt 0
 	$i=0;
 	$min = 999;
 	$max = 0;
-	$labels = '';
+	$labels = array();
+	$engineLabels = array();
+	$nbTics = 0;
 	foreach ($datas as $data): 
 	if (!preg_match('/^[0-9]{7}$/', $data[0])) {
 		continue;
@@ -29,6 +31,7 @@ set style arrow 1 nohead lw 1 lc rgb "red" lt 0
 			$max = $v;
 		}
 	}
+	$nbTics++;
 	$i++;
 	$date = new DateTime(substr($data[0], 0, 4).'-'.substr($data[0], 4, 2).'-'.substr($data[0], -1).'1');
 	$xtics .= (substr($data[0], -1) > 0)? '"'.substr($date->format('M'), 0, 1).'\n'.substr($date->format('j'), 0, 1).'" '.$data[1] : '"'.substr($date->format('M'), 0, 1).'\n " '.$data[1];
@@ -38,20 +41,27 @@ set style arrow 1 nohead lw 1 lc rgb "red" lt 0
 ?>
 <?php 
 if (preg_match('/^[0-9]{4}12[0-1]{1}$/', $data[0])):
+$engineLabels[$data[1]] = $nbTics;
+$nbTics = 0;
+$labels[$data[1]] = "set label '".(substr($data[0], 0, 4)-1)." / ".(substr($data[0], 0, 4))."' at _X_,_MIN_ font ', 11'\n";
 ?>
 set arrow arrowstyle 1 from "<?php echo $data[1] ?>", graph 0 to "<?php echo $data[1] ?>", graph 1
 <?php endif; ?>
-<?php 
-if (preg_match('/^[0-9]{4}04[0-1]{1}$/', $data[0])) {
-	$labels .= "set label '".(substr($data[0], 0, 4) - 1)." / ".substr($data[0], 0, 4)."' at ".($data[1] + 5).",_MIN_ font ', 11'\n";
-}
-?>
 <?php
 endforeach;
+$engineLabels[$data[1]] = $nbTics;
+$labels[$data[1]] = "set label '".(substr($data[0], 0, 4)-1)." / ".(substr($data[0], 0, 4))."' at _X_,_MIN_ font ', 11'\n";
 $min = ($min > 1)? round(($min - 1), 1, PHP_ROUND_HALF_DOWN) : 0;
 $max = round(($max + 0.30), 1, PHP_ROUND_HALF_DOWN);
-
-echo str_replace('_MIN_', $min+0.45, $labels);
+foreach ($labels as $key => $label) {
+	$tic = $engineLabels[$key] - 1;
+	if ($tic > 5) {
+		$vTic = (ceil(($tic - 5) / 2) * 5) + 5 * 5;
+		if ($key >= $vTic) {
+			echo str_replace('_X_', $key - $vTic, str_replace('_MIN_', $min+0.45, $label));
+		}
+	}
+}
 ?>
 
 set xtics nomirror (<?php echo $xtics ?>) font ", 6"
@@ -67,10 +77,19 @@ set style arrow 1 nohead lw 1 lc rgb "red" lt 0
 <?php 
 foreach ($datas[count($datas) - 1] as $k => $v):
 if ($k < 2) { continue; }
-if (!$v) { continue; }
+if (!$v):
+$lastValue = 0;
+for ($i=0, $nb = count($datas); $i<$nb; $i++) {
+	if ($val = $datas[$i][$k]) {
+		$lastValue = $val;
+	}
+}
+if ($lastValue):
 ?>
+set label "Pas de cours\nconstaté <?php echo $cols[$k - 2] ?>\n(nb transaction\ninsuffisant)" at <?php echo $datas[count($datas) - 1][1] + 15 ?>,<?php echo $lastValue + 0.13 ?> front center font ", 08"
+<?php endif; else: ?>
 set label "<?php echo $v ?>" at <?php echo $datas[count($datas) - 1][1] + 15 ?>,<?php echo $v ?> right font ", 11"
-<?php endforeach; ?>
+<?php endif; endforeach; ?>
 
 		
 set style line 1 pt 2 lw 1 ps 0.8 lc rgb "black"
