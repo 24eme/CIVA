22b798196e17a5d356367f2f0dc6459424d6add9
Mathurin Petit <mathurin.petit@gmail.com>
2018-08-22 15:39:06 +0200

    export par libellé complet au civa

--- project/apps/civa/lib/drm/DRMGenerateCSV.class.php
+++ project/apps/civa/lib/drm/DRMGenerateCSV.class.php
@@ -236,7 +236,7 @@ class DRMGenerateCSV {
 
         $complement = "";
         if($produitDetail instanceof DSDetail){
-          $libelle = "";
+          $libelle = $produitDetail->getCepage()->getConfig()->getLibelleFormat();
         }else{
           $libelle = $produitDetail->getLibelle("%format_libelle%");
           $libelle = str_ireplace("Vins sans IG Sans IG","Vins sans IG Blanc",$libelle);
