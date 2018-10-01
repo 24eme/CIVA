63c3f89aa4633bc7983138b11bd5bf0bed2bdb73
Vincent LAURENT <vince.laurent@gmail.com>
2018-10-01 12:08:11 +0200

    Ajout d'une entête dans le fichier bi

--- project/bin/export_ds_csv_bi.sh
+++ project/bin/export_ds_csv_bi.sh
@@ -18,6 +18,7 @@ bash bin/export_ds_csv.sh "negoce" $PERIODE >> $TMPDIR/ds_bi_$KEYUNICITE.csv
 
 cat $TMPDIR/ds_bi_$KEYUNICITE.csv | grep -v "^#" | awk -F ";" '{ appellation=$13 " " $15 " " $14; gsub(/[\ ]+$/, "", appellation); gsub(/[\ ]+/, " ", appellation); type_ds="P"; if($4 ~ /^C/) {type_ds="N"} print $4 ";" type_ds ";" appellation ";" $16 ";" $18 ";" $19 ";" $20 ";" $21}' | sort -t ";" -k 1,1 >> $TMPDIR/ds_bi_$KEYUNICITE.sorted.csv
 
+echo "num tiers;num stock;cvi ou civaba;type de ds;appellation;cepage;volume total;volume normal;volume vt;volume sgn"
 join -t ";" $TMPDIR/tiers_db2_$KEYUNICITE.sorted.csv $TMPDIR/ds_bi_$KEYUNICITE.sorted.csv -1 3 -2 1 -a 2 | awk -F ";" 'BEGIN {OFS=";"} { identifiant=$1; if ($2 ~ /^[0-9]+$/) { $1=$2; $2=$3; $3=identifiant; } else { $1=""; $2=""; $3=identifiant} print $0; }' | grep -Ev ';(P|N);;' | sort -nt ";" -k 1,1
 
 rm $TMPDIR/tiers_db2_$KEYUNICITE.csv 2> /dev/null
