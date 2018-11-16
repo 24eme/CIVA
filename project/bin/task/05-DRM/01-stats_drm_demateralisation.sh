7805562c7c820b55512646125e3af319b81a7ea4
Tangui Morlier <tangui@tangui.eu.org>
2018-11-16 15:39:43 +0100

    Ajout de la raison sociale dans l'entete

--- project/bin/task/05-DRM/01-stats_drm_demateralisation.sh
+++ project/bin/task/05-DRM/01-stats_drm_demateralisation.sh
@@ -9,5 +9,5 @@ DESCRIPTION="Statistiques de la dématérialisation des DRM"
 mkdir -m 777 $TASK_DIR/tmp > /dev/null
 
 EXPORT_FILE=tmp/stats_drm_demat.csv
-curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/drm/_view/all?reduce=false"   | awk -F ',' '{print $2";"$4";"$7";"$12";"$14";"$16}'  | sed 's/"key":.//'  | sed 's/null//g' | sed 's/\]//'  | sort -t ';' -k 2,2 | grep '"[0-9][0-9]*"' | awk 'BEGIN{print "cvi;mois drm;date signature;transfer;conforme"} {print $0}' > $TASK_DIR/$EXPORT_FILE
+curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/drm/_view/all?reduce=false"   | awk -F ',' '{print $2";"$4";"$7";"$12";"$14";"$16}'  | sed 's/"key":.//'  | sed 's/null//g' | sed 's/\]//'  | sort -t ';' -k 2,2 | grep '"[0-9][0-9]*"' | awk 'BEGIN{print "cvi;mois drm;date signature;transfer;conforme;raison sociale"} {print $0}' > $TASK_DIR/$EXPORT_FILE
 echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE)"
