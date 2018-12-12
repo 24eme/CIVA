60a2e3028e6970ebc63280f738d8a50687ca7bd9
Vincent LAURENT <vince.laurent@gmail.com>
2018-12-12 15:52:54 +0100

    Export des acheteurs

---
+++ project/bin/task/01-DR/13-export_acheteurs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. bin/task.inc
+
+DESCRIPTION="Export des acheteurs de la DR"
+
+. bin/task_start.inc
+
+mkdir -m 777 $TASK_DIR/tmp > /dev/null
+
+EXPORT_FILE=tmp/export_acheteurs.csv
+
+echo "Type;CVI;Nom;Commune"
+
+curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/ACHAT/_view/qualite" | cut -d "," -f 2,4,5,6 | sed 's/"key":\["//' | sed 's/"value":{"cvi":"//' | sed 's/"nom":"//' | sed 's/"commune":"//' | sed 's/"}}//' | sed 's/"//g' | sed 's/,/;/g' | grep ";" > $TASK_DIR/$EXPORT_FILE
+
+echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE)"
