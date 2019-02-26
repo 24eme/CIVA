ea7ec43a8ba4361c07c9256d8bd25ca05b88e08c
mathurin petit <mathurin.petit@gmail.com>
2019-02-26 17:50:22 +0100

    couac dans la génération des lignes de DS

--- project/apps/civa/lib/drm/DRMGenerateCSV.class.php
+++ project/apps/civa/lib/drm/DRMGenerateCSV.class.php
@@ -222,8 +222,8 @@ class DRMGenerateCSV {
     public function createRowStockProduitAutreFromDS($produitDetail,$volume){
       $debutLigne = self::TYPE_CAVE . ";" . $this->periode . ";" . $this->identifiant . ";" . $this->numero_accise . ";";
       $lignes = "";
-      $lignes .= $debutLigne . $this->getProduitCSV($produitDetail,'suspendu') . ";" . "stocks_debut;initial;".$volume.=";\n";
-      $lignes .= $debutLigne . $this->getProduitCSV($produitDetail,'suspendu') . ";" . "stocks_fin;final;".$volume.=";\n";
+      $lignes .= $debutLigne . $this->getProduitCSV($produitDetail,'suspendu') . ";" . "stocks_debut;initial;".$volume.";\n";
+      $lignes .= $debutLigne . $this->getProduitCSV($produitDetail,'suspendu') . ";" . "stocks_fin;final;".$volume.";\n";
       return $lignes;
     }
 
