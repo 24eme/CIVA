c1f138af3e7646cb2176e9e13ac9c5b7ad77b8d4
Tangui Morlier <tangui@tangui.eu.org>
2019-09-16 17:26:40 +0200

    Export DB2 des DRM que pour le mois précédent

--- project/bin/task/05-DRM/03-export-db2-mois.sh
+++ project/bin/task/05-DRM/03-export-db2-mois.sh
@@ -3,9 +3,9 @@
 . bin/task.inc
 
 TASK_DIR=$(pwd)/$TASK_DIR
-EXPORT_DIR=drm/export_drm_db2
+EXPORT_DIR=drm/export_drm_db2_mois
 LINK="$TASK_URL/$EXPORT_DIR/?C=M;O=D"
-DESCRIPTION="Export des DRM pour DB2 (toutes) [Voir tous les exports]($LINK)"
+DESCRIPTION="Export des DRM pour DB2 (mois précédent) [Voir tous les exports]($LINK)"
 
 . bin/task_start.inc
 
@@ -18,5 +18,5 @@ PERIODEMAX=$(date --date='1 month ago 10 day ago' +%Y%m)
 mkdir -m 777 -p $TASK_DIR/$EXPORT_DIR > /dev/null
 
 LOGFILE="$TASK_DIR/$EXPORT_DIR/$(date +%Y%m%d%H%M%S).log"
-php symfony drm:export-db2 $TASK_DIR/$EXPORT_DIR --periode_max="$PERIODEMAX" --application=civa > $LOGFILE 2>&1
+php symfony drm:export-db2 $TASK_DIR/$EXPORT_DIR --periode_max="$PERIODEMAX" --periode_min="$PERIODEMAX" --application=civa > $LOGFILE 2>&1
 echo "[Voir les fichiers]($LINK)"
