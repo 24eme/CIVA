dca058656c3cc6806118bd6ac11c182343561ad1
Vincent LAURENT <vince.laurent@gmail.com>
2019-09-23 11:57:40 +0200

    Export db2 des ds via els tâches

---
+++ project/bin/task/03-DS/08-exort_db2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. bin/task.inc
+
+TASK_DIR=$(pwd)/$TASK_DIR
+EXPORT_DIR=ds/db2
+LINK="$TASK_URL/$EXPORT_DIR/?C=M;O=D"
+DESCRIPTION="Export des DS pour DB2 [Voir tous les exports]($LINK)"
+
+. bin/task_start.inc
+
+mkdir -m 777 -p $TASK_DIR/$EXPORT_DIR > /dev/null
+
+php symfony export:ds-civa "$DS_PERIODE" $TASK_DIR/$EXPORT_DIR "propriete"
+php symfony export:ds-civa "$DS_PERIODE" $TASK_DIR/$EXPORT_DIR "negoce"
