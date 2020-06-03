e6ce20da3393523300632ec3e838bc2ab0e18762
Vincent LAURENT <vince.laurent@gmail.com>
2020-06-03 15:46:58 +0200

    Synchronisation des fichiers entres les instance slave et master

---
+++ project/bin/sync_instances.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. $(dirname $0)/config.inc
+
+if ! test $WORKINGDIRDISTANT
+then
+	WORKINGDIRDISTANT=$WORKINGDIR
+fi
+
+rsync -aO $WORKINGDIR"/data/import/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/data/import"
+rsync -aO $WORKINGDIR"/data/upload/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/data/upload"
+rsync -aO $WORKINGDIR"/data/pdf/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/data/pdf"
+rsync -aO $WORKINGDIR"/data/mercuriales/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/data/mercuriales"
+rsync -aO $WORKINGDIR"/data/export/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/data/export"
+rsync -aO $WORKINGDIR"/web/helpPdf/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/web/helpPdf"
+rsync -aO $WORKINGDIR"/web/images/*.pdf" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/web/images/"
+rsync -aO $WORKINGDIR"/web/mise_a_disposition/" $COUCHDISTANTHOST":"$WORKINGDIRDISTANT"/web/mise_a_disposition"
