033a11db690113f9b4573c53ed7ab811d09effd3
Vincent LAURENT <vince.laurent@gmail.com>
2020-06-08 16:20:54 +0200

    IP autorisées dans l'app.yml

--- project/apps/civa/modules/drm/actions/actions.class.php
+++ project/apps/civa/modules/drm/actions/actions.class.php
@@ -10,8 +10,7 @@ class drmActions extends sfActions {
 
       set_time_limit(0);
       ini_set('memory_limit', '512M');
-      $allowIps = array("127.0.0.1", "localhost", "::1");
-      if(!in_array($this->getRequest()->getHttpHeader('addr','remote'), $allowIps) && !$this->getUser()->hasCredential(CompteSecurityUser::CREDENTIAL_ADMIN)) {
+      if(!strpos("::1,127.0.0.1,".sfConfig::get('app_ip_restriction'), $this->getRequest()->getHttpHeader('addr','remote')) && !$this->getUser()->hasCredential(CompteSecurityUser::CREDENTIAL_ADMIN)) {
         throw new sfException("Accès interdit");
       }
 
