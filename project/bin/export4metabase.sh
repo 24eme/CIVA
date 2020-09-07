02d2e2d7c4ca8c94c33ec650a237b94d44375995
Tangui Morlier <tangui@tangui.eu.org>
2020-09-07 17:04:42 +0200

    Données homogènes

--- project/bin/export4metabase.sh
+++ project/bin/export4metabase.sh
@@ -6,9 +6,11 @@ for csv in $PATH_MISEADISPO_CIVA/ds/*csv ; do
     tail -n +2 $csv
 done >> data/ds.csv
 
-cat $PATH_MISEADISPO_CIVA/tmp/export_drs_2* | head -n 1 > data/dr.csv
-ls $PATH_MISEADISPO_CIVA/tmp/ | grep export_drs_2 | awk -F '_' '{print $1"_"$2"_"$3}'  | sort -u | while read file ; do
+echo -n "TYPE;ANNEE;" > data/dr.csv
+cat $PATH_MISEADISPO_CIVA/tmp/export_drs_2019* | head -n 1 >> data/dr.csv
+ls $PATH_MISEADISPO_CIVA/tmp/ | grep export_drs_2 | grep -vE '2014|2015|2016' | awk -F '_' '{print $1"_"$2"_"$3}'  | sort -u | while read file ; do
     ls -rt  $PATH_MISEADISPO_CIVA"/tmp/"$file* | tail -n 1 ;
 done | while read file ; do
-    tail -n +2 $file ;
+    ANNEE=$(echo $file | sed 's/.*export_drs_//' | sed 's/_.*//')
+    tail -n +2 $file  | sed 's/^/DR;'$ANNEE';/';
 done >> data/dr.csv
