6b0a79498426353485fe7a428c5fa240f4f4fbc1
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-16 11:39:34 +0200

    Test DR lieu-dit

--- project/test/unit/DRTest.php
+++ project/test/unit/DRTest.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
-$t = new lime_test(32);
+$t = new lime_test(54);
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('civa', 'test', true);
 $databaseManager = new sfDatabaseManager($configuration);
@@ -12,8 +12,10 @@ $negoces = ListAcheteursConfig::getNegoces();
 foreach($negoces as $negoce) {
     break;
 }
-$appellationKey = "appellation_ALSACEBLANC";
-$produitHash = "/recolte/certification/genre/appellation_ALSACEBLANC/mention/lieu/couleur/cepage_RI";
+$appellationBlancKey = "appellation_ALSACEBLANC";
+$produitBlancHash = "/recolte/certification/genre/appellation_ALSACEBLANC/mention/lieu/couleur/cepage_RI";
+$appellationLieuxDitKey = "appellation_LIEUDIT";
+$produitLieuxDitHash = "/recolte/certification/genre/appellation_LIEUDIT/mention/lieu/couleurBlanc/cepage_CH";
 
 $dr = DRClient::getInstance()->find("DR-".$etablissement->identifiant."-2020", acCouchdbClient::HYDRATE_JSON);
 DRClient::getInstance()->deleteDoc($dr);
@@ -25,74 +27,125 @@ $t->ok($dr->_rev, "DR créé");
 
 $t->comment("Séléction des appellations / acheteurs");
 
-$dr->acheteurs->getNoeudAppellations()->getOrAdd($appellationKey)->set("cave_particuliere", 1);
-$dr->acheteurs->getNoeudAppellations()->getOrAdd($appellationKey)->get("negoces")->add(null, $negoce["cvi"]);
+$dr->acheteurs->getNoeudAppellations()->getOrAdd($appellationBlancKey)->set("cave_particuliere", 1);
+$dr->acheteurs->getNoeudAppellations()->getOrAdd($appellationBlancKey)->get("negoces")->add(null, $negoce["cvi"]);
+
+$dr->acheteurs->getNoeudAppellations()->getOrAdd($appellationLieuxDitKey)->set("cave_particuliere", 1);
+$dr->acheteurs->getNoeudAppellations()->getOrAdd($appellationLieuxDitKey)->get("negoces")->add(null, $negoce["cvi"]);
+
 $dr->update(array('from_acheteurs'));
 $dr->save();
 
-$t->comment("Saisie des colonnes");
+$t->comment("Saisie d'une colonne AOC Alsace blanc");
 
-$produit = $dr->getOrAdd($produitHash);
+$produitBlanc = $dr->getOrAdd($produitBlancHash);
 
-$detail = $produit->detail->add();
+$detail = $produitBlanc->detail->add();
 
 $detail->superficie = 100;
 $achat = $detail->negoces->add();
 $achat->cvi = $negoce["cvi"];
 $achat->quantite_vendue = 20;
 $detail->cave_particuliere = 50;
-$detail->lies = 3;
+$detail->lies = 2;
 $detail->vci = 2;
-
 $dr->update();
 
-$t->is($produit->getTotalVolumeVendus(), 20, "Volume vendu");
-$t->is($produit->getTotalCaveParticuliere(), 50, "Volume en cave particulière");
-$t->is($produit->getConfig()->getRendementNoeud(), 65, "Rendement autorisé cépage");
-$t->is($produit->getRendementMax(), 67, "Rendement autorisé avec vci");
-$t->is($produit->getVolumeMaxRendement(), 65, "Volume maxiumum autorisé par le rendement");
-$t->is($produit->getTotalVolume(), 70, "Volume total récolté");
-$t->is($produit->getRendementRecoltant(), 67, "Rendement effectif du volume récolté (sans les lies)");
-$t->is($produit->getVolumeRevendique(), 65, "Volume revendiqué");
-$t->is($produit->getDplc(), 5, "DPLC");
-$t->is($produit->getLies(), 3, "Lies");
-$t->is($produit->getTotalVci(), 2, "VCI");
-$t->is($produit->getUsagesIndustriels(), 3, "Usages industriels");
-$t->ok(!$produit->canCalculVolumeRevendiqueSurPlace(), "Le volume revendiqué sur place n'est pas calculable");
-$t->ok(!$produit->canCalculSuperficieSurPlace(), "La superficie sur place n'est pas calculable");
-$t->is($produit->getDplcCaveParticuliere(), 0, "Le DPLC en cave particulière est de 0 car on ne connait pas la répartion");
-$t->is($produit->getTotalDontDplcVendus(), 0, "Le DPLC vendu est de 0 car on ne connait pas la répartion");
-$t->is($produit->getTotalSuperficieVendus(), 0, "La superficie vendu est de 0 car on ne connait pas la répartion");
-$t->is($produit->getTotalDontVciVendus(), 0, "Le vci vendu est de 0 car on ne connait pas la répartion");
+$t->is($produitBlanc->getTotalVolumeVendus(), 20, "Volume vendu");
+$t->is($produitBlanc->getTotalCaveParticuliere(), 50, "Volume en cave particulière");
+$t->is($produitBlanc->getConfig()->getRendementNoeud(), 65, "Rendement autorisé cépage");
+$t->is($produitBlanc->getRendementMax(), 68, "Rendement autorisé avec vci");
+$t->is($produitBlanc->getVolumeMaxRendement(), 65, "Volume maxiumum autorisé par le rendement");
+$t->is($produitBlanc->getTotalVolume(), 70, "Volume total récolté");
+$t->is($produitBlanc->getRendementRecoltant(), 68, "Rendement effectif du volume récolté (sans les lies)");
+$t->is($produitBlanc->getVolumeRevendique(), 65, "Volume revendiqué");
+$t->is($produitBlanc->getDplc(), 5, "DPLC");
+$t->is($produitBlanc->getLies(), 2, "Lies");
+$t->is($produitBlanc->getTotalVci(), 2, "VCI");
+$t->is($produitBlanc->getUsagesIndustriels(), 3, "Usages industriels");
+$t->ok(!$produitBlanc->canCalculVolumeRevendiqueSurPlace(), "Le volume revendiqué sur place n'est pas calculable");
+$t->ok(!$produitBlanc->canCalculSuperficieSurPlace(), "La superficie sur place n'est pas calculable");
+
+$t->comment("Saisie d'une colonne AOC Alsace Lieux-dit");
+
+$detail = $dr->getOrAdd($produitLieuxDitHash)->detail->add();
+$produitLieuxDit = $detail->getCouleur();
+
+$detail->lieu = "COLMAR";
+$detail->superficie = 100;
+$achat = $detail->negoces->add();
+$achat->cvi = $negoce["cvi"];
+$achat->quantite_vendue = 20;
+$detail->cave_particuliere = 53;
+$detail->lies = 3;
+$dr->update();
+
+$t->is($produitLieuxDit->getTotalVolumeVendus(), 20, "Volume vendu");
+$t->is($produitLieuxDit->getTotalCaveParticuliere(), 53, "Volume en cave particulière");
+$t->is($produitLieuxDit->getConfig()->getRendementNoeud(), 68, "Rendement autorisé cépage");
+$t->is($produitLieuxDit->getRendementMax(), 68, "Rendement autorisé avec vci");
+$t->is($produitLieuxDit->getVolumeMaxRendement(), 68, "Volume maxiumum autorisé par le rendement");
+$t->is($produitLieuxDit->getTotalVolume(), 73, "Volume total récolté");
+$t->is($produitLieuxDit->getRendementRecoltant(), 70, "Rendement effectif du volume récolté (sans les lies)");
+$t->is($produitLieuxDit->getVolumeRevendique(), 68, "Volume revendiqué");
+$t->is($produitLieuxDit->getDplc(), 5, "DPLC");
+$t->is($produitLieuxDit->getLies(), 3, "Lies");
+$t->is($produitLieuxDit->getUsagesIndustriels(), 5, "Usages industriels");
+$t->ok(!$produitLieuxDit->canCalculVolumeRevendiqueSurPlace(), "Le volume revendiqué sur place n'est pas calculable");
+$t->ok(!$produitLieuxDit->canCalculSuperficieSurPlace(), "La superficie sur place n'est pas calculable");
 
 $dr->save();
 
-$t->comment("Récapitulatif des ventes");
+$t->comment("Récapitulatif des ventes AOC Alsace blanc");
 
-$t->ok($produit->hasRecapitulatif(), "Le cépage nécessite un recap de vente");
-$t->ok($produit->hasRecapitulatifVente(), "Le cépage nécessite un recap de vente et le noeud acheteur est présent");
+$t->ok($produitBlanc->hasRecapitulatif(), "Le cépage nécessite un recap de vente");
+$t->ok($produitBlanc->hasRecapitulatifVente(), "Le cépage nécessite un recap de vente et le noeud acheteur est présent");
 
-$achat = $produit->acheteurs->negoces->get($negoce["cvi"]);
+$achat = $produitBlanc->acheteurs->negoces->get($negoce["cvi"]);
 $achat->dontvci = 1;
 $achat->dontdplc = 1;
 $achat->superficie = 10;
 
-$t->ok($produit->canCalculVolumeRevendiqueSurPlace(), "Le volume revendiqué sur place est calculable");
-$t->ok($produit->canCalculSuperficieSurPlace(), "La superficie sur place est calculable");
-$t->is($produit->getVolumeRevendiqueCaveParticuliere(), 47, "Volume revendique sur place");
+$dr->update();
+
+$t->ok($produitBlanc->canCalculVolumeRevendiqueSurPlace(), "Le volume revendiqué sur place est calculable");
+$t->ok($produitBlanc->canCalculSuperficieSurPlace(), "La superficie sur place est calculable");
+$t->is($produitBlanc->getSuperficieCaveParticuliere(), 90, "Superficie sur place");
+$t->is($produitBlanc->getDplcCaveParticuliere(), 0, "DPLC sur place");
+$t->is($produitBlanc->getVolumeRevendiqueCaveParticuliere(), 47, "Volume revendique sur place");
+$t->is($produitBlanc->getTotalSuperficieVendus(), 10, "Superficie vendus");
+$t->is($produitBlanc->getTotalDontDplcVendus(), 1, "DPLC vendus");
+
+$t->comment("Récapitulatif des ventes AOC Alsace Lieux-dit");
+
+$t->ok($produitLieuxDit->hasRecapitulatif(), "Le cépage nécessite un recap de vente");
+$t->ok($produitLieuxDit->hasRecapitulatifVente(), "Le cépage nécessite un recap de vente et le noeud acheteur est présent");
+
+$achat = $produitLieuxDit->acheteurs->negoces->get($negoce["cvi"]);
+$achat->dontdplc = 1;
+$achat->superficie = 10;
 
 $dr->update();
+
+$t->ok($produitLieuxDit->canCalculVolumeRevendiqueSurPlace(), "Le volume revendiqué sur place est calculable");
+$t->ok($produitLieuxDit->canCalculSuperficieSurPlace(), "La superficie sur place est calculable");
+$t->is($produitLieuxDit->getSuperficieCaveParticuliere(), 90, "Superficie sur place");
+$t->is($produitLieuxDit->getDplcCaveParticuliere(), 1, "DPLC sur place");
+$t->is($produitLieuxDit->getVolumeRevendiqueCaveParticuliere(), 49, "Volume revendiqué sur place");
+$t->is($produitLieuxDit->getTotalSuperficieVendus(), 10, "Superficie vendus");
+$t->is($produitLieuxDit->getTotalDontDplcVendus(), 1, "DPLC vendus");
+
 $dr->save();
 
 $t->comment("PDF");
 
-$achatCouleur = $produit->getCouleur()->acheteurs->negoces->get($negoce["cvi"]);
+$achatCouleur = $produitBlanc->getCouleur()->acheteurs->negoces->get($negoce["cvi"]);
 $t->is($achatCouleur->superficie, 10, "Superficie acheteur au niveau couleur");
 $t->is($achatCouleur->volume, 20, "Volume acheteur au niveau couleur");
 $t->is($achatCouleur->dontvci, 1, "Dont vci au niveau couleur");
 $t->is($achatCouleur->dontdplc, 1, "Dont dplc au niveau couleur");
 
-$achatLieu = $produit->getLieu()->acheteurs->negoces->get($negoce["cvi"]);
+$achatLieu = $produitBlanc->getLieu()->acheteurs->negoces->get($negoce["cvi"]);
 $t->is($achatLieu->superficie, 10, "Superficie acheteur au niveau lieu");
 $t->is($achatLieu->volume, 20, "Volume acheteur au niveau lieu");
 $t->is($achatLieu->dontvci, 1, "Dont vci au niveau lieu");
