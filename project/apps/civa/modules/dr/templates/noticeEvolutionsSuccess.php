8353e7d43faef882efa5a1efac9d2810e60b5738
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-23 08:10:40 +0200

    Mise en forme notice dr

--- project/apps/civa/modules/dr/templates/noticeEvolutionsSuccess.php
+++ project/apps/civa/modules/dr/templates/noticeEvolutionsSuccess.php
@@ -3,6 +3,7 @@
 	<div id="notice_evolutions">
 		<h2>Rendements 2020</h2>
 		<p style="font-size: 14px; margin-top: 15px;">Pour l'AOC Alsace blanc, le rendement se calcule désormais <strong>PAR CÉPAGE</strong>, y compris pour l'Assemblage (Edel) pour lequel la déclaration d'une surface est <strong>OBLIGATOIRE</strong></p>
+		<br />
 		<h2>Constitution de VCI pour la Récolte 2020</h2>
 		<p style="font-size: 14px; margin-top: 15px;">Appellations :<br />
 			<ul style="font-size: 14px; padding-left: 20px; list-style-type: initial;">
@@ -35,7 +36,7 @@
 			</p>
 			<br />
 
-			<p style="margin-top: 10px;">Depuis la récolte 2012, le volume à inscrire en entrée est le <u>volume total récolté</u>, c’est-à-dire <u>lies et bourbes comprises</u> (même si des soutirages ont déjà été effectués, voire livrés en distillerie).</p>
+			<p>Depuis la récolte 2012, le volume à inscrire en entrée est le <u>volume total récolté</u>, c’est-à-dire <u>lies et bourbes comprises</u> (même si des soutirages ont déjà été effectués, voire livrés en distillerie).</p>
 
 			<p>La rubrique "Volume à détruire" comprend à la fois les lies et les volumes éventuels en dépassement de rendement (sans distinction).</p>
 
@@ -43,7 +44,7 @@
 
 			<p>Vous inscrivez vos lies connues dans la ligne "Volume à détruire" et le système vous calculera automatiquement le volume revendiqué par cépage.</p>
 			<br />
-			<a href="<?php echo url_for("dr_telecharger_la_notice"); ?>" style="height: 20px;" class="telecharger-btn"></a>
+			<a href="<?php echo url_for("dr_telecharger_la_notice"); ?>" style="height: 20px; margin-top: 10px;" class="telecharger-btn"></a>
 		</div>
 	</div>
 	<?php include_partial('dr/boutons', array('display' => array('precedent','suivant'), 'dr' => $dr)) ?>
