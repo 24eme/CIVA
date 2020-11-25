94c9aedcb3665e7764e22bccdf04ef3c33da3760
Vincent LAURENT <vince.laurent@gmail.com>
2020-11-25 23:23:09 +0100

    Uniformaisation du nombre de colonnes

--- project/bin/export_drs_mouvements_csv.sh
+++ project/bin/export_drs_mouvements_csv.sh
@@ -15,13 +15,13 @@ bash bin/export_drs_csv.sh $ANNEE | grep -v "hash_produit" | awk -v campagne="$A
 if($6 !~ "TOTAL" && $7 !~ "TOTAL") {
     base_ligne="DR;" campagne ";" $3 ";" $4 ";" $5 ";" $6 ";" $7 ";" $8 ";" $9;
     if($13) {
-        print base_ligne ";superficie;" $13;
+        print base_ligne ";superficie;" $13 ";;";
     }
     if($14) {
-        print base_ligne ";volume;" $14;
+        print base_ligne ";volume;" $14 ";;";
     }
     if($11 && $1 == $3) {
-        print base_ligne ";volume_cave_particuliere;" $11;
+        print base_ligne ";volume_cave_particuliere;" $11 ";;";
     }
     if($11 && $1 != $3) {
         gsub("\"", "", $22);
@@ -29,33 +29,33 @@ if($6 !~ "TOTAL" && $7 !~ "TOTAL") {
         print base_ligne ";volume_" $22 ";" $11 ";" $1 ";" $2;
     }
     if($15) {
-        print base_ligne ";lies;" $15;
+        print base_ligne ";lies;" $15 ";;";
     }
     if($17) {
-        print base_ligne ";vci;" $17;
+        print base_ligne ";vci;" $17 ";;";
     }
 }
 if( $6 ~ "TOTAL" ) {
     base_ligne="DR;" campagne ";" $3 ";" $4 ";" $5 ";;;;"
-    print base_ligne ";volume_revendique;" $13 - $15 - $17;
-    print base_ligne ";usages_industriels;" $15;
+    print base_ligne ";volume_revendique;" $13 - $15 - $17 ";;";
+    print base_ligne ";usages_industriels;" $15 ";;";
 }
 if( $7 ~ "TOTAL" ) {
     base_ligne="DR;" campagne ";" $3 ";" $4 ";" $5 ";" $6 ";;;"
     if($11 && $1 == $3) {
         if($10) {
-            print base_ligne ";superficie_cave_particuliere;" $10;
+            print base_ligne ";superficie_cave_particuliere;" $10 ";;";
         }
 
         if($11 && length($12) > 0) {
-            print base_ligne ";volume_revendique_cave_particuliere;" $11 - $12 - $16;
+            print base_ligne ";volume_revendique_cave_particuliere;" $11 - $12 - $16 ";;";
         }
 
         if(length($12) > 0) {
-            print base_ligne ";dplc_cave_particuliere;" $12;
+            print base_ligne ";dplc_cave_particuliere;" $12 ";;";
         }
         if($16) {
-            print base_ligne ";vci_cave_particuliere;" $16;
+            print base_ligne ";vci_cave_particuliere;" $16 ";;";
         }
     }
     if($11 && $1 != $3) {
