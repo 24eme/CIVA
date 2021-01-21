361d0d131c5a5cc196257c945864fc0e1ad71792
Vincent LAURENT <vince.laurent@gmail.com>
2021-01-21 02:59:41 +0100

    Ratio mal calculé

--- project/lib/model/couchdb/DR/_DRRecolteNoeud.class.php
+++ project/lib/model/couchdb/DR/_DRRecolteNoeud.class.php
@@ -615,7 +615,12 @@ abstract class _DRRecolteNoeud extends acCouchdbDocumentTree {
 
         $ratio = $this->getTotalVolume() / $this->getTotalVolumeAcheteurs($type);
 
-        return round($this->getTotalVci() * $ratio, 2);
+        if(!$ratio) {
+
+            return 0;
+        }
+
+        return round($this->getTotalVci() / $ratio, 2);
     }
 
     public function getTotalSuperficieVendus() {
@@ -692,7 +697,12 @@ abstract class _DRRecolteNoeud extends acCouchdbDocumentTree {
 
         $ratio = $this->getTotalVolume() / $this->getVolumeAcheteur($cvi, $type);
 
-        return round($this->getTotalVci() * $ratio, 2);
+        if(!$ratio) {
+
+            return 0;
+        }
+
+        return round($this->getTotalVci() / $ratio, 2);
     }
 
     public function getTotalDontVciVendusByCvi($type, $cvi) {
