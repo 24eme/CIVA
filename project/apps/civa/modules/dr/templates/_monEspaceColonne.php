513b109c027e6efdc713744d9f5b998e2699e95d
Vincent LAURENT <vince.laurent@gmail.com>
2021-11-10 18:17:01 +0100

    Mise à jour doc récotlé 2021

--- project/apps/civa/modules/dr/templates/_monEspaceColonne.php
+++ project/apps/civa/modules/dr/templates/_monEspaceColonne.php
@@ -19,9 +19,9 @@
 	<h3 class="titre_section">Documents d'aide</h3>
 	<div class="contenu_section">
 	    <ul>
-	        <li><a href="<?php //echo url_for('dr_telecharger_la_notice') ?>" class="pdf" style="cursor: not-allowed; opacity: 0.5;">Télécharger&nbsp;la&nbsp;notice&nbsp;Récolte <small>(en cours d'élaboration)</small></a></li>
+	        <li><a href="<?php echo url_for('dr_telecharger_la_notice') ?>" class="pdf">Télécharger&nbsp;la&nbsp;notice&nbsp;Récolte</a></li>
 	        <li><a href="<?php echo url_for('dr_telecharger_guide_vci') ?>" class="pdf">Télécharger le guide du VCI</a></li>
-          <li><a href="/drm/doc/docs/Fonctionnement_VCI_2020_2021.pdf" class="pdf">Fonctionnement&nbsp;VCI&nbsp;2020-2021</a></li>
+          <li><a href="/drm/doc/docs/Fonctionnement_VCI_2021_2022.pdf" class="pdf">Fonctionnement&nbsp;VCI&nbsp;2021-2022</a></li>
 	    </ul>
 	    <p class="intro pdf_link">Ces notices sont au format PDF. Pour les visualiser, veuillez utiliser un <a href='http://pdfreaders.org/'>lecteur PDF</a>.</p>
 	</div>
