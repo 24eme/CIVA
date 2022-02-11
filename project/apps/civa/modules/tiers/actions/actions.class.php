acd32c05d04a98911ab61adcef6cfb2b3cc9e6a3
Vincent LAURENT <vince.laurent@gmail.com>
2022-02-11 17:00:00 +0100

    Meilleur gestion des onglets de la nav avec giilda

--- project/apps/civa/modules/tiers/actions/actions.class.php
+++ project/apps/civa/modules/tiers/actions/actions.class.php
@@ -35,7 +35,7 @@ class tiersActions extends sfActions {
 
         $this->compte = $this->getRoute()->getCompte();
 
-        $this->blocs = $this->buildBlocs($this->compte);
+        $this->blocs = $this->buildBlocs($this->compte, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
         $this->nb_blocs = count($this->blocs);
 
         if(count($this->blocs) == 1) {
@@ -96,16 +96,17 @@ class tiersActions extends sfActions {
             $compte = CompteClient::getInstance()->findByLogin($request->getParameter('compte'));
         }
 
+	$isAdmin = $request->getParameter('isAdmin', false);
         if(!$compte) {
-            return $this->renderPartial("tiers/ongletsAdmin", array("active" => $request->getParameter('active'), 'absolute' => true));
+            return $this->renderPartial("tiers/ongletsAdmin", array("active" => $request->getParameter('active'), 'isAdmin' => $isAdmin, 'absolute' => true));
         }
 
-        $blocs = $this->buildBlocs($compte);
+        $blocs = $this->buildBlocs($compte, $isAdmin);
 
-        return $this->renderPartial("tiers/onglets", array("compte" => $compte, 'isAdmin' => true, "blocs" => $blocs, "active" => $request->getParameter('active'), 'absolute' => true));
+        return $this->renderPartial("tiers/onglets", array("compte" => $compte, 'isAdmin' => $isAdmin, "blocs" => $blocs, "active" => $request->getParameter('active'), 'absolute' => true));
     }
 
-    protected function buildBlocs($compte) {
+    protected function buildBlocs($compte, $isAdmin = false) {
         $blocs = array();
         if($compte->hasDroit(Roles::TELEDECLARATION_DR)) {
             $blocs[Roles::TELEDECLARATION_DR] = $this->generateUrl('mon_espace_civa_dr_compte', $compte);
@@ -123,14 +124,14 @@ class tiersActions extends sfActions {
         $etablissement = $compte->getSociete()->getEtablissementPrincipal();
         
         $url_compte = sfConfig::get("app_giilda_url_compte");
-        if($url_compte && preg_match('/(societe|etablissement|compte)/', $this->getRequest()->getParameter('active'))) {
+        if($isAdmin && $url_compte && preg_match('/(societe|etablissement|compte)/', $this->getRequest()->getParameter('active'))) {
             $blocs[Roles::CONTACT] = sfConfig::get("app_giilda_url_compte_admin");
-        } elseif($url_compte) {
+        } elseif($isAdmin && $url_compte) {
             $blocs[Roles::CONTACT] = sprintf($url_compte, $etablissement->identifiant);
         }
 
         $url_facture = sfConfig::get("app_giilda_url_facture");
-        if($url_facture && $this->getRequest()->getParameter('active') == 'facture') {
+        if($url_facture && $this->getRequest()->getParameter('active') == 'facture' && $isAdmin) {
             $blocs[Roles::FACTURE] = sfConfig::get("app_giilda_url_facture_admin");
         } elseif($url_facture) {
             $blocs[Roles::FACTURE] = sprintf($url_facture, $etablissement->identifiant);
@@ -183,7 +184,7 @@ class tiersActions extends sfActions {
         $this->secure(Roles::TELEDECLARATION_DR);
         $this->etablissement = $this->getRoute()->getEtablissement();
         $this->compte = $this->etablissement->getSociete()->getContact();
-        $this->blocs = $this->buildBlocs($this->compte);
+        $this->blocs = $this->buildBlocs($this->compte, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
         $this->campagne = CurrentClient::getCurrent()->campagne;
         $this->dr = DRClient::getInstance()->retrieveByCampagneAndCvi($this->etablissement->getIdentifiant(), $this->campagne);
 
@@ -204,7 +205,7 @@ class tiersActions extends sfActions {
     public function executeMonEspaceDRAcheteur(sfWebRequest $request) {
         $this->secure(Roles::TELEDECLARATION_DR_ACHETEUR);
         $this->compte = $this->getUser()->getCompte();
-        $this->blocs = $this->buildBlocs($this->compte);
+        $this->blocs = $this->buildBlocs($this->compte, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
         $this->etablissement = $this->getRoute()->getEtablissement();
 
         $this->help_popup_action = "help_popup_mon_espace_civa";
@@ -236,7 +237,7 @@ class tiersActions extends sfActions {
         $this->type_ds = $request->getParameter("type");
         $this->etablissement = $this->getRoute()->getEtablissement();
         $this->compte = $this->etablissement->getSociete()->getContact();
-        $this->blocs = $this->buildBlocs($this->compte);
+        $this->blocs = $this->buildBlocs($this->compte, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
 
         $this->help_popup_action = "help_popup_mon_espace_civa";
     }
@@ -251,7 +252,7 @@ class tiersActions extends sfActions {
     public function executeMonEspaceVrac(sfWebRequest $request) {
         $this->secure(Roles::TELEDECLARATION_VRAC);
         $this->compte = $this->getRoute()->getCompte();
-        $this->blocs = $this->buildBlocs($this->compte);
+        $this->blocs = $this->buildBlocs($this->compte, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
         $this->etablissements = VracClient::getInstance()->getEtablissements($this->compte->getSociete());
 
         $this->help_popup_action = "help_popup_mon_espace_civa";
@@ -267,7 +268,7 @@ class tiersActions extends sfActions {
     public function executeMonEspaceGamma(sfWebRequest $request) {
         $this->secure(Roles::TELEDECLARATION_GAMMA);
         $this->compte = $this->getUser()->getCompte();
-        $this->blocs = $this->buildBlocs($this->compte);
+        $this->blocs = $this->buildBlocs($this->compte, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
         $this->etablissement = $this->getRoute()->getEtablissement();
         $this->isInscrit = GammaClient::getInstance()->findByEtablissement($this->etablissement);
 
