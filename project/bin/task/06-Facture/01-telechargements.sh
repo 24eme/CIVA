276ce1773f2061561b6fc508792dfee839f0fef4
Vincent LAURENT <vince.laurent@gmail.com>
2022-04-04 08:20:19 +0200

    Oublié le sort

--- project/bin/task/06-Facture/01-telechargements.sh
+++ project/bin/task/06-Facture/01-telechargements.sh
@@ -14,6 +14,6 @@ curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/facture/_view/e
 curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/facture/_view/etablissement?include_docs=true" | jq -c '.rows[]' | jq -c '[.doc.date_facturation,.doc.telechargee]' | sort | grep null | cut -d "," -f 1 |  sort | uniq -c | sed 's/\["//' | sed 's/"//g' | sed -r 's/^[ ]+//' | awk -F ' ' '{ print $2 ";" $1 }' | sort > $TASK_DIR/$EXPORT_FILE.pastelechargees
 
 echo "Date;Factures téléchargées;Fatures non téléchargées" > $TASK_DIR/$EXPORT_FILE
-join -t ";" -1 1 -2 1 -a 1 $TASK_DIR/$EXPORT_FILE.telechargees $TASK_DIR/$EXPORT_FILE.pastelechargees -r >> $TASK_DIR/$EXPORT_FILE
+join -t ";" -1 1 -2 1 -a 1 $TASK_DIR/$EXPORT_FILE.telechargees $TASK_DIR/$EXPORT_FILE.pastelechargees | sort -r >> $TASK_DIR/$EXPORT_FILE
 
 echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE?$(date +%Y%m%d%H%M%S))"
