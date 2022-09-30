d58957257a07c0bc9d65cf44fda5d7754025b54e
Jb Lm <lemetayer.jb@gmail.com>
2022-09-30 16:19:42 +0200

    template pour l'etape conditions des contrats

---
+++ project/apps/civa/modules/vrac/templates/_form_conditions.php
@@ -0,0 +1,78 @@
+<?php use_helper('Float') ?>
+<p class="intro_contrat_vrac">Veuillez saisir ici les conditions applicables au contrat.</p>
+
+<table class="validation table_donnees">
+	<thead>
+		<tr>
+			<th>Produits</th>
+			<th class="date_retiraison" style="text-align: center">Date début retiraison</th>
+			<th class="date_retiraison" style="text-align: center">Date limite retiraison</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			$counter = 0;
+    		foreach ($form['produits_retiraisons'] as $key => $embedForm) :
+    			$detail = $vrac->get($key);
+			    $alt = ($counter%2);
+		?>
+		<tr<?php if ($alt): ?> class="alt"<?php endif; ?>>
+			<td>
+				<?php echo $detail->getLibelleSansCepage(); ?> <strong><?php echo $detail->getLieuLibelle(); ?> <?php echo $detail->getCepage()->getLibelle(); ?> <?php echo $detail->getComplementPartielLibelle(); ?>  <?php echo $detail->millesime; ?> <?php echo $detail->denomination; ?></strong><?php echo ($detail->exist('label') && $detail->get("label"))? " ".VracClient::$label_libelles[$detail->get("label")] : ""; ?>
+			</td>
+			<td class="date_retiraison">
+    			<span><?php echo $embedForm['retiraison_date_debut']->renderError() ?></span>
+    			<?php echo $embedForm['retiraison_date_debut']->render(array('class' => 'input_date datepicker')) ?>
+			</td>
+			<td class="date_retiraison">
+    			<span><?php echo $embedForm['retiraison_date_limite']->renderError() ?></span>
+    			<?php echo $embedForm['retiraison_date_limite']->render(array('class' => 'input_date datepicker')) ?>
+			</td>
+		</tr>
+		<?php
+			$counter++;
+			endforeach;
+		?>
+
+	</tbody>
+</table>
+
+<?php if(!$vrac->isPapier()): ?>
+<table class="validation table_donnees">
+	<thead>
+		<tr>
+			<th style="width: 212px;">Conditions</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				<?php echo $form['conditions_paiement']->renderLabel() ?>
+			</td>
+			<td>
+				<span><?php echo $form['conditions_paiement']->renderError() ?></span>
+				<?php echo $form['conditions_paiement']->render(array('class' => 'input_long')) ?>
+			</td>
+		</tr>
+		<tr class="alt">
+			<td>
+				<?php echo $form['conditions_particulieres']->renderLabel() ?>
+			</td>
+			<td>
+				<span><?php echo $form['conditions_particulieres']->renderError() ?></span>
+				<?php echo $form['conditions_particulieres']->render(array('class' => 'input_long')) ?>
+			</td>
+		</tr>
+		<?php if(isset($form['clause_reserve_propriete'])): ?>
+		<tr>
+			<td>
+				<?php echo $form['clause_reserve_propriete']->renderLabel() ?>
+			</td>
+			<td>
+			<?php echo $form['clause_reserve_propriete']->render() ?> <small style="font-size: 12px; color: #666; margin-left: 10px;">(Les modalités de cette clause sont indiquées au <a href="<?php echo url_for('vrac_pdf_annexe', array("type_contrat" => $vrac->type_contrat, "clause_reserve_propriete" => true)) ?>">verso du contrat</a>)</small>
+			</td>
+		</tr>
+		<?php endif; ?>
+	</tbody>
+</table>
+<?php endif; ?>
