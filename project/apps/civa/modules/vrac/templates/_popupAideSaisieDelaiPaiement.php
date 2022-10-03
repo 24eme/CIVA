d6cd10ffb1d39fed3dc2040c7ecd0b8d40fb3dd0
Jb Lm <lemetayer.jb@gmail.com>
2022-10-03 23:03:46 +0200

    popup aide a la saisie delai de paiement

---
+++ project/apps/civa/modules/vrac/templates/_popupAideSaisieDelaiPaiement.php
@@ -0,0 +1,48 @@
+<div id="popup_conditions_paiement" class="popup_ajout" title="Délais de paiement">
+    <form method="post" action="">
+        <table>
+            <tr>
+                <th align="right">Délai :</th>
+                <td align="left">
+                    <select style="width:280px;margin-bottom:5px" name="delai">
+                        <option value="Délai légal : 60 jours après la date d’émission de la facture">Délai légal : 60 jours après la date d’émission de la facture</option>
+                        <option value="Paiement sous 7 jours">Paiement sous 7 jours</option>
+                    </select>
+                </td>
+            </tr>
+            <tr id="ligneEscompte">
+                <th align="right">Escompte :</th>
+                <td align="left">
+                    <input type="text" name="escompte" style="width:100px;" />&nbsp;%
+                </td>
+            </tr>
+        </table>
+        <div class="clearfix" style="text-align: center; margin-top: 15px;">
+            <input type="image" src="/images/boutons/btn_valider.png" alt="Ajouter" name="boutons[next]" class="ajouter" />
+            <a class="close_popup" href=""><img alt="Annuler" src="/images/boutons/btn_annuler.png"></a>
+        </div>
+    </form>
+</div>
+<script type="text/javascript">
+    $('#ligneEscompte').hide();
+    $('#popup_conditions_paiement select[name="delai"]').change(function() {
+        if ($(this).val() == 'Paiement sous 7 jours') {
+            $('#ligneEscompte').show();
+        }
+    });
+    $("#popup_conditions_paiement .ajouter").click(function() {
+        var delai = $('#popup_conditions_paiement select[name="delai"]').val();
+        var escompte = $('#popup_conditions_paiement input[name="escompte"]').val();
+        var ligne = delai;
+        if (escompte) {
+            ligne += ', avec escompte de '+escompte+'%';
+        }
+        $('#<?php echo $target ?>').val(ligne);
+        $('#<?php echo $target ?>').removeAttr('disabled');
+        $("#popup_conditions_paiement a.close_popup").trigger("click");
+        $('#ligneEscompte').hide();
+        $('#popup_conditions_paiement select[name="delai"]').val($('#popup_conditions_paiement select[name="delai"] option:first').val());
+        $('#popup_conditions_paiement input[name="escompte"]').val('');
+        return false;
+    });
+</script>
