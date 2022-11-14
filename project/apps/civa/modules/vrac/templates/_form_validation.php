8c7d5eeeff07d3580c7df3a08148a150026eea89
Vincent LAURENT <vince.laurent@gmail.com>
2022-11-14 23:16:22 +0100

    Pas d'erreurs il le champs n'est pas rempli

--- project/apps/civa/modules/vrac/templates/_form_validation.php
+++ project/apps/civa/modules/vrac/templates/_form_validation.php
@@ -44,14 +44,14 @@
             <td class="date_retiraison_limite" style="text-align: center;">
 				<?php if($detail->retiraison_date_debut && !$vrac->isPluriannuelCadre()): ?>
                 <?php echo format_date($detail->retiraison_date_debut, 'dd/MM/yyyy') ?>
-                <?php else: ?>
+				<?php elseif($detail->retiraison_date_debut): ?>
 					<?php echo format_date('1970-'.$detail->retiraison_date_debut, 'dd/MM') ?>
                 <?php endif; ?>
 			</td>
             <td class="date_retiraison_limite" style="text-align: center;">
                 <?php if($detail->retiraison_date_limite && !$vrac->isPluriannuelCadre()): ?>
                     <?php echo format_date($detail->retiraison_date_limite, 'dd/MM/yyyy') ?>
-                <?php else: ?>
+                <?php elseif($detail->retiraison_date_limite): ?>
 	                <?php echo format_date('1970-'.$detail->retiraison_date_limite, 'dd/MM') ?>
                 <?php endif;  ?>
 			</td>
