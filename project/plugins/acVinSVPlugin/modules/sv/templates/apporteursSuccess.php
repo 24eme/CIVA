ce762cef3b89751d45a4cdf7befa85215230f698
Gabriel Poma <gpoma@24eme.fr>
2022-11-30 15:59:45 +0100

    cvi 1ere colonne apporteurs

--- project/plugins/acVinSVPlugin/modules/sv/templates/apporteursSuccess.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/apporteursSuccess.php
@@ -4,8 +4,8 @@
 <table class="table table-bordered table-striped">
   <thead>
     <tr>
-      <th class="col-xs-3">Nom de l'apporteur</th>
       <th class="col-xs-1">CVI</th>
+      <th class="col-xs-3">Nom de l'apporteur</th>
       <th class="col-xs-6 text-center">Commune</th>
       <th class="col-xs-2">Statut</th>
       <th></th>
@@ -13,8 +13,8 @@
   </thead>
 <?php foreach($sv->apporteurs as $apporteur): ?>
 <tr>
-  <td><?php echo $apporteur->nom ?></td>
   <td><?php echo $apporteur->cvi ?></td>
+  <td><?php echo $apporteur->nom ?></td>
   <td><?php echo $apporteur->commune ?></td>
   <td class="text-center"><?php if($apporteur->getNbSaisies() >= count($apporteur->produits)): ?><span class="label label-success">Saisie complète</span><?php elseif($apporteur->getNbSaisies() > 0): ?><span class="label label-warning">Saisie en cours</span><?php elseif($apporteur->getNbSaisies() == 0): ?><span class="text-muted">À saisir</span><?php endif; ?></td>
   <td class="text-right"><a href="<?php echo url_for('sv_saisie', array('sf_subject' => $sv, 'cvi' => $apporteur->getKey())); ?>" class="btn btn-xs btn-default">Saisir <span class="glyphicon glyphicon-chevron-right"></span></a></td>
