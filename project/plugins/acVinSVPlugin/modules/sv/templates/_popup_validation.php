8d421059e1322f0ee752986ccdf56b228bbb83c5
Gabriel Poma <gpoma@24eme.fr>
2022-12-01 12:34:53 +0100

    style popup validation

--- project/plugins/acVinSVPlugin/modules/sv/templates/_popup_validation.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/_popup_validation.php
@@ -1,39 +1,44 @@
-<div class="popup_validation_sv" class="popup_ajout popup_confirme" title="Validation de votre SV">
-  <form method="POST" action="">
-    <p>
-        Une fois votre déclaration validée, vous ne pourrez plus la modifier. <br /><br />
-        Confirmez vous la validation de votre déclaration de récolte ?<br />
-    </p>
-
-    <div id="administration_validation">
-        <h2 class="titre_section">Validation</h2>
-        <div class="contenu_section">
-            <div class="bloc_gris presentation">
-              <div class="bloc_form">
-                <?php echo $form->renderGlobalErrors(); ?>
-                <?php echo $form->renderHiddenFields(); ?>
-                <div class="ligne_form">
-                    <?php echo $form['date']->renderLabel(null, array('style' => 'display: inline-block;')); ?>
-                    <?php echo $form['date']->renderError(); ?>
-                    <?php echo $form['date']->render(array('class' => "datepicker")); ?>
+<div id="popup_validation_sv" class="modal" tabindex="-1" role="dialog" title="Validation de votre SV">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Veuillez confirmer la validation de la SV</h4>
+            </div>
+            <div class="modal-body">
+                <p>Vous êtes sur le point de valider votre déclaration de production. Une fois votre déclaration validée, vous ne pourrez plus la modifier.</p>
+            </div>
+            <div class="modal-footer">
+                <div class="row">
+                    <div class="col-xs-6 text-left"><button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button></div>
+                    <div class="col-xs-6 text-right"><button type="submit" data-loading-text="Validation en cours ..." class="btn btn-success btn-loading" id="signature_sv_popup_confirm">Valider la déclaration</button></div>
                 </div>
-              </div>
             </div>
         </div>
     </div>
-  </form>
 </div>
 
+<style>
+  .modal {
+    background-color: rgba(0,0,0,0.6)
+  }
+</style>
 <script>
 var initConfirmeValidationSV = function() {
-    $('#valideSV').click(function() {
-        openPopup($("#popup_confirme_validation"));
+    const modal_sv = document.getElementById("popup_validation_sv")
+    const modal_opener = document.getElementById("valideSV")
+    const modal_closer = document.querySelectorAll("[data-dismiss=modal]") || []
+
+    modal_opener.addEventListener('click', function() {
+        modal_sv.classList.add('show')
         return false;
     });
-    $('#valideSV_OK').click(function() {
-        $("#popup_confirme_validation").dialog('close');
-        $("#principal").submit();
-        return false;
-    });
+
+    modal_closer.forEach(function (el) {
+      el.addEventListener('click', function () {
+        modal_sv.classList.remove('show')
+      })
+    })
 }
+initConfirmeValidationSV()
 </script>
