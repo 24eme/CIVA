5d29d97c3ba464bcaf7e38969dbe537e791ad6fe
Gabriel Poma <gpoma@24eme.fr>
2022-12-01 14:25:21 +0100

    sv: verify, listing ligne d'erreur

--- project/plugins/acVinSVPlugin/modules/sv/templates/verifySuccess.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/verifySuccess.php
@@ -1,7 +1,10 @@
-<?php foreach ($verify as $code_erreur => $lines): ?>
 <div class="columns">
+  <div class="col-xs-4"><strong>Erreurs</strong></div>
+  <div class="col-xs-8"><strong>Lignes</strong></div>
+
+  <?php foreach ($verify as $code_erreur => $lines): ?>
   <div class="col-xs-4">
-    <?php if ($code_erreur === SVClient::CSV_ERROR_ACHETEUR): echo "Mauvais acheteur"; endif ?>
+    <?php if ($code_erreur === SVClient::CSV_ERROR_ACHETEUR): echo "La ligne concerne un autre acheteur."; endif ?>
     <?php if ($code_erreur === SVClient::CSV_ERROR_APPORTEUR): echo "Apporteur non reconnu"; endif ?>
     <?php if ($code_erreur === SVClient::CSV_ERROR_PRODUIT): echo "Produit non reconnu"; endif ?>
   </div>
@@ -10,8 +13,8 @@
       <a href='"#line".<?php echo $line[0] ?>'>#<?php echo $line[0] ?></a>
     <?php endforeach ?>
   </div>
+  <?php endforeach ?>
 </div>
-<?php endforeach ?>
 
 <table>
 
