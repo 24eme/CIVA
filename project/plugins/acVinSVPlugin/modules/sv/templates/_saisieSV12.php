84697153c3721d7b0a696102a01763cd98f4db86
Vincent LAURENT <vince.laurent@gmail.com>
2022-12-02 12:08:16 +0100

    Reinversion

--- project/plugins/acVinSVPlugin/modules/sv/templates/_saisieSV12.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/_saisieSV12.php
@@ -4,8 +4,8 @@
       <th class="col-xs-4">Apporteur</th>
       <th class="col-xs-4">Produit</th>
       <th class="col-xs-1 text-center">Superficie déclarée<br /><small>(ares)</small></th>
-      <th class="col-xs-1 text-center">Taux d'extraction</th>
       <th class="col-xs-1 text-center">Quantité récolté<br /><small>(kg)</small></th>
+      <th class="col-xs-1 text-center">Taux d'extraction</th>
       <th class="col-xs-1 text-center">Volume revendiqué<br /><small>(hl)</small></th>
     </tr>
   </thead>
@@ -16,8 +16,8 @@
       <td><?php echo $produit->nom ?><br /><small class="text-muted"><?php echo $produit->cvi ?> - <?php echo $produit->commune ?></small></td>
       <td><?php echo $produit->getRawValue()->getLibelleHtml() ?></td>
       <td><?php echo $formProduit['superficie_recolte']->render() ?></td>
-      <td><?php echo $formProduit['taux_extraction']->render() ?></td>
       <td><?php echo $formProduit['quantite_recolte']->render() ?></td>
+      <td><?php echo $formProduit['taux_extraction']->render() ?></td>
       <td><?php echo $formProduit['volume_revendique']->render() ?></td>
     </tr>
   <?php endforeach ?>
