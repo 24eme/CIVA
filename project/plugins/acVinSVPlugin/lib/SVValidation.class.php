c8ca319d166e4737adc0e7be9f2866720906a149
Gabriel Poma <gpoma@24eme.fr>
2022-12-06 14:40:54 +0100

    sv validation: controle trop de volume stockage

--- project/plugins/acVinSVPlugin/lib/SVValidation.class.php
+++ project/plugins/acVinSVPlugin/lib/SVValidation.class.php
@@ -1,12 +1,29 @@
 <?php
 
-class SVValidation extends DocumentValidation {
+class SVValidation extends DocumentValidation
+{
     public function configure()
     {
-
+        $this->addControle('erreur', 'stockage_repartition', "Trop de volume déclaré dans le lieu de stockage");
     }
 
-    public function controle() {
+    public function controle()
+    {
+        if (count($this->document->stockage) > 1) {
+            foreach($this->document->stockage as $stockage) {
+                $produits = is_array($stockage->getProduits()) ? $stockage->getProduits() : $stockage->getProduits()->toArray();
+                $stocks_negatifs = array_filter($produits, function ($produit) {
+                    return $produit < 0;
+                });
 
+                if (empty($stocks_negatifs) === false) {
+                    $this->addPoint(
+                        'erreur',
+                        'stockage_repartition',
+                        $stockage->numero
+                    );
+                }
+            }
+        }
     }
 }
