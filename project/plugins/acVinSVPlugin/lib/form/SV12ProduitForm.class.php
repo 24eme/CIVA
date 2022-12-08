8d14bbbfd59b13148f61e7e3a6d0e464e1d17923
Vincent LAURENT <vince.laurent@gmail.com>
2022-12-08 02:29:43 +0100

    sv: taux par défaut et calcul dans la saisie SV12

--- project/plugins/acVinSVPlugin/lib/form/SV12ProduitForm.class.php
+++ project/plugins/acVinSVPlugin/lib/form/SV12ProduitForm.class.php
@@ -8,19 +8,19 @@ class SV12ProduitForm extends acCouchdbObjectForm
     }
 
     public function configure() {
-        $this->setWidget('superficie_recolte', new bsWidgetFormInputFloat(array(), array('placeholder' => 'ares', 'class' => 'form-control text-right input-float input-sm')));
+        $this->setWidget('superficie_recolte', new bsWidgetFormInputFloat(array(), array('class' => 'form-control text-right input-float input-sm')));
         $this->setValidator('superficie_recolte', new sfValidatorNumber(array('required' => false)));
 
-        $this->setWidget('quantite_recolte', new bsWidgetFormInputInteger(array(), array('placeholder' => 'kg', 'class' => 'form-control text-right input-integer input-sm input_quantite')));
+        $this->setWidget('quantite_recolte', new bsWidgetFormInputInteger(array(), array('class' => 'form-control text-right input-integer input-sm input_quantite')));
         $this->setValidator('quantite_recolte', new sfValidatorInteger(array('required' => false)));
 
-        $this->setWidget('taux_extraction', new bsWidgetFormInputFloat(array(), array('class' => 'form-control text-right input-float input-sm input_taux_extraction')));
+        $this->setWidget('taux_extraction', new bsWidgetFormInputFloat(array(), array('class' => 'form-control text-right input-float input-sm input_taux_extraction', 'readonly' => 'readonly', 'tabindex' => -1)));
         $this->setValidator('taux_extraction', new sfValidatorNumber(array('required' => false)));
 
         $this->setWidget('volume_recolte', new bsWidgetFormInputFloat(array(), array('placeholder' => 'hl', 'class' => 'form-control text-right input-float input-sm')));
         $this->setValidator('volume_recolte', new sfValidatorNumber(array('required' => false)));
 
-        $this->setWidget('volume_revendique', new bsWidgetFormInputFloat(array(), array('placeholder' => 'hl', 'class' => 'form-control text-right input-float input-sm input_volume_revendique')));
+        $this->setWidget('volume_revendique', new bsWidgetFormInputFloat(array(), array('placeholder' => '', 'class' => 'form-control text-right input-float input-sm input_volume_revendique')));
         $this->setValidator('volume_revendique', new sfValidatorNumber(array('required' => false)));
 
         if($this->getObject()->exist('volume_mouts')) {
