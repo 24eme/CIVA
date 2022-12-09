840f766925412f87f9c0773502c421ee75b4bd5e
Jb Lm <lemetayer.jb@gmail.com>
2022-12-09 15:50:55 +0100

    conditionnement affichage numero de visa

--- project/apps/civa/modules/vrac/templates/_liste.php
+++ project/apps/civa/modules/vrac/templates/_liste.php
@@ -41,7 +41,7 @@
                     </svg>
                 <?php elseif($object->contrat_pluriannuel): ?>
                     <svg style="color: #7e8601; margin-left: 5px;" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-journals" viewBox="0 0 16 16">
-                      <title>Contrat pluriannuel n°<?php echo $object->numero_visa ?></title>
+                      <title>Contrat pluriannuel<?php echo ($object->numero_visa)? ' n°'.$object->numero_visa : ''; ?></title>
                       <path d="M5 0h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2 2 2 0 0 1-2 2H3a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1H1a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v9a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1H3a2 2 0 0 1 2-2z"/>
                       <path d="M1 6v-.5a.5.5 0 0 1 1 0V6h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V9h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 2.5v.5H.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1H2v-.5a.5.5 0 0 0-1 0z"/>
                     </svg>
