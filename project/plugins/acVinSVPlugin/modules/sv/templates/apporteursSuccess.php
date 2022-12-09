4970b7a4fb2a377674fe6d6f9c171b82049eaa55
Vincent LAURENT <vince.laurent@gmail.com>
2022-12-09 13:38:31 +0100

    Mise en forme tableau apporteur

--- project/plugins/acVinSVPlugin/modules/sv/templates/apporteursSuccess.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/apporteursSuccess.php
@@ -24,7 +24,7 @@
       <th class="col-xs-2 text-center">Commune</th>
       <th class="col-xs-2 text-center">Superficie<br/>par apporteur</th>
       <th class="col-xs-2 text-center">Revendiqué<br/>par apporteur</th>
-      <th class="col-xs-2 text-center">Statut</th>
+      <th class="col-xs-2 text-center"></th>
       <th></th>
     </tr>
   </thead>
@@ -35,7 +35,7 @@
   <td><?php echo $apporteur->nom ?></td>
   <td><?php echo $apporteur->commune ?></td>
   <td class="text-right"><?php echoFloat($recap['superficie']) ?> <small class="text-muted">ares</small></td>
-  <td class="text-right"><?php echoFloat($recap['revendique']) ?> <small class="text-muted">hl</small></td>
+  <td class="text-right"><?php if($recap['revendique']): ?><?php echoFloat($recap['revendique']) ?> <small class="text-muted">hl</small><?php endif; ?></td>
   <td class="text-center"><?php if($apporteur->getNbSaisies() >= count($apporteur->produits)): ?><span class="label label-success">Saisie complète</span><?php elseif($apporteur->getNbSaisies() > 0): ?><span class="label label-warning">Saisie en cours</span><?php endif; ?></td>
   <td class="text-right"><a href="<?php echo url_for('sv_saisie', array('sf_subject' => $sv, 'cvi' => $apporteur->getKey())); ?>" class="btn btn-xs btn-default">Saisir <span class="glyphicon glyphicon-chevron-right"></span></a></td>
 </tr>
