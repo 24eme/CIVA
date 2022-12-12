91f379638f0e656c5e891679d63e00326c59c7f6
Jb Lm <lemetayer.jb@gmail.com>
2022-12-12 15:30:49 +0100

    bouton supprimer

--- project/apps/civa/modules/vrac/templates/supprimerSuccess.php
+++ project/apps/civa/modules/vrac/templates/supprimerSuccess.php
@@ -31,22 +31,28 @@
 						</td>
 						<td>
 							<span><?php echo $form['motif_suppression']->renderError() ?></span>
-							<?php echo $form['motif_suppression']->render(array('rows' => 6, 'cols' => 60)) ?>	
+							<?php echo $form['motif_suppression']->render(array('rows' => 6, 'cols' => 60)) ?>
 						</td>
-					</tr>			
+					</tr>
 				</tbody>
-			</table>		
+			</table>
 		</div>
-		
+
 		<table id="actions_fiche">
 			<tr>
 				<td style="width: 33%">&nbsp;</td>
 				<td align="center"></td>
 				<td style="width: 33%; text-align: right;">
-					<input type="image" src="/images/boutons/btn_supprimer_contrat_rouge.png" alt="Supprimer le contrat" />
+                    <button class="btn_majeur btn_rouge btn_grand" type="submit">
+                        Supprimer
+                        <svg style="color:white;" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash" viewBox="0 -1 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                    </button>
 				</td>
 			</tr>
 		</table>
 	</form>
-	
+
 </div>
