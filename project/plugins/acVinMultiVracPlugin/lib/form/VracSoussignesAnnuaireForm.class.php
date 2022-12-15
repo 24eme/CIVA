9e071c1ec821ba819d4ad3609975836faf32bd07
Vincent LAURENT <vince.laurent@gmail.com>
2022-12-15 18:12:02 +0100

    Annuaire création

--- project/plugins/acVinMultiVracPlugin/lib/form/VracSoussignesAnnuaireForm.class.php
+++ project/plugins/acVinMultiVracPlugin/lib/form/VracSoussignesAnnuaireForm.class.php
@@ -29,9 +29,9 @@ class VracSoussignesAnnuaireForm extends VracSoussignesForm
         	'type_contrat' => new sfValidatorChoice(array('required' => false, 'choices' => array_keys($contratTypes))),
             'contrat_pluriannuel' => new sfValidatorChoice(array('choices' => [0,1])),
         	'acheteur_type' => new sfValidatorChoice(array('required' => false, 'choices' => array_keys($types))),
-            'acheteur_assujetti_tva' => new sfValidatorChoice(array('choices' => [0,1])),
+            'acheteur_assujetti_tva' => new sfValidatorChoice(array('required' => false, 'choices' => [0,1])),
         	'vendeur_type' => new sfValidatorChoice(array('required' => false, 'choices' => array_keys($types))),
-            'vendeur_assujetti_tva' => new sfValidatorChoice(array('choices' => [0,1])),
+            'vendeur_assujetti_tva' => new sfValidatorChoice(array('required' => false, 'choices' => [0,1])),
         	'acheteur_recoltant_identifiant' => new sfValidatorChoice(array('required' => false, 'choices' => array_keys(array_merge($recoltantChoices, array('add' => ''))))),
         	'acheteur_negociant_identifiant' => new sfValidatorChoice(array('required' => false, 'choices' => array_keys(array_merge($negociantChoices, array('add' => ''))))),
         	'acheteur_cave_cooperative_identifiant' => new sfValidatorChoice(array('required' => false, 'choices' => array_keys(array_merge($caveCooperativeChoices, array('add' => ''))))),
@@ -68,8 +68,12 @@ class VracSoussignesAnnuaireForm extends VracSoussignesForm
 	    	$this->getObject()->vendeur_identifiant = $vendeur->_id;
 	    	$this->getObject()->storeVendeurInformations($vendeur);
     	}
-        $this->getObject()->acheteur_assujetti_tva = (isset($values['acheteur_assujetti_tva']) && $values['acheteur_assujetti_tva'])? 1 : 0;
-        $this->getObject()->vendeur_assujetti_tva = (isset($values['vendeur_assujetti_tva']) && $values['vendeur_assujetti_tva'])? 1 : 0;
+        if(isset($values['acheteur_assujetti_tva']) && $values['acheteur_assujetti_tva'] !== null) {
+            $this->getObject()->acheteur_assujetti_tva = (isset($values['acheteur_assujetti_tva']) && $values['acheteur_assujetti_tva'])? 1 : 0;
+        }
+        if(isset($values['vendeur_assujetti_tva']) && $values['vendeur_assujetti_tva'] !== null) {
+            $this->getObject()->vendeur_assujetti_tva = (isset($values['vendeur_assujetti_tva']) && $values['vendeur_assujetti_tva'])? 1 : 0;
+        }
     	if ($this->getObject()->isNew()) {
     		$this->getObject()->type_contrat = $values['type_contrat'];
             $this->getObject()->contrat_pluriannuel = (isset($values['contrat_pluriannuel']) && $values['contrat_pluriannuel'])? 1 : 0;
