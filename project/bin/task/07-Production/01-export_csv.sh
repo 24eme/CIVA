a1cca9555d286b1cd46337aee6d7164fbb0eee0f
Gabriel Poma <gpoma@24eme.fr>
2022-12-16 18:16:20 +0100

    sv: export csv utilise DR_CAMPAGNE

--- project/bin/task/07-Production/01-export_csv.sh
+++ project/bin/task/07-Production/01-export_csv.sh
@@ -2,14 +2,14 @@
 
 . bin/task.inc
 
-DESCRIPTION="Export CSV des SV de la campagne $SV_CAMPAGNE"
+DESCRIPTION="Export CSV des SV de la campagne $DR_CAMPAGNE"
 
 . bin/task_start.inc
 
 mkdir -m 777 $TASK_DIR/tmp > /dev/null
 
-EXPORT_FILE=tmp/export_svs_"$SV_CAMPAGNE"_$(date +%Y%m%d%H%M%S).csv
+EXPORT_FILE=tmp/export_svs_"$DR_CAMPAGNE"_$(date +%Y%m%d%H%M%S).csv
 
-bash bin/export_svs_csv.sh "$SV_CAMPAGNE" > "$TASK_DIR/$EXPORT_FILE"
+bash bin/export_svs_csv.sh "$DR_CAMPAGNE" > "$TASK_DIR/$EXPORT_FILE"
 
 echo "[Télécharger le fichier]($TASK_URL/$EXPORT_FILE?$(date +%Y%m%d%H%M%S))"
