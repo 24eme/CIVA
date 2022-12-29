220f284fca6eb63f030e7449d37216f76208a8d5
Gabriel Poma <gpoma@24eme.fr>
2022-12-29 17:15:26 +0100

    sv: inversement colonnes superficie / volume mouts

--- project/plugins/acVinSVPlugin/modules/sv/templates/_validationSV12.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/_validationSV12.php
@@ -5,8 +5,8 @@
       <th class="col-xs-1 text-center">Superficie déclarée<br /><small>(ares)</small></th>
       <th class="col-xs-1 text-center">Quantité récolté<br /><small>(kg)</small></th>
       <th class="col-xs-1 text-center">Volume revendiqué<br /><small>(hl)</small></th>
-      <th class="col-xs-1 text-center">Volume de moûts<br /><small>(hl)</small></th>
       <th class="col-xs-1 text-center">Superficie de moûts<br /><small>(ares)</small></th>
+      <th class="col-xs-1 text-center">Volume de moûts<br /><small>(hl)</small></th>
       <th class="col-xs-1 text-center">Nombre d'apporteurs</th>
     </tr>
   </thead>
@@ -17,8 +17,8 @@
       <td class="text-right"><?php echoFloat($produit->superficie_recolte) ?> <small class="text-muted">ares</small></td>
       <td class="text-right"><?php echo $produit->quantite_recolte ?> <small class="text-muted">kg</small></td>
       <td class="text-right"><?php echoFloat($produit->volume_revendique) ?> <small class="text-muted">hl</small></td>
-      <td class="text-right"><?php if($produit->volume_mouts > 0): ?><?php echoFloat($produit->volume_mouts) ?> <small class="text-muted">hl</small><?php endif; ?></td>
       <td class="text-right"><?php if($produit->superficie_mouts > 0): ?><?php echoFloat($produit->superficie_mouts) ?> <small class="text-muted">ares</small><?php endif; ?></td>
+      <td class="text-right"><?php if($produit->volume_mouts > 0): ?><?php echoFloat($produit->volume_mouts) ?> <small class="text-muted">hl</small><?php endif; ?></td>
       <td class="text-right"><?php echo count($produit->apporteurs) ?></td>
     </tr>
   <?php endforeach ?>
@@ -28,8 +28,8 @@
     <td class="text-right"><?php echoFloat($totalParColonne['superficie']) ?> <small class="text-muted">ares</small></td>
     <td class="text-right"><?php echo $totalParColonne['recolte'] ?> <small class="text-muted">kg</small></td>
     <td class="text-right"><?php echoFloat($totalParColonne['revendique']) ?> <small class="text-muted">hl</small></td>
-    <td class="text-right"><?php echoFloat($totalParColonne['mouts']) ?> <small class="text-muted">hl</small></td>
     <td class="text-right"><?php echoFloat($totalParColonne['superficie_mouts']) ?> <small class="text-muted">ares</small></td>
+    <td class="text-right"><?php echoFloat($totalParColonne['mouts']) ?> <small class="text-muted">hl</small></td>
     <td class="text-right"><?php echo count($sv->apporteurs) ?></td>
   </tr>
   </tbody>
