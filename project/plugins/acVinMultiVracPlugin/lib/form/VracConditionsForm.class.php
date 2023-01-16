b7a15101657bdb1e75947b8f192a8c92af52dc55
Vincent LAURENT <vince.laurent@gmail.com>
2023-01-16 15:50:36 +0100

    Suprresion des ":" à la fin des labels

--- project/plugins/acVinMultiVracPlugin/lib/form/VracConditionsForm.class.php
+++ project/plugins/acVinMultiVracPlugin/lib/form/VracConditionsForm.class.php
@@ -22,43 +22,43 @@ class VracConditionsForm extends acCouchdbObjectForm
         if($this->getObject()->exist('suivi_qualitatif')) {
 			$this->setWidget('suivi_qualitatif', new sfWidgetFormChoice(array('choices' => ["1" => "Oui", "0" => "Non"], 'expanded' => true), array('required' => 'required')));
 			$this->setValidator('suivi_qualitatif', new sfValidatorChoice(array('choices' => [1,0])));
-			$this->getWidgetSchema()->setLabel('suivi_qualitatif', "Suivi qualitatif :");
+			$this->getWidgetSchema()->setLabel('suivi_qualitatif', "Suivi qualitatif");
 		}
 
 		if($this->getObject()->exist('clause_reserve_propriete')) {
 			$this->setWidget('clause_reserve_propriete', new sfWidgetFormChoice(array('choices' => ["1" => "Oui", "0" => "Non"], 'expanded' => true)));
 			$this->setValidator('clause_reserve_propriete', new sfValidatorChoice(array('choices' => [1,0], 'required' => false)));
-			$this->getWidgetSchema()->setLabel('clause_reserve_propriete', "Clause de réserve de propriété :");
+			$this->getWidgetSchema()->setLabel('clause_reserve_propriete', "Clause de réserve de propriété");
 		}
 
 		if($this->getObject()->exist('clause_mandat_facturation')) {
 			$this->setWidget('clause_mandat_facturation', new sfWidgetFormChoice(array('choices' => ["1" => "Oui", "0" => "Non"], 'expanded' => true)));
 			$this->setValidator('clause_mandat_facturation', new sfValidatorChoice(array('choices' => [1,0], 'required' => false)));
-			$this->getWidgetSchema()->setLabel('clause_mandat_facturation', "Mandat de facturation :");
+			$this->getWidgetSchema()->setLabel('clause_mandat_facturation', "Mandat de facturation");
 		}
 
 		if($this->getObject()->exist('vendeur_frais_annexes')) {
 			$this->setWidget('vendeur_frais_annexes', new sfWidgetFormTextarea());
 			$this->setValidator('vendeur_frais_annexes', new sfValidatorString(array('required' => false)));
-			$this->getWidgetSchema()->setLabel('vendeur_frais_annexes', "Frais annexes en sus à la charge du vendeur :");
+			$this->getWidgetSchema()->setLabel('vendeur_frais_annexes', "Frais annexes en sus à la charge du vendeur");
 		}
 
 		if($this->getObject()->exist('acheteur_primes_diverses')) {
 			$this->setWidget('acheteur_primes_diverses', new sfWidgetFormTextarea());
 			$this->setValidator('acheteur_primes_diverses', new sfValidatorString(array('required' => false)));
-			$this->getWidgetSchema()->setLabel('acheteur_primes_diverses', "Primes diverses à la charge de l'acheteur :");
+			$this->getWidgetSchema()->setLabel('acheteur_primes_diverses', "Primes diverses à la charge de l'acheteur");
 		}
 
 		if($this->getObject()->exist('clause_resiliation')) {
 			$this->setWidget('clause_resiliation', new sfWidgetFormTextarea());
 			$this->setValidator('clause_resiliation', new sfValidatorString(array('required' => false)));
-			$this->getWidgetSchema()->setLabel('clause_resiliation', "Résiliation hors cas de force majeur :");
+			$this->getWidgetSchema()->setLabel('clause_resiliation', "Résiliation hors cas de force majeur");
 		}
 
 		if($this->getObject()->exist('clause_evolution_prix')) {
 			$this->setWidget('clause_evolution_prix', new sfWidgetFormTextarea());
 			$this->setValidator('clause_evolution_prix', new sfValidatorString(array('required' => false)));
-			$this->getWidgetSchema()->setLabel('clause_evolution_prix', "Critères et modalités d'évolution des prix pour les années N+1 et N+2 :");
+			$this->getWidgetSchema()->setLabel('clause_evolution_prix', "Critères et modalités d'évolution des prix pour les années N+1 et N+2");
 		}
 
         $produitsRetiraisons = new VracRetiraisonsCollectionForm($this->getObject()->declaration->getActifProduitsDetailsSorted());
