af2c6702afcc6bbce2aa933474bf2a1851c77889
Jb Lm <lemetayer.jb@gmail.com>
2023-02-16 18:12:14 +0100

    evite le warning

--- project/plugins/acVinMultiVracPlugin/lib/model/Vrac/Vrac.class.php
+++ project/plugins/acVinMultiVracPlugin/lib/model/Vrac/Vrac.class.php
@@ -1058,7 +1058,7 @@ class Vrac extends BaseVrac implements InterfaceArchivageDocument
 	}
 
     public function hasVersion() {
-        return file_get_contents($this->getAttachmentUri(self::VENDEUR_PROJET_FILENAME)) !== false;
+        return @file_get_contents($this->getAttachmentUri(self::VENDEUR_PROJET_FILENAME)) !== false;
     }
 
     public function getMother() {
