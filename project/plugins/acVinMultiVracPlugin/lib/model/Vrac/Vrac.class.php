d7b272446159c3424078cd857b17aa94d30b0670
Jb Lm <lemetayer.jb@gmail.com>
2023-02-16 18:11:54 +0100

    fct raccourci pour savoir si tiers a signé

--- project/plugins/acVinMultiVracPlugin/lib/model/Vrac/Vrac.class.php
+++ project/plugins/acVinMultiVracPlugin/lib/model/Vrac/Vrac.class.php
@@ -686,6 +686,16 @@ class Vrac extends BaseVrac implements InterfaceArchivageDocument
     	return (bool) $this->valide->date_validation_vendeur;
     }
 
+    public function hasAcheteurSigne()
+    {
+    	return (bool) $this->valide->date_validation_acheteur;
+    }
+
+    public function hasCourtierSigne()
+    {
+    	return (bool) $this->valide->date_validation_mandataire;
+    }
+
     public function hasCourtier() {
         return ($this->mandataire_identifiant)? true : false;
     }
