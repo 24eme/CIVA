f42d404dd9de00a4f2266b312b7947b72064e835
Vincent LAURENT <vince.laurent@gmail.com>
2023-02-17 12:03:04 +0100

    Engagée et delais de paiement

--- project/apps/civa/modules/vrac/templates/_produits.php
+++ project/apps/civa/modules/vrac/templates/_produits.php
@@ -16,7 +16,7 @@
 			<th class="prix">Prix</th>
 			<th class="volume">Volume</th>
 			<?php else: ?>
-            <th class="volume"><?php echo ucfirst($quantiteType); ?> <?php if(!$vrac->needRetiraison()): ?>engagée<?php else: ?>estimé<?php endif; ?></th>
+            <th class="volume"><?php echo ucfirst($quantiteType); ?> <?php if(!$vrac->needRetiraison()): ?>engagé<?php else: ?>estimé<?php endif; ?></th>
 			<th class="prix">Prix</th>
 			<?php if ($vrac->needRetiraison() && ($vrac->isCloture() || $form)): ?>
 			<th class="echeance">Date</th>
