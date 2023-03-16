8826f048ff7c8ff5fee787155985870e6ed202f4
Vincent LAURENT <vince.laurent@gmail.com>
2023-03-16 09:49:04 +0100

    Chronologie plus cohérente

--- project/apps/civa/modules/vrac/templates/_chronologie_contrat.php
+++ project/apps/civa/modules/vrac/templates/_chronologie_contrat.php
@@ -61,25 +61,31 @@
         <?php else: ?>
         <tr>
 			<td>
-				<span class="picto_crayon" style="padding-left:15px; margin-left: 2px;">&nbsp;Projet de contrat initié par <?php if ($vrac->hasVersion() && $vrac->hasCourtier()): ?>le courtier<?php elseif($vrac->hasVersion()): ?>le vendeur<?php else: ?>l'acheteur<?php endif; ?></span>
+				<span class="picto_crayon" style="padding-left:15px; margin-left: 2px;">&nbsp;Projet de contrat initié par <?php if ($vrac->hasCourtier()): ?>le courtier<?php elseif($vrac->hasVersion() || $vrac->isVendeurProprietaire()): ?>le vendeur<?php else: ?>l'acheteur<?php endif; ?></span>
 			</td>
             <td style="text-align: left;">
 				<?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?>
 			</td>
 		</tr>
-        <?php if ($vrac->hasVersion()): ?>
+        <?php if ($vrac->hasVersion() || $vrac->isVendeurProprietaire()): ?>
         <tr>
 			<td>
-				<span class="picto_check" style="padding-left:15px; margin-left: 2px;">&nbsp;Projet validé par l'acheteur et soumis à l'acheteur</span>
+				<span class="picto_sablier" style="padding-left:15px; margin-left: 2px;">&nbsp;Projet soumis à l'acheteur pour validation</span>
 			</td>
-            <td style="text-align: left;"><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?></td>
+            <td style="text-align: left;"><?php if(!$vrac->isVendeurProprietaire()): ?><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?><?php endif; ?></td>
+		</tr>
+        <tr>
+			<td>
+				<span class="picto_check" style="padding-left:15px; margin-left: 2px;">&nbsp;Projet validé par l'acheteur et soumis au vendeur</span>
+			</td>
+            <td style="text-align: left;"><?php if(!in_array($vrac->valide->statut, array(Vrac::STATUT_CREE, Vrac::STATUT_PROJET_VENDEUR))): ?><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?><?php endif; ?></td>
 		</tr>
         <?php endif; ?>
         <tr>
 			<td>
 				<span class="picto_sablier" style="padding-left:15px; margin-left: 2px;">&nbsp;Projet soumis au vendeur pour signature</span>
 			</td>
-            <td style="text-align: left;"><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?></td>
+            <td style="text-align: left;"><?php if(!in_array($vrac->valide->statut, array(Vrac::STATUT_CREE, Vrac::STATUT_PROJET_VENDEUR))): ?><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?><?php endif; ?></td>
 		</tr>
         <tr class="<?php if (!$vrac->hasVendeurSigne()): ?> text-muted<?php endif; ?>">
 			<td>
