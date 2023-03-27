dcf3e642d7f7aa5d81dd17ea1f8fb5d81cf903fa
Jb Lm <lemetayer.jb@gmail.com>
2023-03-27 17:20:53 +0200

    date d'enlevement required

--- project/apps/civa/modules/vrac/templates/_form_retiraisons_item.php
+++ project/apps/civa/modules/vrac/templates/_form_retiraisons_item.php
@@ -5,7 +5,7 @@
 	<td colspan="3"></td>
 	<td class="echeance">
 		<span><?php echo $form['date']->renderError() ?></span>
-		<?php echo (!$detail->cloture)? $form['date']->render(array('class' => 'input_date datepicker')) : $form['date']->render(array('class' => 'input_date', 'readonly' => 'readonly')); ?>
+		<?php echo (!$detail->cloture)? $form['date']->render(array('class' => 'input_date datepicker', 'required' => 'required')) : $form['date']->render(array('class' => 'input_date', 'readonly' => 'readonly')); ?>
 	</td>
 	<td class="enleve">
 		<span><?php echo $form['volume']->renderError() ?></span>
