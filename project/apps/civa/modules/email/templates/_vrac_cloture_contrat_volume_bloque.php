90cfdd09d1ed9d7acff2a05d93306f63bcc6a9bb
Jb Lm <lemetayer.jb@gmail.com>
2023-03-30 16:52:02 +0200

    on retire le mail de notification de cloture

--- project/apps/civa/modules/email/templates/_vrac_cloture_contrat_volume_bloque.php
+++
@@ -1,8 +0,0 @@
-<?php include_partial('email/header', array('vrac' => $vrac)); ?>
-
-
-Tous les enlèvements ayant été effectués, ce contrat a été cloturé : <?php echo ProjectConfiguration::getAppRouting()->generate('vrac_fiche', array('sf_subject' => $vrac), true); ?>
-
-Attention ! Ce contrat contient des produits dont une partie du volume est en réserve !
-
-<?php include_partial('email/footer', array('vrac' => $vrac)); ?>
