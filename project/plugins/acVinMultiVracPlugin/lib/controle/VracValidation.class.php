c8a79a1d7b760204e99b43b30fa736edf0f24f96
Jb Lm <lemetayer.jb@gmail.com>
2023-03-30 16:08:23 +0200

    affichage du pt de vigilance volume bloque sur la fiche

--- project/plugins/acVinMultiVracPlugin/lib/controle/VracValidation.class.php
+++ project/plugins/acVinMultiVracPlugin/lib/controle/VracValidation.class.php
@@ -5,17 +5,18 @@ class VracValidation extends DocumentValidation
 	const TAUX_VOLUME_CONTROLE = 0.1;
 
 	protected $produits_controle = array();
-	
+
 	public function __construct($document, $options = null)
     {
         parent::__construct($document, $options);
         $this->noticeVigilance = false;
     }
-    
-  	public function configure() 
+
+  	public function configure()
   	{
     	$this->addControle('vigilance', 'ecart_volume', 'Il y a un écart de volume important entre le volume estimé et le volume réel d\'enlèvement.');
     	$this->addControle('erreur', 'volume_non_saisi', 'La cloture d\'un produit n\'est pas possible sans spécifier un volume réel d\'enlèvement.');
+        $this->addControle('vigilance', 'volume_bloque', 'Ce contrat contient des produits dont une partie du volume est en réserve');
   	}
 
   	public function controle()
@@ -46,8 +47,13 @@ class VracValidation extends DocumentValidation
 	    }
 
 	    if(count($ecart_libelles) > 0){
-	      $this->addPoint('vigilance', 'ecart_volume', implode(",", $ecart_libelles)); 
+	      $this->addPoint('vigilance', 'ecart_volume', implode(",", $ecart_libelles));
 	    }
+
+        if($produitsVolumeBloque = $this->document->declaration->getProduitsWithVolumeBloque()) {
+            $produits = array_map(function($val) { return $val->getLibelleComplet(); }, $produitsVolumeBloque);
+            $this->addPoint('vigilance', 'volume_bloque', implode(", ", $produits), $this->generateUrl('vrac_etape', array('sf_subject' => $this->document, 'etape' => 'produits')));
+        }
   	}
 
   	public function getProduitsHashInError() {
