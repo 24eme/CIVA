dbef983bf1546aff798c823d06a541fa4e82bf61
Vincent LAURENT <vince.laurent@gmail.com>
2023-04-11 02:59:33 +0200

    Fixe mail

--- project/apps/civa/modules/email/templates/_vrac_demande_validation_acheteur.php
+++ project/apps/civa/modules/email/templates/_vrac_demande_validation_acheteur.php
@@ -4,4 +4,5 @@ Un projet de contrat a été créé par le vendeur et attend la validation de l'
 
 Pour le visualiser et le valider cliquez sur le lien suivant : <?php echo ProjectConfiguration::getAppRouting()->generate('vrac_fiche', array('sf_subject' => $vrac), true); ?>
 
+
 <?php include_partial('email/footer', array('vrac' => $vrac)); ?>
