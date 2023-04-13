5c9869d6662daa9975076c92ee370f6719eabd9f
Gabriel Poma <gpoma@24eme.fr>
2023-04-13 18:26:38 +0200

    sv: export ajout rebeches

--- project/apps/civa/lib/export/ExportSV11Json.class.php
+++ project/apps/civa/lib/export/ExportSV11Json.class.php
@@ -30,6 +30,10 @@ class ExportSV11Json extends ExportSVJson
         $apporteursParProduit = $this->sv->getApporteursParProduit();
 
         foreach ($apporteursParProduit as $hash_produit => $apporteurs_du_produit) {
+            if (strpos($hash_produit, '/cepages/RB') !== false) {
+                continue; // pas les rebêches dans la boucle principale
+            }
+
             $apporteurs = [];
 
             // pour le code_douane
@@ -76,6 +80,27 @@ class ExportSV11Json extends ExportSVJson
             }
 
             $infosApporteur['volumeAEliminer'] = $volumeAEliminer;
+        }
+
+        // rebêches
+        if (strpos($hash_produit, '/CREMANT/') !== false) {
+            $produitsAssocies = ['typeAssociation' => 'REB'];
+            $hash_rebeche = str_replace(['/declaration/', '/cepages/PN', '/cepages/BL'], ['', '/cepages/RBRS', '/cepages/RB'], $hash_produit);
+            $produitsAssocies['codeProduitAssocie'] = $this->sv->getConfiguration()->get($hash_rebeche);
+
+            if ($this->sv->hasRebechesInProduits()) {
+                // rebeches en détail
+                $rebeches = $apporteur->get($hash_rebeche)->getFirst();
+
+                $produitsAssocies['volumeIssuRaisinsProduitAssocie'] = $rebeches->volume_revendique;
+            } else {
+                // % des rebeches totales
+                if (strpos($hash_produit, '/cepages/BL') !== false) {
+                    $produitsAssocies['volumeIssuRaisinsProduitAssocie'] = $produit->volume_revendique * 100 / $this->sv->rebeches;
+                }
+            }
+
+            $infosApporteur['produitsAssocies'] = $produitsAssocies;
         }
 
         return $infosApporteur;
