e7482b74b05be08e903bd82d443f0fa1363b879e
Gabriel Poma <gpoma@24eme.fr>
2023-04-17 15:23:12 +0200

    sv: export json on refacto getSites()

--- project/apps/civa/lib/export/ExportSV11Json.class.php
+++ project/apps/civa/lib/export/ExportSV11Json.class.php
@@ -115,35 +115,4 @@ class ExportSV11Json extends ExportSVJson
 
         return $infosApporteur;
     }
-
-    public function getSites()
-    {
-        $sites = [];
-        foreach ($this->sv->getNotEmptyLieuxStockage() as $stockage) {
-            $site = [];
-            $site['codeSite'] = $stockage->numero;
-            $sites[$stockage->numero] = $site;
-        }
-
-        foreach ($this->sv->getRecapProduits() as $hash => $produit) {
-            $code_produit = $this->sv->getConfiguration()->get($produit->produit_hash)->code_douane;
-            $mention = $produit->denominationComplementaire;
-
-            foreach ($this->sv->getNotEmptyLieuxStockage() as $id => $lieu) {
-                $produitsLieu = $lieu->produits;
-                $produitsLieu = (is_array($produitsLieu) === false) ? $produitsLieu->toArray() : $produitsLieu;
-                if (array_key_exists($hash, $produitsLieu)) {
-                    $add = [
-                        'codeProduit' => $code_produit,
-                        'mentionValorisante' => $mention ?: "",
-                        'volumeObtenu' => number_format($produitsLieu[$hash], 2, ".", "")
-                    ];
-
-                    $sites[$lieu->numero]['produits'][] = $add;
-                }
-            }
-        }
-
-        return array_values($sites);
-    }
 }
--- project/apps/civa/lib/export/ExportSVJson.class.php
+++ project/apps/civa/lib/export/ExportSVJson.class.php
@@ -26,4 +26,35 @@ class ExportSVJson
 
         return $json.PHP_EOL;
     }
+
+    protected function getSites()
+    {
+        $sites = [];
+        foreach ($this->sv->getNotEmptyLieuxStockage() as $stockage) {
+            $site = [];
+            $site['codeSite'] = $stockage->numero;
+            $sites[$stockage->numero] = $site;
+        }
+
+        foreach ($this->sv->getRecapProduits() as $hash => $produit) {
+            $code_produit = $this->sv->getConfiguration()->get($produit->produit_hash)->code_douane;
+            $mention = $produit->denominationComplementaire;
+
+            foreach ($this->sv->getNotEmptyLieuxStockage() as $id => $lieu) {
+                $produitsLieu = $lieu->produits;
+                $produitsLieu = (is_array($produitsLieu) === false) ? $produitsLieu->toArray() : $produitsLieu;
+                if (array_key_exists($hash, $produitsLieu)) {
+                    $add = [
+                        'codeProduit' => $code_produit,
+                        'mentionValorisante' => $mention ?: "",
+                        'volumeObtenu' => number_format($produitsLieu[$hash], 2, ".", "")
+                    ];
+
+                    $sites[$lieu->numero]['produits'][] = $add;
+                }
+            }
+        }
+
+        return array_values($sites);
+    }
 }
