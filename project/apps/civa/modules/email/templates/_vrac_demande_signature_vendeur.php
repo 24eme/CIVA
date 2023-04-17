0205f5b7e3ee4deba3ce9ced4b48b402e191bba8
Vincent LAURENT <vince.laurent@gmail.com>
2023-04-17 16:41:23 +0200

    Wording

--- project/apps/civa/modules/email/templates/_vrac_demande_signature_vendeur.php
+++ project/apps/civa/modules/email/templates/_vrac_demande_signature_vendeur.php
@@ -2,7 +2,7 @@
 
 Un projet de contrat attend votre signature.
 
-Pour le visualiser et le signer cliquez sur le lien suivant : <?php echo url_for('vrac_fiche', array('sf_subject' => $vrac), true); ?>
+Pour le visualiser et le signer, cliquez sur le lien suivant : <?php echo url_for('vrac_fiche', array('sf_subject' => $vrac), true); ?>
 
 
 <?php include_partial('email/footer', array('vrac' => $vrac)); ?>
