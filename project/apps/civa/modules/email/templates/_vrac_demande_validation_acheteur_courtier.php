f9fdc838bd8f38622b2c97f830bc034aceac98a6
Vincent LAURENT <vince.laurent@gmail.com>
2023-04-17 16:41:23 +0200

    Wording

--- project/apps/civa/modules/email/templates/_vrac_demande_validation_acheteur_courtier.php
+++ project/apps/civa/modules/email/templates/_vrac_demande_validation_acheteur_courtier.php
@@ -2,7 +2,7 @@
 
 Un projet de contrat a été créé par <?php echo  trim($vrac->vendeur->intitule.' '.$vrac->vendeur->raison_sociale) ?> et attend votre validation.
 
-Pour le visualiser et le valider cliquez sur le lien suivant : <?php echo ProjectConfiguration::getAppRouting()->generate('vrac_fiche', array('sf_subject' => $vrac), true); ?>
+Pour le visualiser et le valider, cliquez sur le lien suivant : <?php echo ProjectConfiguration::getAppRouting()->generate('vrac_fiche', array('sf_subject' => $vrac), true); ?>
 
 
 <?php include_partial('email/footer', array('vrac' => $vrac)); ?>
