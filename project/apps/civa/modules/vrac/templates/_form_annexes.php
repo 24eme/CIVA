86d9a2fc674f8351dc583ea7810bdd5929eab165
Jb Lm <lemetayer.jb@gmail.com>
2023-06-12 16:43:24 +0200

    bouton submit qui recharge la page et ne renvoie pas à l'etape suivante

--- project/apps/civa/modules/vrac/templates/_form_annexes.php
+++ project/apps/civa/modules/vrac/templates/_form_annexes.php
@@ -16,7 +16,7 @@
 				<span><?php echo $form['fichier']->renderError() ?></span>
 				<?php echo $form['fichier']->render() ?>
                 <?php echo $form['libelle']->render(['placeholder' => 'Saisir ici le nom de l\'annexe']) ?>
-                <button type="submit" name="ajouter" class="btn_majeur btn_vert" style="padding:0 5px;">
+                <button type="submit" name="submitAndReload" class="btn_majeur btn_vert" style="padding:0 5px;">
                     <svg style="position:relative;top:2px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
                     Ajouter
                 </button>
