74173a52ee649e2c235bb56147253ebd45aabea0
Jb Lm <lemetayer.jb@gmail.com>
2023-06-15 11:59:40 +0200

    mise en evidence des annexes modifiées

--- project/apps/civa/modules/vrac/templates/_ficheAnnexes.php
+++ project/apps/civa/modules/vrac/templates/_ficheAnnexes.php
@@ -15,7 +15,7 @@ $annexes = $vrac->getAllAnnexesFilename();
     </thead>
 	<tbody>
         <?php foreach($annexes as $annexeFilename => $annexe): ?>
-		<tr>
+		<tr class="<?php echo isVersionnerCssClass($vrac, "_attachments/$annexeFilename/content_type") ?>">
 			<td colspan="2">
                 <a class=" " style="padding:0 5px;" href="<?php echo url_for('vrac_annexe', ['sf_subject' => $vrac, 'operation' => 'visualiser', 'annexe' => $annexe]) ?>"><svg style="position:relative;top:2px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/></svg></a>
 				<?php echo str_replace(VracClient::VRAC_PREFIX_ANNEXE,'',$annexeFilename) ?>
