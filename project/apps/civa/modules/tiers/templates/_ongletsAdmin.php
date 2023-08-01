870de7d62c4f966f9fa5d555949ddf89649a215f
Vincent LAURENT <vince.laurent@gmail.com>
2023-08-02 00:36:34 +0200

    class active sur le <li> au lieu du <a>

--- project/apps/civa/modules/tiers/templates/_ongletsAdmin.php
+++ project/apps/civa/modules/tiers/templates/_ongletsAdmin.php
@@ -5,7 +5,7 @@
 	<li class="ui-tabs-disabled"><a href="<?php echo url_for("admin", array(), isset($absolute)) ?>">Achat Récolte</a></li>
 	<li class="ui-tabs-disabled"><a href="<?php echo url_for("admin", array(), isset($absolute)) ?>">Production</a></li>
     <?php if(sfConfig::get("app_giilda_url_drm_admin")): ?>
-	<li><a class="<?php if(preg_match('/(drm)/', $active)): ?>ui-tabs-selected<?php endif; ?>" href="<?php echo sfConfig::get("app_giilda_url_drm_admin") ?>">DRM</a></li>
+	<li class="<?php if(preg_match('/(drm)/', $active)): ?>ui-tabs-selected<?php endif; ?>"><a href="<?php echo sfConfig::get("app_giilda_url_drm_admin") ?>">DRM</a></li>
     <?php endif; ?>
 	<li class="ui-tabs-disabled"><a href="<?php echo url_for("admin", array(), isset($absolute)) ?>">Contrats</a></li>
 	<li class="ui-tabs-disabled"><a href="<?php echo url_for("admin", array(), isset($absolute)) ?>">Gamm@</a></li>
