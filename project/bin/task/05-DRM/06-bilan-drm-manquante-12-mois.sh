045640dabc9cae459bf5255033e71346afef501c
Gabriel Poma <gpoma@24eme.fr>
2023-08-24 18:53:24 +0200

    export: drm manquant on filtre les établissements suspendus

--- project/bin/task/05-DRM/06-bilan-drm-manquante-12-mois.sh
+++ project/bin/task/05-DRM/06-bilan-drm-manquante-12-mois.sh
@@ -10,7 +10,7 @@ mkdir -p $TASK_DIR/tmp 2> /dev/null
 
 PREFIX_EXPORT_FILE=tmp/bilan_drm_$(date +%Y%m%d%H%M%S)
 
-cat $TASK_DIR/export/bi/export_bi_etablissements.csv  | awk -F ';' '{print $5";"$6";"$2}' | sed 's/ETABLISSEMENT-//' | sort  -t ';' -k 1,1 > $TASK_DIR/$PREFIX_EXPORT_FILE"_etablissements.csv.tmp"
+cat $TASK_DIR/export/bi/export_bi_etablissements.csv | grep -v ";SUSPENDU;" | awk -F ';' '{print $5";"$6";"$2}' | sed 's/ETABLISSEMENT-//' | sort  -t ';' -k 1,1 > $TASK_DIR/$PREFIX_EXPORT_FILE"_etablissements.csv.tmp"
 curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/drm/_view/derniere?reduce=false" | awk -F '"' '{if ( $10 >= "'$( date  --date="13 months ago" +"%Y%m" )'" && $10 <= "'$( date  --date="40 days ago" +"%Y%m" )'" ) print $4; }' | grep -v '\-M' | sed 's/-/ /g'  | awk '{ tab[ $2 ][ $3 ] = 1; mois[$3] = 1 } END{ printf("CVI;"); for (y in mois ) { printf "%s;", y; } print " "; for (i in tab) { printf("%s;", i)  ; for (y in mois ) { printf ( "%s;", tab[i][y] ); }  print " " ;} }'  | sed 's/;;/;øøøøøø;/g' | sed 's/;;/;øøøøøø;/g' > $TASK_DIR/$PREFIX_EXPORT_FILE"_toutes.csv.tmp"
 tail -n +2 $TASK_DIR/$PREFIX_EXPORT_FILE"_toutes.csv.tmp" | sort > $TASK_DIR/$PREFIX_EXPORT_FILE"_toutes.csv.sorted.tmp"
 
