49ce53aeb21fc6d66bb69ac855e45a45b09487cc
Gabriel Poma <gpoma@24eme.fr>
2023-11-02 12:37:06 +0100

    sv: on affiche le revendiqué mouts dans l'étape 3

--- project/plugins/acVinSVPlugin/modules/sv/templates/_saisieRevendicationSV12.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/_saisieRevendicationSV12.php
@@ -33,21 +33,21 @@
       <th class="col-xs-3">Produit</th>
       <th class="col-xs-1 text-center"></th>
       <th class="col-xs-2 text-center">Volume de moûts<br /><small>(hl)</small></th>
-      <th class="col-xs-2 text-center">Volume de moûts revendiqué<br /><small>(hl)</small></th>
       <th class="col-xs-2 text-center">Superficie de moûts <small>(ares)</small></th>
+      <th class="col-xs-2 text-center">Volume de moûts revendiqué<br /><small>(hl)</small></th>
     </tr>
   </thead>
   <tbody>
   <?php foreach($form['produits'] as $hash => $formProduit): ?>
-  <?php if(!isset($formProduit['volume_mouts'])): continue; endif; ?>
+  <?php if(!isset($formProduit['volume_mouts_revendique'])): continue; endif; ?>
   <?php $produit = $sv->get($hash); ?>
     <tr class="vertical-center">
       <td><?php echo $produit->nom ?></td>
       <td><?php echo $produit->getRawValue()->getLibelleHtml() ?></td>
       <td></td>
-      <td><?php echoFloat($produit->volume_mouts) ?> <small class="text-muted">hl</small></td>
+      <td class="text-right"><?php echoFloat($produit->volume_mouts) ?> <small class="text-muted">hl</small></td>
+      <td class="text-right"><?php echoFloat($produit->superficie_mouts) ?> <small class="text-muted">ares</small></td>
       <td><div class="input-group"><?php echo $formProduit['volume_mouts_revendique']->render() ?><span class="input-group-addon" style="background: #f2f2f2;"><small class="text-muted">hl</small></span></div></td>
-      <td><?php echoFloat($produit->superficie_mouts) ?> <small class="text-muted">ares</small></td>
     </tr>
   <?php endforeach ?>
   </tbody>
