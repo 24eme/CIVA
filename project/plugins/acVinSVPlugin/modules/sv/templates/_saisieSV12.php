2780dc8f323a9cf1df5406920f140c4316e4bc26
Gabriel Poma <gpoma@24eme.fr>
2023-11-02 12:25:11 +0100

    sv: bouton ajout de mouts

--- project/plugins/acVinSVPlugin/modules/sv/templates/_saisieSV12.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/_saisieSV12.php
@@ -12,7 +12,17 @@
   <?php $produit = $sv->get($hash); ?>
     <tr class="vertical-center">
       <td><?php echo $produit->nom ?></td>
-      <td><?php echo $produit->getRawValue()->getLibelleHtml() ?></td>
+      <td>
+        <?php echo $produit->getRawValue()->getLibelleHtml() ?>
+        <?php if (strpos($hash, '/CREMANT/') !== false && ! $produit->getRawValue()->exist('volume_mouts')): ?>
+          <br/>
+          <small>
+            <a class="text-muted" href="<?php echo url_for('sv_ajout_mouts_apporteur', ['id' => $sv->_id, 'cvi' => $cvi, 'hash' => str_replace('/', '-', $hash)]) ?>">
+              Ajouter des moûts
+            </a>
+          </small>
+        <?php endif ?>
+      </td>
       <td><div class="input-group"><?php echo $formProduit['superficie_recolte']->render() ?><span class="input-group-addon" style="background: #f2f2f2;"><small class="text-muted">ares</small></span></div></td>
       <td><div class="input-group"><?php echo $formProduit['quantite_recolte']->render() ?><input class="form-control text-right input-float input-sm input_quantite_pre hidden" type="text" autocomplete="off" data-decimal-auto="2" data-decimal="2" readonly="readonly" /><span class="input-group-addon" style="background: #f2f2f2;"><small class="text-muted">kg</small></span></div></td>
     </tr>
@@ -29,7 +39,6 @@
       <th class="col-xs-3">Produit</th>
       <th class="col-xs-1 text-center"></th>
       <th class="col-xs-2 text-center">Volume de moûts<br /><small>(hl)</small></th>
-      <th class="col-xs-2 text-center">Volume de moûts revendiqué<br /><small>(hl)</small></th>
       <th class="col-xs-2 text-center">Superficie de moûts <small>(ares)</small></th>
     </tr>
   </thead>
@@ -42,7 +51,6 @@
       <td><?php echo $produit->getRawValue()->getLibelleHtml() ?></td>
       <td></td>
       <td><div class="input-group"><?php echo $formProduit['volume_mouts']->render() ?><span class="input-group-addon" style="background: #f2f2f2;"><small class="text-muted">hl</small></span></div></td>
-      <td><div class="input-group"><?php echo $formProduit['volume_mouts_revendique']->render() ?><span class="input-group-addon" style="background: #f2f2f2;"><small class="text-muted">hl</small></span></div></td>
       <td><div class="input-group"><?php echo $formProduit['superficie_mouts']->render() ?><span class="input-group-addon" style="background: #f2f2f2;"><small class="text-muted">ares</small></span></div></td>
     </tr>
   <?php endforeach ?>
