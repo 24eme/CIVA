de1740bdde79be5c4021d9f3d62468dd870ca017
Vincent LAURENT <vince.laurent@gmail.com>
2023-12-14 16:14:34 +0100

    Mout revendiqué setté autmatiquement

--- project/plugins/acVinSVPlugin/lib/form/SV12RevendicationProduitForm.class.php
+++ project/plugins/acVinSVPlugin/lib/form/SV12RevendicationProduitForm.class.php
@@ -4,7 +4,6 @@ class SV12RevendicationProduitForm extends acCouchdbObjectForm
 {
     protected function updateDefaultsFromObject() {
         parent::updateDefaultsFromObject();
-        $this->setDefault('taux_extraction', $this->getObject()->getTauxExtraction());
         if($this->getObject()->exist('volume_mouts')) {
             $this->setDefault('volume_mouts_revendique', $this->getObject()->volume_mouts);
         }
@@ -12,7 +11,7 @@ class SV12RevendicationProduitForm extends acCouchdbObjectForm
 
     public function configure()
     {
-        if ($this->getObject()->superficie_recolte || $this->getObject()->quantite_recolte) {
+        if ($this->getObject()->superficie_recolte || $this->getObject()->quantite_recolte || $this->getObject()->volume_revendique) {
             $this->setWidget('volume_revendique', new bsWidgetFormInputFloat(array(), ['placeholder' => '', 'class' => 'form-control text-right input-float input_volume_revendique']));
             $this->setValidator('volume_revendique', new sfValidatorNumber(array('required' => false)));
         }
--- project/plugins/acVinSVPlugin/modules/sv/templates/apporteursSuccess.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/apporteursSuccess.php
@@ -46,14 +46,14 @@
       <small class="text-muted">ares</small>
   </td>
   <td class="text-right">
-      <?php if($sv->type == SVClient::TYPE_SV12): ?>
-        <?php if ($recap['quantite'] && $recap['mouts_revendique']): ?>
-          (<abbr title="Revendiqué">R</abbr>) <?php echoFloat($recap['quantite']) ?> <small class="text-muted">kg</small><br/>
-          (<abbr title="Moûts">M</abbr>) <?php echoFloat($recap['mouts_revendique']) ?> <small class="text-muted">hl</small><br/>
-        <?php elseif ($recap['quantite']): ?>
-          <?php echoFloat($recap['quantite']) ?> <small class="text-muted">kg</small><br/>
-        <?php endif ?>
-      <?php elseif($recap['revendique'] || $recap['mouts_revendique']): ?>
+      <?php if($sv->type == SVClient::TYPE_SV12 && $recap['quantite']): ?>
+          <?php if($recap['mouts_revendique']): ?>(<abbr title="Revendiqué">R</abbr>) <?php endif; ?><?php echoFloat($recap['quantite']) ?> <small class="text-muted">kg</small>
+      <?php endif ?>
+      <?php if ($sv->type == SVClient::TYPE_SV12 && $recap['mouts_revendique']): ?>
+          <?php if($recap['quantite']): ?><br /><?php endif; ?>
+          (<abbr title="Moûts">M</abbr>) <?php echoFloat($recap['mouts_revendique']) ?> <small class="text-muted">hl</small>
+      <?php endif; ?>
+      <?php if($sv->type == SVClient::TYPE_SV11 && ($recap['revendique'] || $recap['mouts_revendique'])): ?>
         <?php if($recap['revendique'] && $recap['mouts_revendique']): ?>
         (<abbr title="Revendiqué + Moûts">R+M</abbr>) <?php echoFloat($recap['revendique'] + $recap['mouts_revendique']) ?>
         <?php elseif($recap['mouts_revendique']): ?>
