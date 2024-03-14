d25e3d79338bdafd4c4a3b66332f00d9b6f9f792
Gabriel Poma <gpoma@24eme.fr>
2024-03-14 18:04:43 +0100

    civa: liens symboliques vers plugins giilda

--- project/plugins/acVinComptePlugin/modules/compte/templates/addremovetagSuccess.php
+++
@@ -1,6 +0,0 @@
-<section id="principal" class="recherche_contact">
- <section id="contenu_etape">
-   <h2>Modification des tags en cours</h2>
-   <p><?php echo $restants; ?> restants</p>
- </section>
-</section>
