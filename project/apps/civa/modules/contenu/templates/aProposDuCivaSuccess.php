92a23371e1aaafda6c070c94decb3d40a4761c89
chachathecool <charlene.connorshg@gmail.com>
2024-08-13 13:20:32 +0200

    Modification dénomination CIVA dans le module de contenu

--- project/apps/civa/modules/contenu/templates/aProposDuCivaSuccess.php
+++ project/apps/civa/modules/contenu/templates/aProposDuCivaSuccess.php
@@ -2,13 +2,10 @@
 
 <!-- #application_dr -->
 <div id="application_dr" class="clearfix pages-statiques">
-    <p>Le Conseil Interprofessionnel des Vins d’Alsace (CIVA) a été institué par décret en date du 22 avril 1963.</p>
+    <p>Le Comité Interprofessionnel des Vins d’Alsace (CIVA) a été institué par décret en date du 22 avril 1963.</p>
 
     <p>Son champ d’activité couvrait à l’origine l’Appellation d’Origine Contrôlée "Alsace", reconnue par décret du 3 octobre 1962.</p>
 
     <p>Il s’est étendu par la suite aux appellations "Alsace Grand Cru" et "Crémant d’Alsace" reconnues respectivement par décret du 20 novembre 1975 modifié en 1992, et décret du 24 août 1976.</p>
-
-    <p>Ainsi le Comité Interprofessionnel du Vin d’Alsace tel qu’il était dénommé au départ, est-il devenu par la suite "Conseil Interprofessionnel des Vins d’Alsace" notamment pour tenir compte de la diversité des vins d’Alsace et de leurs appellations.</p>
-
 </div>
 <!-- fin #application_dr -->
