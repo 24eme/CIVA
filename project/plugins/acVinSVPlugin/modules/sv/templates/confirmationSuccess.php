d88a7bb72543b45ed0ba6531094e724b5c486196
chachathecool <charlene.connorshg@gmail.com>
2024-11-18 12:10:31 +0100

    Refactor: déplacer l'encart de partage AVA
    Lorsque le négociant a autorisé le partage des données
    avec l'AVA, un encart "Autorisation de transmission à l'AVA"
    s'affiche sur la page de confirmation sous l'encart "Confirmation"

--- project/plugins/acVinSVPlugin/modules/sv/templates/confirmationSuccess.php
+++ project/plugins/acVinSVPlugin/modules/sv/templates/confirmationSuccess.php
@@ -6,11 +6,7 @@
     <h2 style="margin-bottom: 20px;">Déclaration envoyée</h2>
 
     <div class="row">
-    <?php  if($sv->hasAutorisation(SVClient::AUTORISATION_AVA)): ?>
-        <div class="col-xs-5">
-    <?php else: ?>
         <div class="col-xs-7">
-    <?php endif ?>
           <div class="panel panel-default panel-success">
             <div class="panel-heading"><h3 class="panel-title">Confirmation</h3></div>
             <div class="panel-body" style="padding-bottom: 36px;">
@@ -20,33 +16,31 @@
           </div>
         </div>
 
-        <?php  if($sv->hasAutorisation(SVClient::AUTORISATION_AVA)): ?>
-        <div class="col-xs-4">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h3 class="panel-title">Autorisation de transmission à l'AVA (ODG)</h2></div>
-                    <div class="panel-body" style="padding-bottom: 29px;">
-                        <p>Vous pourrez directement exploiter les données de votre Déclaration de Production en télédéclarant votre Déclaration de Revendication sur le <a style="text-decoration: underline;" target="_blank" href="<?php echo sfConfig::get('app_ava_url') ?>">portail de télédéclaration de l'Association des Viticulteurs d'Alsace</a></p>
-                    </div>
+        <div class="col-xs-5">
+            <div class="panel panel-default panel-default">
+                <div class="panel-heading"><h3 class="panel-title">Votre avis</h3></div>
+                <div class="panel-body">
+                    <p>Votre retour d'expérience nous intéresse</p>
+                    <p>Laissez nous vos commentaires à propos de la saisie de la déclaration de Production.</p>
+                    <a href="<?php echo url_for('sv_feed_back', $sv); ?>">
+                    <img src="/images/boutons/btn_donnez_votre_avis.png" alt="Donnez votre avis" /></a>
+                </div>
             </div>
         </div>
-        <?php endif; ?>
+    </div>
 
-        <?php  if($sv->hasAutorisation(SVClient::AUTORISATION_AVA)): ?>
-            <div class="col-xs-3">
-        <?php else: ?>
-            <div class="col-xs-5">
-        <?php endif ?>
-              <div class="panel panel-default panel-default">
-                <div class="panel-heading"><h3 class="panel-title">Votre avis</h3></div>
-                <div class="panel-body" <?php  if($sv->hasAutorisation(SVClient::AUTORISATION_AVA)): ?> style="padding-bottom: 16px;" <?php endif; ?> >
-                  <p>Votre retour d'expérience nous intéresse</p>
-                  <p>Laissez nous vos commentaires à propos de la saisie de la déclaration de Production.</p>
-                  <a href="<?php echo url_for('sv_feed_back', $sv); ?>">
-                      <img src="/images/boutons/btn_donnez_votre_avis.png" alt="Donnez votre avis" />
+    <?php  if($sv->hasAutorisation(SVClient::AUTORISATION_AVA)): ?>
+        <div class="row">
+            <div class="col-xs-7">
+                <div class="panel panel-default panel-default">
+                    <div class="panel-heading"><h3 class="panel-title">Autorisation de transmission à l'AVA (ODG)</h3></div>
+                        <div class="panel-body">
+                            <p>Vous pourrez directement exploiter les données de votre Déclaration de Production en télédéclarant votre Déclaration de Revendication sur le <a style="text-decoration: underline;" target="_blank" rel="noopener noreferrer" href="<?php echo sfConfig::get('app_ava_url') ?>">portail de télédéclaration de l'Association des Viticulteurs d'Alsace</a></p>
+                        </div>
                 </div>
-          </div>
+            </div>
         </div>
-      </div>
+      <?php endif; ?>
 
     <div class="text-left">
       <a href="<?php echo url_for('mon_espace_civa_production', $sv->etablissement) ?>"><img src="/images/boutons/btn_retourner_mon_espace.png" alt="Retourner à mon espace CIVA" name="boutons[previous]" /></a>
