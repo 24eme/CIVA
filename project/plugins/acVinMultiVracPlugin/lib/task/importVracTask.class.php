f25a0a83d58da0be7f889bd26351a8f2cd9fb741
Gabriel Poma <gpoma@24eme.fr>
2025-03-04 19:24:10 +0100

    tache import vrac + "modele" mais fonction d'import à changer

---
+++ project/plugins/acVinMultiVracPlugin/lib/task/importVracTask.class.php
@@ -0,0 +1,37 @@
+<?php
+
+class importVracTask extends importAbstractTask
+{
+    protected function configure() {
+        // add your own arguments here
+        $this->addArguments(array(
+            new sfCommandArgument('file', sfCommandArgument::REQUIRED, "Fichier csv pour l'import"),
+        ));
+
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declaration'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+        ));
+
+        $this->namespace = 'import';
+        $this->name = 'vracs';
+        $this->briefDescription = 'Importe des contrats depuis un fichier csv';
+        $this->detailedDescription = <<<EOF
+La tâche [importVrac|INFO] importe des vracs depuis un fichier csv avec un format prédéfini.
+Call it with:
+
+  [php symfony importVrac|INFO]
+EOF;
+    }
+
+    protected function execute($arguments = array(), $options = array()) {
+        // initialize the database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+        $file = new CsvFile($arguments['file']);
+        $vracs = VracCsvImport::createFromArray($file->getCsv());
+        $vracs->import();
+    }
+}
