f028949fc68a9016d0ecada7f741df7a7724aaad
JB LM <lemetayer.jb@gmail.com>
2025-03-27 18:45:30 +0100

    correction du bug de saisie des enlevements

--- project/plugins/acVinMultiVracPlugin/lib/form/VracEnlevementsCollectionForm.class.php
+++ project/plugins/acVinMultiVracPlugin/lib/form/VracEnlevementsCollectionForm.class.php
@@ -2,36 +2,32 @@
 class VracEnlevementsCollectionForm extends acCouchdbObjectForm
 {
 	public $virgin_object = null;
-	
+
 	public function configure()
 	{
 		$this->disableLocalCSRFProtection();
-		/*if (count($this->getObject()) == 0) {
-			$this->virgin_object = $this->getObject()->add();
-		}*/
 		foreach ($this->getObject() as $key => $object) {
-			$this->embedForm ($key, new VracEnlevementForm($object));
+			$this->embedForm($key, new VracEnlevementForm($object));
 		}
 	}
 
 	public function bind(array $taintedValues = null, array $taintedFiles = null)
 	{
+        foreach($taintedValues as $key => $values) {
+           if(!is_array($values) || array_key_exists($key, $this->embeddedForms)) {
+               continue;
+           }
+           $this->embedForm($key, new VracEnlevementForm($this->getObject()));
+       }
 		foreach ($this->embeddedForms as $key => $form) {
-			if(!array_key_exists($key, $taintedValues)) {
-				$this->unEmbedForm($key);
-			}
+            if($form) {
+                $form->bind($taintedValues[$key], isset($taintedFiles[$key])? $taintedFiles[$key] : null);
+                $this->updateEmbedForm($key, $form);
+            }
 		}
-		foreach($taintedValues as $key => $values) {
-			if(!is_array($values) || array_key_exists($key, $this->embeddedForms)) {
-				continue;
-			}
-
-			$this->embedForm($key, new VracEnlevementForm($this->getObject()));
-		}
-
-		//parent::bind($taintedValues, $taintedFiles);
+		parent::bind($taintedValues, $taintedFiles);
 	}
-	
+
 	public function updateEmbedForm($name, $form) {
     	$this->widgetSchema[$name] = $form->getWidgetSchema();
         $this->validatorSchema[$name] = $form->getValidatorSchema();
