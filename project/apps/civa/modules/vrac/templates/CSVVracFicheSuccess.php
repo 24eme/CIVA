e099c1ca5074612f7d0e54bc4c77d8b29b716155
Gabriel Poma <gpoma@24eme.fr>
2025-09-17 17:57:06 +0200

    civa: multivrac: affichage erreur visu

--- project/apps/civa/modules/vrac/templates/CSVVracFicheSuccess.php
+++ project/apps/civa/modules/vrac/templates/CSVVracFicheSuccess.php
@@ -2,6 +2,29 @@
     <h1 class="titre_principal">Bilan de l'import des contrats</h1>
     <div class="contenu">
 
+        <h3 class="titre_section">Erreurs dans le fichier</h3>
+
+        <?php if ($csvVrac->hasErreurs()): ?>
+            <div class="row bg-secondary">
+                <?php for ($i = 1; $i <= count($vracimport->getCsv()); $i++): ?>
+                    <?php if ($listeerreurs = $csvVrac->getErreurs($i)): ?>
+                        <div class="col-xs-1">
+                            <a href="#line<?php echo $i ?>">#<?php echo $i ?></a>
+                        </div>
+                        <div class="col-xs-11">
+                            <?php foreach ($listeerreurs as $e): ?>
+                                <p><?php echo $e->diagnostic; ?></p>
+                            <?php endforeach ?>
+                        </div>
+                    <?php endif ?>
+                <?php endfor; ?>
+            </div>
+        <?php else: ?>
+            <div class="alert alert-success">
+                Pas d'erreur dans le fichier
+            </div>
+        <?php endif ?>
+
         <h3 class="titre_section">Fichier importé</h3>
 
         <div class="table-responsive">
@@ -16,7 +39,7 @@
                 </thead>
                 <tbody>
                     <?php foreach ($vracimport->getCsv() as $num => $line): ?>
-                    <tr>
+                    <tr id="line<?php echo $num + 1 ?>" class="<?php echo count($csvVrac->getErreurs($num + 1)) ? 'bg-danger' : '' ?>">
                         <td><?php echo $num + 1 ?></td>
                         <?php foreach ($line as $td): ?>
                             <td><?php echo $td ?></td>
