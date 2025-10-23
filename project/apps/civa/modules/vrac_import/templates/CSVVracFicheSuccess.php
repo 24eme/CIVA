97cf5278b107b71290250d5cfb6722046ec99939
Gabriel Poma <gpoma@24eme.fr>
2025-10-23 16:33:16 +0200

    civa: multivrac: étape import OK (étape 2)

--- project/apps/civa/modules/vrac_import/templates/CSVVracFicheSuccess.php
+++ project/apps/civa/modules/vrac_import/templates/CSVVracFicheSuccess.php
@@ -21,5 +21,6 @@
     <?php if ($csvVrac->statut === CSVVRACClient::LEVEL_ERROR): ?>
         <?php include_partial('vrac_import/fiche_erreur', compact('csvVrac', 'vracimport', 'compte')) ?>
     <?php else: ?>
+        <?php include_partial('vrac_import/fiche_conforme', compact('csvVrac', 'vracimport', 'compte', 'formAnnexe')) ?>
     <?php endif; ?>
 </div>
