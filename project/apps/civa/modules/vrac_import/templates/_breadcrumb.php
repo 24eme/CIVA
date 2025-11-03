2dafcff3ab167909b1ebe380d4b9ce154e8498e8
Vincent LAURENT <vince.laurent@gmail.com>
2025-11-03 17:14:24 +0100

    Breadcrumb et rail d'étape en partial

---
+++ project/apps/civa/modules/vrac_import/templates/_breadcrumb.php
@@ -0,0 +1,5 @@
+<ol class="breadcrumb">
+    <li><a href="#">Contrats</a></li>
+    <li><a href="#"><?php echo $compte->nom_a_afficher ?></a></li>
+    <li><a href="#">Import de contrats</a></li>
+</ol>
