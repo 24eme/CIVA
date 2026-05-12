306939ced395503f7131778eb2b26b282a4d34f8
Vincent LAURENT <vince.laurent@gmail.com>
2026-05-12 12:52:07 +0200

    Tâche document set value

--- project/plugins/acCouchdbPlugin/lib/task/document/acCouchdbDocumentSetValueTask.class.php
+++ project/plugins/acCouchdbPlugin/lib/task/document/acCouchdbDocumentSetValueTask.class.php
@@ -14,7 +14,8 @@ class acCouchdbDocumentSetValueTask extends sfBaseTask
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
       new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
-      new sfCommandOption('delete', null, sfCommandOption::PARAMETER_OPTIONAL, 'Option for deleting one field', false),
+      new sfCommandOption('unset', null, sfCommandOption::PARAMETER_REQUIRED, 'Remove the field', false),
+
       // add your own options here
     ));
 
@@ -22,10 +23,10 @@ class acCouchdbDocumentSetValueTask extends sfBaseTask
     $this->name             = 'setvalue';
     $this->briefDescription = '';
     $this->detailedDescription = <<<EOF
-The [acCouchdbDocumentSetValue|INFO] task does things.
+The [maintenanceCompteStatut|INFO] task does things.
 Call it with:
 
-  [php symfony document:setvalue|INFO]
+  [php symfony maintenanceCompteStatut|INFO]
 EOF;
   }
 
@@ -35,66 +36,80 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-    $output = array();
-    $doc = acCouchdbManager::getClient()->find($arguments['doc_id']);
-    if(isset($options["delete"]) && $options["delete"]){
-      $hash = array_shift($arguments['hash_values']);
-      $doc->remove($hash);
-      $output[] = $hash.":supprimé";
-    }else{
-
-      $values = array();
-      $hash = null;
-      $value = null;
-      foreach($arguments['hash_values'] as $i => $arg) {
-          if($i % 2 == 0) {
-              $hash = $arg;
-          } else {
-              $value = $arg;
-          }
+    $values = array();
+    $hash = null;
+    $value = null;
+    foreach($arguments['hash_values'] as $i => $arg) {
+        if ($options['unset']) {
+            $values[$arg] = true;
+            continue;
+        }
+        if($i % 2 == 0) {
+            $hash = $arg;
+        } else {
+            $value = $arg;
+        }
 
 
-          if($hash && $value) {
-              $values[$hash] = $value;
-              $hash = null;
-              $value = null;
-          }
-      }
-
-
-
-      if(!$doc) {
-
-          return;
-      }
-
-      foreach($values as $hash => $value) {
-          if(!$doc->exist($hash)) {
-
-              return;
-          }
-          if($doc->get($hash) instanceof acCouchdbJson) {
-
-              return;
-          }
-          if($doc->get($hash) === $value) {
-
-              continue;
-          }
-
-          $output[] = $hash.":\"".$value."\" (".$doc->get($hash).")";
-          $doc->set($hash, $value);
-      }
+        if($hash && $value) {
+            $values[$hash] = $value;
+            $hash = null;
+            $value = null;
+        }
     }
 
-      $oldrev = $doc->_rev;
 
-      $doc->save();
+    $doc = acCouchdbManager::getClient()->find($arguments['doc_id']);
 
-      if($oldrev == $doc->_rev) {
-          return;
-      }
-      echo "Le document ".$doc->_id."@".$oldrev." a été sauvé @".$doc->_rev.", les valeurs suivantes ont été changés : ".implode(",", $output)."\n";
+    if(!$doc) {
 
+        return;
+    }
+
+    $output = array();
+    foreach($values as $hash => $value) {
+        if ($options['unset']) {
+            $doc->remove($hash);
+            $output[] = $hash." REMOVING";
+            continue;
+        }
+        $doc->add($hash);
+
+        if($doc->get($hash) instanceof acCouchdbJson) {
+
+            return;
+        }
+
+        if($value === "null") {
+            $value = null;
+        }
+        if ($value === "false") {
+            $value = false;
+        }elseif ($value === "true") {
+            $value = true;
+        }
+
+        if($doc->get($hash) === $value) {
+
+            continue;
+        }
+
+        if(preg_match("/^\+[0-9]+$/", $value)) {
+            $value = + str_replace("+", "", $value);
+        }
+
+        $output[] = $hash.":\"".$value."\" (".$doc->get($hash).")";
+        $doc->set($hash, $value);
+    }
+
+    $oldrev = $doc->_rev;
+
+    $doc->save();
+
+    if($oldrev == $doc->_rev) {
+        return;
+    }
+
+    echo "Le document ".$doc->_id."@".$oldrev." a été sauvé @".$doc->_rev.", les valeurs suivantes ont été changés : ".implode(",", $output)."\n";
   }
 }
