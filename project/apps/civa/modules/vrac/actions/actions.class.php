5a2080e19cb4f44e5c362015687262bcc6425633
Gabriel Poma <gpoma@24eme.fr>
2026-09-01 18:49:58 +0200

    civa: téléchargement annexes bonnes infos de fichier

--- project/apps/civa/modules/vrac/actions/actions.class.php
+++ project/apps/civa/modules/vrac/actions/actions.class.php
@@ -871,8 +871,15 @@ class vracActions extends sfActions
             }
 		}
 		if (($filename = $vrac->getAnnexeFilename($annexe)) && $operation == 'visualiser') {
+            $fileinfos = $vrac->_attachments->get($filename);
+            $file =      $vrac->getAttachmentUri($filename);
+
+            $this->getResponse()->setContentType($fileinfos->content_type);
+            $this->getResponse()->setHttpHeader('Content-Length', $fileinfos->length);
             $this->getResponse()->setHttpHeader('Content-disposition', 'attachment; filename="' . $filename . '"');
-            return $this->renderText(file_get_contents($vrac->getAttachmentUri($filename)));exit;
+
+            return $this->renderText(file_get_contents($file));
+            exit;
 		}
         return $this->redirect('vrac_etape', array('sf_subject' => $vrac, 'etape' => VracEtapes::ETAPE_ANNEXES));
     }
